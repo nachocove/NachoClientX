8bd07e026a8d0989373998146cf42ddadcd0b59d
Steve Scalpone <stevescalpone@Steves-MacBook-Pro.local>
2013-11-21 22:29:49 -0800

    Display more info about a folder with ToString (for debugging).

--- NachoClient.Android/NachoCore/Model/NcFolder.cs
+++ NachoClient.Android/NachoCore/Model/NcFolder.cs
@@ -8,13 +8,23 @@ namespace NachoCore.Model
     {
         [Indexed]
         public string ServerId { get; set; }
+
         [Indexed]
         public string ParentId { get; set; }
+
         public string AsSyncKey { get; set; }
+
         public bool AsSyncRequired { get; set; }
+
         [Indexed]
         public string DisplayName { get; set; }
+
         public uint Type { get; set; }
+
+        public override string ToString ()
+        {
+            return "NcFolder: sid=" + ServerId + " pid=" + ParentId + " skey=" + AsSyncKey + " dn=" + DisplayName + " type=" + Type.ToString();
+        }
     }
 }
 
