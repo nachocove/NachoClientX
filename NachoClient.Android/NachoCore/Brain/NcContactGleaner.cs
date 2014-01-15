a522bc6ab9ef945bb30b71c7d30568be0a5f3b01
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-01-15 15:55:54 -0800

    fixes #39, fixes #36 (gleaned contacts)

---
+++ NachoClient.Android/NachoCore/Brain/NcContactGleaner.cs
@@ -0,0 +1,16 @@
+//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Brain
+{
+    public class ContactGleaner
+    {
+        public ContactGleaner ()
+        {
+        }
+
+
+    }
+}
+
