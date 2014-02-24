2b7f0b68792fc4e70857e2d3eba67bd85de65005
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-02-24 10:41:25 -0800

    fixed p-invoke bug (again)

--- NachoClient.iOS/NachoPlatform.iOS/RegDomiOS.cs
+++ NachoClient.iOS/NachoPlatform.iOS/RegDomiOS.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Runtime.InteropServices;
 
 namespace NachoPlatform
@@ -27,11 +28,16 @@ namespace NachoPlatform
         }
 
         [DllImport("__Internal")]
-        private static extern string nacho_get_regdom (string domain);
+        private static extern void nacho_get_regdom (StringBuilder dest, uint limit, string domain);
 
-        public string RegDomFromFqdn (string domain) {
-            return nacho_get_regdom (domain);
+        public string RegDomFromFqdn (string domain)
+        {
+            // RFC 2818: The DNS itself places only one restriction on the particular labels that can be used to identify resource records. That one restriction relates to the length of the label and the full name. The length of any one label is limited to between 1 and 63 octets. A full domain name is limited to 255 octets (including the separators)."
+            lock (syncRoot) {
+                StringBuilder sb = new StringBuilder (256);
+                nacho_get_regdom (sb, (uint)sb.Capacity, domain);
+                return sb.ToString ();
+            }
         }
     }
 }
-
