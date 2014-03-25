e0739b66decdd4d79b57f63907aa199403f0df88
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-03-25 14:57:26 -0700

    each error case is covered. fixes #109, #90, #89, #12, #64

---
+++ NachoClient.Android/NachoCore/BackEnd/ActiveSync/IBEContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using NachoCore.Model;
+using NachoCore.Utils;
+
+namespace NachoCore.ActiveSync
+{
+    public interface IBEContext
+    {
+        IProtoControlOwner Owner { set; get; }
+        AsProtoControl ProtoControl { set; get; }
+        McProtocolState ProtocolState { get; set; }
+        McServer Server { get; set; }
+        McAccount Account { get; }
+        McCred Cred { get; }
+    }
+}
+
