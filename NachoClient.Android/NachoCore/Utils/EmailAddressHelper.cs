af3b0619a313bde5f945b6d47d3a3b25d277ee53
Henry Kwok <henry.kwok@nukona.com>
2014-04-05 04:58:02 -0700

    Add a method for parsing a single or a list of email addresses with or without display name.

---
+++ NachoClient.Android/NachoCore/Utils/EmailAddressHelper.cs
@@ -0,0 +1,25 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace NachoCore.Utils
+{
+    public class EmailAddressHelper
+    {
+        public static List<MailAddress> ParseString (string emailAddressString)
+        {
+            List<MailAddress> emailAddressList = new List<MailAddress> ();
+            char[] comma = new char[] {','};
+            string[] emailAddressStrings = 
+                emailAddressString.Split (comma, 1000000, StringSplitOptions.RemoveEmptyEntries); // TODO - hardcoded limit.
+            foreach (string s in emailAddressStrings) {
+                MailAddress emailAddress = new MailAddress (s.Trim ().Replace("\"", ""));
+                emailAddressList.Add (emailAddress);
+            }
+            return emailAddressList;
+        }
+    }
+}
+
