f14dc78978ad169325e9adff05c8bc87c36c334b
Henry Kwok <henryk@nachocove.com>
2014-04-25 01:28:58 -0700

    Add Crashlytics dSYM upload.

---
+++ NachoClient.iOS/UploadDSym/nachomail/nachomail/NachoMailAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NachoMailAppDelegate.h
+//  nachomail
+//
+//  Created by Henry Kwok on 4/18/14.
+//  Copyright (c) 2014 Nacho Cove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NachoMailAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NachoClient.iOS/UploadDSym/nachomail/nachomail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nachomail
+//
+//  Created by Henry Kwok on 4/18/14.
+//  Copyright (c) 2014 Nacho Cove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NachoMailAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NachoMailAppDelegate class]));
+    }
+}
