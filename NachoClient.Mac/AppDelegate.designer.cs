ea25993072302fc5046032fbae048fd0a4520bec
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-05-01 11:22:55 -0700

    added mac client shell

---
+++ NachoClient.Mac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NachoClient.Mac
+{
+    // Should subclass MonoMac.AppKit.NSResponder
+    [MonoMac.Foundation.Register ("AppDelegate")]
+    public partial class AppDelegate
+    {
+    }
+}
+
