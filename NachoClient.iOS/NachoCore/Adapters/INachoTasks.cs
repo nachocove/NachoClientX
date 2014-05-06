7712717b55b1d77a636cfbafd0b36d64d345dacb
Steve Scalpone <sscalpone@gmail.com>
2014-05-06 11:59:09 -0700

    Xamarin changed the ProductVersion value for me.

---
+++ NachoClient.iOS/NachoCore/Adapters/INachoTasks.cs
@@ -0,0 +1,14 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoClient.iOS
+{
+    public class INachoTasks
+    {
+        public INachoTasks ()
+        {
+        }
+    }
+}
+
