21fcf8b7bf895c33cd6ecee1de1f1111c2e70586
Steve Scalpone <sscalpone@gmail.com>
2014-05-09 14:09:21 -0700

    More address unit tests.  This in response to email from amzn.

--- Test.Android/EmailAddressHelperTest.cs
+++ Test.Android/EmailAddressHelperTest.cs
@@ -36,6 +36,12 @@ namespace Test.Android
                 new MailboxAddress ("Jeff Enderwick", "jeffe@nachocove.com"),
                 new MailboxAddress ("Steve Scalpone", "steves@nachocove.com"),
             };
+            MailboxAddress[] expected5 = new MailboxAddress[1] {
+                new MailboxAddress ("", "henryk@nachocove.com")
+            };
+            MailboxAddress[] expected6 = new MailboxAddress[1] {
+                new MailboxAddress ("", "henryk@nachocove.com")
+            };
 
             // Single email address, no display name
             InternetAddressList got1 = NcEmailAddress.ParseString ("henryk@nachocove.com");
@@ -58,6 +64,13 @@ namespace Test.Android
             InternetAddressList got4 = NcEmailAddress.ParseString ("\"Henry Kwok\" <henryk@nachocove.com>, \"Jeff Enderwick\" <jeffe@nachocove.com>, Steve Scalpone <steves@nachocove.com>");
             Compare (expected4, got4);
 
+            // We think this address is illegal
+            InternetAddressList got5 = NcEmailAddress.ParseString(@"""Amazon Web Services, Inc."" <no-reply-aws@amazon.com<mailto:no-reply-aws@amazon.com>>");
+            Assert.AreEqual (0, got5.Count);
+
+            // We this this address is illegal
+            InternetAddressList got6 = NcEmailAddress.ParseString(@"""Jeff Enderwick"" <jeffe@nachocove.com<mailto:jeffe@nachocove.com>>");
+            Assert.AreEqual (0, got5.Count);
         }
 
         private void Compare (MailboxAddress[] expected, InternetAddressList got)
@@ -69,36 +82,6 @@ namespace Test.Android
                 Assert.AreEqual (expected [n].Address, gotMailboxAddress.Address);
                 Assert.AreEqual (expected [n].Name, gotMailboxAddress.Name);
             }
-        }
-
-        string test_1 = @"
-From:
-To: ""\""Stephen Scalpone\"" <rascal2210@yahoo.com>""
-    <""Stephen Scalpone"" <rascal2210@yahoo.com>
-Date: Mon, 17 Mar 2014 22:18:57 +0000
-Subject: Re: Test message #1
-Message-ID: <635306663379746980.644.1@Stephens-iPhone>
-Content-Type: text/plain; charset=""utf-8""
-Content-Transfer-Encoding: quoted-printable
-MIME-Version: 1.0
-
-=0A=
-> Who flung pooh?=0A=
-> =0A=
-> Red letters getting bigger and bigger and wider=EF=BF=BD=
-";
-
-        [Test]
-        public void TestMimeParserBadAddress ()
-        {
-            MimeMessage mimeMsg;
-            using (var bodySource = new MemoryStream (Encoding.UTF8.GetBytes (test_1))) {
-                var bodyParser = new MimeParser (bodySource, MimeFormat.Default);
-                mimeMsg = bodyParser.ParseMessage ();
-            }  
-            var value1 = mimeMsg.To.ToString ();
-            var value2 = mimeMsg.To[0].Name;
-            var value3 = (mimeMsg.To[0] as MailboxAddress).Address;
         }
     }
 }
