875e6988ae625a9dd6e41594e911e2df17fd8d71
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-05-28 17:55:04 -0700

    pinger WIP

---
+++ NachoClient.iOS/PushAssist.cs
@@ -0,0 +1,43 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoClient.iOS
+{
+    public class PushAssist
+    {
+        private static volatile PushAssist instance;
+        private static object syncRoot = new Object ();
+
+        public static PushAssist Instance {
+            get {
+                if (instance == null) {
+                    lock (syncRoot) {
+                        if (instance == null) {
+                            instance = new PushAssist ();
+                        }
+                    }
+                }
+                return instance; 
+            }
+        }
+
+        private PushAssist ()
+        {
+        }
+
+        public void SetDeviceToken (byte[] deviceToken)
+        {
+
+        }
+
+        public void ResetDeviceToken ()
+        {
+        }
+
+        // Once we get device-token, we can do URL-A and get client-token.
+        // Once we have client-token, we are ready to use service.
+        // 
+    }
+}
+
