2ce4a664ddb498f924574d0b4e187e50b5f3579a
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-05-30 00:56:59 -0700

    coverted pending dependency from single to table to support multiplicity

---
+++ NachoClient.Android/NachoCore/Model/McPendDep.cs
@@ -0,0 +1,28 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Model
+{
+    public class McPendDep : McObject
+    {
+        public int PredId { get; set; }
+        public int SuccId { get; set; }
+
+        public McPendDep ()
+        {
+        }
+
+        public McPendDep (int predId, int succId)
+        {
+            PredId = predId;
+            SuccId = succId;
+        }
+
+        public static void DeleteAllSucc (int predId)
+        {
+            NcModel.Instance.Db.Execute ("DELETE FROM McPendDep WHERE PredId = ?", predId);
+        }
+    }
+}
+
---
+++ Test.Android/McPendDepTest.cs
@@ -0,0 +1,73 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NachoCore;
+using NachoCore.Utils;
+using NachoCore.Model;
+using NachoCore.ActiveSync;
+
+namespace Test.iOS
+{
+    [TestFixture]
+    public class McPendDepTest
+    {
+        public McPendDepTest ()
+        {
+            NcModel.Instance.Db = new TestDb ();
+        }
+
+        [Test]
+        public void DepThenIndep ()
+        {
+            List<int> depPendIdList = new List<int> ();
+            var createFolder = new McPending (1) {
+                Operation = McPending.Operations.FolderCreate,
+                ServerId = "parent",
+                ParentId = "0",
+                DisplayName = "Folder",
+                FolderCreate_Type = Xml.FolderHierarchy.TypeCode.UserCreatedContacts_14,
+            };
+            createFolder.Insert ();
+            for (int iter = 0; iter < 10; ++iter) {
+                var pending = new McPending (1) {
+                    Operation = McPending.Operations.ContactCreate,
+                    ItemId = iter,
+                    ParentId = "guid",
+                    ClientId = iter.ToString(),
+                };
+                pending.Insert ();
+                pending.MarkPredBlocked (createFolder.Id);
+                depPendIdList.Add (pending.Id);
+            }
+            // Verify all are blocked.
+            var suc = McPending.QuerySuccessors (1, createFolder.Id);
+            Assert.IsNotNull (suc);
+            Assert.True (10 == suc.Count);
+            foreach (var pid in depPendIdList) {
+                var pending = NcModel.Instance.Db.Get<McPending> (pid);
+                Assert.True (pending.State == McPending.StateEnum.PredBlocked);
+                var dep = McPending.QueryPredecessors (1, pending.Id);
+                Assert.IsNotNull (dep);
+                Assert.True (1 == dep.Count);
+                var pred = dep.First ();
+                Assert.True (createFolder.Id == pred.Id);
+            }
+            createFolder.UnblockSuccessors ();
+            // Verify all aren't blocked.
+            suc = McPending.QuerySuccessors (1, createFolder.Id);
+            Assert.IsNotNull (suc);
+            Assert.True (0 == suc.Count);
+            foreach (var pid in depPendIdList) {
+                var pending = NcModel.Instance.Db.Get<McPending> (pid);
+                Assert.True (pending.State == McPending.StateEnum.Eligible);
+                var dep = McPending.QueryPredecessors (1, pending.Id);
+                Assert.IsNotNull (dep);
+                Assert.True (0 == dep.Count);
+            }
+        }
+    }
+}
+
