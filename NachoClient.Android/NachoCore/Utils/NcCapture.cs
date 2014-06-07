54b84debfe908f139ba47d2ea443d2f3332e2ddf
Henry Kwok <henryk@nachocove.com>
2014-06-07 03:29:50 -0700

    Add pause and resume to NcCapture for app sleeping and waking up. Add unit tests.

--- NachoClient.Android/NachoCore/Utils/NcCapture.cs
+++ NachoClient.Android/NachoCore/Utils/NcCapture.cs
@@ -4,10 +4,57 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using NachoCore.Utils;
 
 namespace NachoCore.Utils
 {
-    public class NcCapture
+    // An interface for both the real and the mock version
+    public interface IStopwatch
+    {
+        long ElapsedMilliseconds { get; }
+
+        void Start ();
+
+        void Stop ();
+
+        void Reset ();
+    }
+
+    // Since System.Diagnostics.Stopwatch does not inherit from IStopwatch,
+    // we need to create a class that wraps Stopwatch and exports the 
+    // interface required by IStopwatch.
+    public class PlatformStopwatch : IStopwatch
+    {
+        private Stopwatch Watch;
+
+        public long ElapsedMilliseconds {
+            get {
+                return Watch.ElapsedMilliseconds;
+            }
+        }
+
+        public PlatformStopwatch ()
+        {
+            Watch = new Stopwatch ();
+        }
+
+        public void Start ()
+        {
+            Watch.Start ();
+        }
+
+        public void Stop ()
+        {
+            Watch.Stop ();
+        }
+
+        public void Reset ()
+        {
+            Watch.Reset ();
+        }
+    }
+
+    public class NcCapture : IDisposable
     {
         private class Summary
         {
@@ -96,42 +143,120 @@ namespace NachoCore.Utils
             }
         }
 
-        private string Kind;
-        private Stopwatch Watch;
+        private class NcCaptureKind
+        {
+            public Summary Summary;
+            public List<NcCapture> CaptureList;
 
-        private static Dictionary<string,Summary> PerKind;
+            public NcCaptureKind ()
+            {
+                Summary = new Summary (true);
+                CaptureList = new List<NcCapture> ();
+            }
+
+            public void Add (NcCapture capture)
+            {
+                CaptureList.Add(capture);
+            }
+
+            public void Remove (NcCapture capture)
+            {
+                bool result = CaptureList.Remove (capture);
+                NcAssert.True (result);
+            }
+        }
+
+        private string Kind;
+        private IStopwatch Watch;
+        // Note that we cannot use Stopwatch.IsRunning property because when going
+        // to background, we need to stop the capture but remember that it
+        // needs to restart when waking up.
+        private bool _IsRunning;
+        public bool IsRunning {
+            get {
+                return _IsRunning;
+            }
+        }
+        private static object ClassLockObj;
+        private static Dictionary<string, NcCaptureKind> PerKind;
+        public static Type StopwatchClass = typeof(PlatformStopwatch);
 
         private NcCapture (string kind)
         {
-            Kind = kind;
-            Watch = new Stopwatch ();
+            NcAssert.True (null != ClassLockObj);
+            lock (ClassLockObj) {
+                NcAssert.True (PerKind.ContainsKey (kind));
+
+                Kind = kind;
+                _IsRunning = false;
+                Watch = (IStopwatch)Activator.CreateInstance (StopwatchClass);
+
+                // Add to the global tracking list
+                NcAssert.True (PerKind.ContainsKey (kind));
+                PerKind [kind].Add (this);
+            }
+        }
+
+        public void Dispose ()
+        {
+            lock (ClassLockObj) {
+                // Remove self from the global tracking list
+                PerKind [Kind].Remove (this);
+            }
         }
 
         public static bool AddKind (string kind)
         {
-            if (null == PerKind) {
-                PerKind = new Dictionary<string, Summary> ();
+            if (null == ClassLockObj) {
+                ClassLockObj = new object ();
             }
-            if (PerKind.ContainsKey (kind)) {
-                return false;
-            } else {
-                PerKind.Add (kind, new Summary (true));
+            lock (ClassLockObj) {
+                if (null == PerKind) {
+                    PerKind = new Dictionary<string, NcCaptureKind> ();
+                }
+                if (PerKind.ContainsKey (kind)) {
+                    return false;
+                } else {
+                    PerKind.Add (kind, new NcCaptureKind ());
+                    return true;
+                }
+            }
+        }
+
+        // Remove a kind. This is only for testing purpose. Do not use this in
+        // production code! The ramification of allowing removal of kind dynamically
+        // is that we need to lock up critical sections when summarizing and
+        // reporting a Summary since now it may disappear during its being read.
+        public static bool RemoveKind(string kind)
+        {
+            lock (ClassLockObj) {
+                if (!PerKind.ContainsKey (kind)) {
+                    return false;
+                }
+                NcCaptureKind captureKind = PerKind [kind];
+                if (0 < captureKind.CaptureList.Count) {
+                    return false;
+                }
+                PerKind.Remove (kind);
                 return true;
             }
         }
 
         public static string Summarize (string kind)
         {
-            return string.Format ("Kind: {0}", kind) + PerKind [kind].ToString ();
+            // Note that we don't take ClassLockObj here because there is no
+            // way to delete a kind.
+            return string.Format ("[Kind: {0}] ", kind) + PerKind [kind].Summary.ToString ();
         }
 
         public static string Summarize ()
         {
             StringBuilder sb = new StringBuilder();
-            foreach (var key in PerKind.Keys) {
-                sb.Append (string.Format ("Kind: {0}", key));
-                sb.Append (PerKind [key].ToString ());
-                sb.Append (Environment.NewLine);
+            lock (ClassLockObj) {
+                foreach (var key in PerKind.Keys) {
+                    sb.Append (Summarize (key));
+                    sb.Append (Environment.NewLine);
+                }
             }
             return sb.ToString ();
         }
@@ -151,6 +276,21 @@ namespace NachoCore.Utils
         public void Start ()
         {
             Watch.Start ();
+            _IsRunning = true;
+        }
+
+        public void Pause ()
+        {
+            if (IsRunning) {
+                Watch.Stop ();
+            }
+        }
+
+        public void Resume ()
+        {
+            if (IsRunning) {
+                Watch.Start ();
+            }
         }
 
         public void Stop ()
@@ -161,14 +301,15 @@ namespace NachoCore.Utils
         public void Stop (Dictionary<string,int> xtra)
         {
             Watch.Stop ();
-            var summary = PerKind [Kind];
+            var summary = PerKind [Kind].Summary;
             summary.Update ((uint)Watch.ElapsedMilliseconds);
-
+            _IsRunning = false;
         }
 
         public void Reset ()
         {
             Watch.Reset ();
+            _IsRunning = false;
         }
 
         public static void Report (string Kind)
@@ -176,14 +317,61 @@ namespace NachoCore.Utils
             if (!PerKind.ContainsKey(Kind)) {
                 return;
             }
-            Summary summary = PerKind [Kind];
+            Summary summary = PerKind [Kind].Summary;
             summary.Report (Kind);
         }
 
         public static void Report ()
         {
-            foreach (string kind in PerKind.Keys) {
-                Report (kind);
+            lock (ClassLockObj) {
+                foreach (string kind in PerKind.Keys) {
+                    Report (kind);
+                }
+            }
+        }
+
+        delegate void CaptureListWalker (NcCapture capture);
+
+        private static void WalkCaptureList (string Kind, CaptureListWalker walker)
+        {
+            lock (ClassLockObj) {
+                if (!PerKind.ContainsKey (Kind)) {
+                    return;
+                }
+                foreach (NcCapture capture in PerKind [Kind].CaptureList) {
+                    walker (capture);
+                }
+            }
+        }
+        public static void PauseKind (string Kind)
+        {
+            WalkCaptureList (Kind, cap => {
+                cap.Pause ();
+            });
+        }
+
+        public static void ResumeKind (string Kind)
+        {
+            WalkCaptureList (Kind, cap => {
+                cap.Resume ();
+            });
+        }
+
+        public static void ResumeAll ()
+        {
+            lock (ClassLockObj) {
+                foreach (string kind in PerKind.Keys) {
+                    ResumeKind (kind);
+                }
+            }
+        }
+
+        public static void PauseAll ()
+        {
+            lock (ClassLockObj) {
+                foreach (string kind in PerKind.Keys) {
+                    PauseKind (kind);
+                }
             }
         }
     }
