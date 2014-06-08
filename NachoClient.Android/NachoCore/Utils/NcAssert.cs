c5b7594d901e0e11acf294a998134e02d06fa14c
Henry Kwok <henryk@nachocove.com>
2014-06-08 15:50:58 -0700

    Make NcAssert error log more descriptive. Identify the type of assertion and include caller info.

--- NachoClient.Android/NachoCore/Utils/NcAssert.cs
+++ NachoClient.Android/NachoCore/Utils/NcAssert.cs
@@ -1,6 +1,8 @@
 //  Copyright (C) 2013 Nacho Cove, Inc. All rights reserved.
 //
 using System;
+using System.Runtime.CompilerServices;
+using System.IO;
 
 namespace NachoCore.Utils
 {
@@ -9,15 +11,27 @@ namespace NachoCore.Utils
         // All subsequent assertion exceptions must be derived of NachoAssertionFailure.
         public class NachoAssertionFailure : Exception
         {
-            public NachoAssertionFailure (string message) : base (message)
+            public NachoAssertionFailure (string message, string sourceFile,
+                int lineNumber, string member) : base (message)
             {
-                Log.Error (Log.LOG_ASSERT, message);
+                string callerInfo = "";
+                if ((0 < sourceFile.Length) && (0 < lineNumber)) {
+                    callerInfo = String.Format("{0},{1}: ", Path.GetFileName(sourceFile), lineNumber);
+                }
+                if (0 < member.Length) {
+                    callerInfo += member + "()";
+                }
+                if (0 < callerInfo.Length) {
+                    callerInfo = " (" + callerInfo + ")";
+                }
+                Log.Error (Log.LOG_ASSERT, "{0}{1}", message, callerInfo);
             }
         }
 
         public class NachoDefaultCaseFailure : NachoAssertionFailure
         {
-            public NachoDefaultCaseFailure (string message) : base (message)
+            public NachoDefaultCaseFailure (string message, string sourceFile,
+                int lineNumber, string member) : base (message, sourceFile, lineNumber, member)
             {
             }
         }
@@ -26,42 +40,40 @@ namespace NachoCore.Utils
         {
         }
 
-        public static void True (bool b)
+        public static void True (bool b, string message = "",
+            // You don't need to fill in these parameters, they will be
+            // automatically assigned.
+            [CallerFilePath] string sourceFile = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string member = "")
         {
             if (!b) {
-                throw new NachoAssertionFailure ("No message");
+                throw new NachoAssertionFailure ("NcAssert.True: " + message, sourceFile, lineNumber, member);
             }
         }
 
-        public static void True (bool b, string message)
-        {
-            if (!b) {
-                throw new NachoAssertionFailure (message);
-            }
-        }
-
-        public static void NotNull (Object o)
+        public static void NotNull (Object o, string message = "",
+            // You don't need to fill in these parameters, they will be
+            // automatically assigned.
+            [CallerFilePath] string sourceFile = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string member = "")
         {
             if (null == o) {
-                throw new NachoAssertionFailure ("No message");
+                throw new NachoAssertionFailure ("NcAssert.NotNull: " + message,
+                    sourceFile, lineNumber, member);
             }
         }
 
-        public static void NotNull (Object o, string message)
+        public static void CaseError (string message = "",
+            // you don't need to fill in these parameters, they will be
+            // automatically assigned.
+            [CallerFilePath] string sourceFile = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string member = "")
         {
-            if (null == o) {
-                throw new NachoAssertionFailure (message);
-            }
-        }
-
-        public static void CaseError ()
-        {
-            throw new NachoDefaultCaseFailure ("No message");
-        }
-
-        public static void CaseError (string message)
-        {
-            throw new NachoDefaultCaseFailure (message);
+            throw new NachoDefaultCaseFailure ("NcAssert.CaseError: " + message,
+                sourceFile, lineNumber, member);
         }
     }
 }
