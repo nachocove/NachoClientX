a7f71a3215e8290cc5971a3ec86490c4a279caec
Henry Kwok <henryk@nachocove.com>
2014-06-12 11:35:27 -0700

    Capture timing statistics for db operations.

--- NachoClient.Android/NachoCore/Model/McObject.cs
+++ NachoClient.Android/NachoCore/Model/McObject.cs
@@ -1,5 +1,7 @@
 using SQLite;
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
 using System.Linq;
 using System.Reflection;
 using NachoCore.Utils;
@@ -9,6 +11,67 @@ namespace NachoCore.Model
     // If SQLite.Net would tolerate an abstract class, we'd be one.
     public class McObject
     {
+        /// OperationCaptures holds a set of NcCapture for one type of operations 
+        /// (Insert, Delete, or Update) for all tables. There is one NcCapture
+        /// per thread per table. It is per thread because NcCapture is not
+        /// thread-safe and I don't want to serialize all db operations.
+        private class OperationCaptures
+        {
+            public struct CaptureKey
+            {
+                public int threadId;
+                public string className;
+
+                public CaptureKey (int threadId_, string className_)
+                {
+                    threadId = threadId_;
+                    className = className_;
+                }
+
+                public static CaptureKey FromClassName (string className)
+                {
+                    int threadId = Thread.CurrentThread.ManagedThreadId;
+                    return new CaptureKey (threadId, className);
+                }
+            }
+
+            ConcurrentDictionary<CaptureKey, NcCapture> Captures;
+            string OpName;
+
+            public OperationCaptures (string opName)
+            {
+                Captures = new ConcurrentDictionary<CaptureKey, NcCapture> ();
+                OpName = opName;
+            }
+
+            private string CaptureName (string className)
+            {
+                return "McModel." + className + "." + OpName;
+            }
+
+            public void Add (string className)
+            {
+                CaptureKey key = CaptureKey.FromClassName (className);
+                if (!Captures.ContainsKey (key)) {
+                    string kind = CaptureName (className);
+                    NcCapture.AddKind (kind);
+                    Captures.TryAdd (key, NcCapture.Create (kind));
+                }
+            }
+
+            public NcCapture Find (string className)
+            {
+                CaptureKey key = CaptureKey.FromClassName (className);
+                Add (className); // Add() will check if the key already exists
+                NcAssert.True (Captures.ContainsKey (key));
+                return Captures [key];
+            }
+        }
+
+        private static OperationCaptures InsertCaptures;
+        private static OperationCaptures DeleteCaptures;
+        private static OperationCaptures UpdateCaptures;
+
         [PrimaryKey, AutoIncrement, Unique]
         public int Id { get; set; }
         // Optimistic concurrency control
@@ -18,6 +81,20 @@ namespace NachoCore.Model
         {
             Id = 0;
             LastModified = DateTime.MinValue;
+
+            string className = ClassName ();
+            if (null == InsertCaptures) {
+                InsertCaptures = new OperationCaptures ("Insert");
+                InsertCaptures.Add (className);
+            }
+            if (null == DeleteCaptures) {
+                DeleteCaptures = new OperationCaptures ("Delete");
+                DeleteCaptures.Add (className);
+            }
+            if (null == UpdateCaptures) {
+                UpdateCaptures = new OperationCaptures ("Update");
+                UpdateCaptures.Add (className);
+            }
         }
 
         public string ClassName ()
@@ -28,19 +105,34 @@ namespace NachoCore.Model
         public virtual int Insert ()
         {
             NcAssert.True (0 == Id);
-            return NcModel.Instance.Db.Insert (this);
+            NcCapture capture = InsertCaptures.Find (ClassName ());
+            capture.Start ();
+            int rc =  NcModel.Instance.Db.Insert (this);
+            capture.Stop ();
+            capture.Reset ();
+            return rc;
         }
 
         public virtual int Delete ()
         {
             NcAssert.True (0 != Id);
-            return NcModel.Instance.Db.Delete (this);
+            NcCapture capture = DeleteCaptures.Find (ClassName ());
+            capture.Start ();
+            int rc = NcModel.Instance.Db.Delete (this);
+            capture.Stop ();
+            capture.Reset ();
+            return rc;
         }
 
         public virtual int Update ()
         {
             NcAssert.True (0 != Id);
-            return NcModel.Instance.Db.Update (this);
+            NcCapture capture = UpdateCaptures.Find (ClassName ());
+            capture.Start ();
+            int rc = NcModel.Instance.Db.Update (this);
+            capture.Stop ();
+            capture.Reset ();
+            return rc;
         }
 
         public static T QueryById<T> (int id) where T : McObject, new()
