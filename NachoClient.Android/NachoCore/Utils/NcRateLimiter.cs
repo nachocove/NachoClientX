a8f3a5e105ad0fea73bc16eb5934ee50b5f68bc8
Jeff Enderwick <jeff.enderwick@gmail.com>
2014-06-18 13:34:57 -0700

    add rate limiter

---
+++ NachoClient.Android/NachoCore/Utils/NcRateLimiter.cs
@@ -0,0 +1,45 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Utils
+{
+    public class NcRateLimter
+    {
+        public NcRateLimter (double nPerSecond, double refreshSeconds)
+        {
+            Allowance = (int)(nPerSecond * refreshSeconds);
+            NcAssert.True (0 < Allowance);
+            RefreshMsecs = (int)(refreshSeconds * 1000.0);
+            NcAssert.True (0 < RefreshMsecs);
+            Refresh ();
+        }
+
+        public bool Enabled { set; get; }
+        private int RefreshMsecs;
+        private int Allowance;
+        private DateTime LastRefresh;
+        private int Remaining;
+
+        private void Refresh ()
+        {
+            LastRefresh = DateTime.UtcNow;
+            Remaining = Allowance;
+        }
+
+        public bool TakeToken ()
+        {
+            if (!Enabled) {
+                return true;
+            }
+            if (LastRefresh.AddMilliseconds(RefreshMsecs) < DateTime.UtcNow) {
+                Refresh ();
+            }
+            if (0 < Remaining) {
+                --Remaining;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Test.Android/NcRateLimiterTest.cs
@@ -0,0 +1,45 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using NUnit.Framework;
+using NachoCore.Utils;
+
+namespace Test.Common
+{
+    public class NcRateLimiterTest : NcTestBase
+    {
+        [Test]
+        public void Simple ()
+        {
+            var rl = new NcRateLimter (1.0, 1.0);
+            Assert.NotNull (rl);
+            for (var i = 0; i < 100; i++) {
+                Assert.True (rl.TakeToken ());
+            }
+            rl.Enabled = true;
+            NcAssert.True (rl.TakeToken ());
+            NcAssert.True (!rl.TakeToken ());
+        }
+
+        [Test]
+        public void MoreThanOne ()
+        {
+            var rl = new NcRateLimter (100.0, 0.25);
+            rl.Enabled = true;
+            Assert.NotNull (rl);
+            var WaitFor = DateTime.UtcNow.AddMilliseconds (300);
+            for (var i = 0; i < 25; i++) {
+                Assert.True (rl.TakeToken ());
+            }
+            Assert.True (!rl.TakeToken ());
+            while (DateTime.UtcNow < WaitFor) {
+                ;
+            }
+            for (var i = 0; i < 25; i++) {
+                Assert.True (rl.TakeToken ());
+            }
+            Assert.True (!rl.TakeToken ());
+        }
+    }
+}
+
