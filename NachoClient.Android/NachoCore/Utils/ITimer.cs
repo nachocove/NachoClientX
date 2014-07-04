bcedd4083ac76cb68a15b7d449c1ec12a9c6d6a5
Henry Kwok <henryk@nachocove.com>
2014-07-04 03:34:26 -0700

    Add timer interface and a platform timer class. This indirection allows timer to be mocked in unit tests.
    This is a re-commit of f2693b6f4b3fb8935764edaed07fb5aa37066087 in pausable_timer branch.

---
+++ NachoClient.Android/NachoCore/Utils/ITimer.cs
@@ -0,0 +1,18 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Utils
+{
+    public interface ITimer : IDisposable
+    {
+        bool Change (Int32 due, Int32 period);
+
+        bool Change (Int64 due, Int64 period);
+
+        bool Change (TimeSpan due, TimeSpan period);
+
+        bool Change (UInt32 due, UInt32 period);
+    }
+}
+
