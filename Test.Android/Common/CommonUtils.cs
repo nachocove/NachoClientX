18d8a3a0f1521819b4be9f8752bf8b954dbe44ee
Aaron Smith <asmith1810@gmail.com>
2014-07-07 17:10:54 -0700

    Interface for AsStrategy so it can be mocked out for confrez tests.

--- Test.Android/Common/CommonUtils.cs
+++ Test.Android/Common/CommonUtils.cs
@@ -42,6 +42,7 @@ namespace Test.iOS
 
             MockOwner mockOwner = new MockOwner ();
             AsProtoControl protoControl = new AsProtoControl (mockOwner, accountId);
+            protoControl.SyncStrategy = new MockStrategy ();
 
             return protoControl;
         }
