ed4d25675907dc6a0ee6da77af09004369a9b9f6
Steve Scalpone <sscalpone@gmail.com>
2014-07-17 17:29:42 -0700

    Forestall csproj merge errors.  Checking in unused new packages.

---
+++ NachoClient.Android/NachoCore/Model/McEmailAddress.cs
@@ -0,0 +1,40 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using SQLite;
+using MimeKit;
+
+namespace NachoCore.Model
+{
+    public class McEmailAddress : McObjectPerAccount
+    {
+        public McEmailAddress ()
+        {
+        }
+
+        [Indexed, Unique]
+        public string CanonicalEmailAddress { get; set; }
+
+        public string DisplayEmailAddress { get; set; }
+
+        [Indexed] // pre-computed for fast search
+        public string DisplayFirstName { get; set; }
+
+        [Indexed] // pre-computed for fast search
+        public string DisplayLastName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        [Indexed]
+        public int Score { get; set; }
+
+        public bool IsHot { get; set; }
+
+        public bool IsVip { get; set; }
+
+        public bool IsBlacklisted { get; set; }
+
+
+    }
+}
+
---
+++ NachoClient.Android/NachoCore/Model/McMapContactEmail.cs
@@ -0,0 +1,36 @@
+﻿//  Copyright (C) 2014 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using SQLite;
+
+namespace NachoCore.Model
+{
+    public class McMapContactEmail : McObjectPerAccount
+    {
+        public McMapContactEmail ()
+        {
+
+        }
+
+        // Contact sources
+        public enum ContactSource
+        {
+            EAS = 1,
+            GAL = 2,
+            RIC = 3,
+            Gleaner = 4,
+            Device = 10,
+        };
+
+        // Source of contact, affects the interpretation of ContactIndex
+        public ContactSource Source { get; set; }
+
+
+        [Indexed] // pointer to the McEmailAddress
+        public int EmailAddressIndex { get; set; }
+
+        [Indexed] // contact-type specific point; a McContact index for EAS, GAL, and Gleaned contacts
+        public int ContactIndex { get; set; }
+    }
+}
+
