4742095a9e9942deb95abc6716d5b7578d66a96b
Henry Kwok <henryk@nachocove.com>
2014-07-29 10:44:14 -0700

    Fix a typo of TimeZone tag.

--- NachoClient.Android/NachoCore/Utils/ASWBXML.cs
+++ NachoClient.Android/NachoCore/Utils/ASWBXML.cs
@@ -193,7 +193,7 @@ namespace NachoCore.Wbxml
             codePages [2].AddToken (0x30, "MonthOfYear");
             codePages [2].AddToken (0x31, "StartTime");
             codePages [2].AddToken (0x32, "Sensitivity");
-            codePages [2].AddToken (0x33, "Timezone");
+            codePages [2].AddToken (0x33, "TimeZone");
             codePages [2].AddToken (0x34, "GlobalObjId");
             codePages [2].AddToken (0x35, "ThreadTopic");
             codePages [2].AddToken (0x39, "InternetCPID");
