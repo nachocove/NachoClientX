e0b28ce57cefae539e23869f34e38039820b9dbb
Cole Britton <coleb@nachocove.com>
2014-08-05 11:53:49 -0700

    Redid files Hierarchy view to match style

--- NachoClient.iOS/NachoUI.iOS/FilesHierarchyViewController.cs
+++ NachoClient.iOS/NachoUI.iOS/FilesHierarchyViewController.cs
@@ -6,23 +6,28 @@ using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using NachoCore.Utils;
 using SWRevealViewControllerBinding;
+using System.Drawing;
 
 namespace NachoClient.iOS
 {
-	public partial class FilesHierarchyViewController : NcUITableViewController
-	{
+    public partial class FilesHierarchyViewController : NcUIViewController
+    {
         INachoFileChooserParent owner;
         FilesViewController.ItemType itemType;
 
-        // cell id's
-        string BasicCell = "BasicCell";
+        UIColor separatorColor = A.Color_NachoSeparator;
+        protected static float SCREEN_WIDTH = UIScreen.MainScreen.Bounds.Width;
+        protected int LINE_OFFSET = 30;
+        protected int CELL_HEIGHT = 44;
+        protected float TEXT_LINE_HEIGHT = 19.124f;
+        protected UIColor solidTextColor = A.Color_NachoBlack;
 
         // segue id's
         string FilesHierarchyToFiles = "FilesHierarchyToFiles";
 
-		public FilesHierarchyViewController (IntPtr handle) : base (handle)
-		{
-		}
+        public FilesHierarchyViewController (IntPtr handle) : base (handle)
+        {
+        }
 
         /// <summary>
         /// INachoFileChooser delegate
@@ -43,8 +48,9 @@ namespace NachoClient.iOS
             if (owner == null) {
                 NavigationItem.LeftBarButtonItems = new UIBarButtonItem[] { revealButton };
             }
+               
 
-            this.TableView.TableFooterView = new UIView (new System.Drawing.RectangleF (0, 0, 0, 0));
+            FilesHierarchyView ();
         }
 
         public override void ViewWillAppear (bool animated)
@@ -55,67 +61,137 @@ namespace NachoClient.iOS
             }
         }
 
-        public override int NumberOfSections (UITableView tableView)
+        //        public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+        //        {
+        //            UITableViewCell cell = null;
+        //            cell = tableView.DequeueReusableCell (BasicCell);
+        //            if (cell == null) {
+        //                cell = new UITableViewCell (UITableViewCellStyle.Default, BasicCell);
+        //            }
+        //            NcAssert.True (null != cell);
+        //
+        //            switch (indexPath.Row) {
+        //            case 0:
+        //                cell.TextLabel.Text = "Attachments";
+        //                break;
+        //            case 1:
+        //                cell.TextLabel.Text = "Shared Documents";
+        //                break;
+        //            case 2:
+        //                cell.TextLabel.Text = "Notes";
+        //                break;
+        //            }
+        //
+        //            // styling
+        //            cell.TextLabel.TextColor = A.Color_NachoBlack;
+        //            cell.TextLabel.Font = A.Font_AvenirNextRegular14;
+        //
+        //            return cell;
+        //        }
+
+        const int FILES_ATTACHMENT_DETAIL_TAG = 100;
+        const int FILES_SHARED_FILE_DETAIL_TAG = 101;
+        const int FILES_NOTES_DETAIL_TAG = 102;
+
+        public void FilesHierarchyView ()
         {
-            return 1;
-        }
+            scrollView.Frame = new RectangleF (0, 0, SCREEN_WIDTH, View.Frame.Height);
 
-        public override int RowsInSection (UITableView tableview, int section)
-        {
-            return 3;
-        }
+            // Attechment Cell
+            UIView filesAttachmentsView = new UIView (new RectangleF (0, LINE_OFFSET, SCREEN_WIDTH, CELL_HEIGHT));
+            filesAttachmentsView.BackgroundColor = UIColor.White;
 
-        public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
-        {
-            UITableViewCell cell = null;
-            cell = tableView.DequeueReusableCell (BasicCell);
-            if (cell == null) {
-            cell = new UITableViewCell (UITableViewCellStyle.Default, BasicCell);
-            }
-            NcAssert.True (null != cell);
+            UIImageView attachmentAccessoryImage = new UIImageView (new RectangleF (SCREEN_WIDTH - 23, 14, 10, 16));
+            attachmentAccessoryImage.Image = Util.MakeArrow (A.Color_NachoBlue);
+            filesAttachmentsView.AddSubview (attachmentAccessoryImage);
 
-            switch (indexPath.Row) {
-            case 0:
-                cell.TextLabel.Text = "Attachments";
-                break;
-            case 1:
-                cell.TextLabel.Text = "Shared Documents";
-                break;
-            case 2:
-                cell.TextLabel.Text = "Notes";
-                break;
-            }
+            AddTextLabelWithImage (40, 12.438f, 100, TEXT_LINE_HEIGHT, "Attachments", UIImage.FromBundle ("icn-mtng-attachment"), 14.5f, filesAttachmentsView);
 
-            // styling
-            cell.TextLabel.TextColor = A.Color_NachoBlack;
-            cell.TextLabel.Font = A.Font_AvenirNextRegular14;
-
-            return cell;
-        }
-
-        public override void RowSelected (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
-        {
-            SegueHolder holder = new SegueHolder (null);
-            if (owner != null) {
-                holder = new SegueHolder (owner);
-            }
-
-            switch (indexPath.Row) {
-            case 0:
+            var attachmentTap = new UITapGestureRecognizer ();
+            attachmentTap.AddTarget (() => {
                 itemType = FilesViewController.ItemType.Attachment;
-                break;
-            case 1:
+                //PerformSegue ("EventToAttachment", this);
+            });
+            filesAttachmentsView.AddGestureRecognizer (attachmentTap);
+
+            // Notes Cell
+            UIView filesSharedFilesView = new UIView (new RectangleF (0, LINE_OFFSET + CELL_HEIGHT, SCREEN_WIDTH, CELL_HEIGHT));
+            filesSharedFilesView.BackgroundColor = UIColor.White;
+
+            UIImageView sharedFilesAccessoryImage = new UIImageView (new RectangleF (SCREEN_WIDTH - 23, 14, 10, 16));
+            sharedFilesAccessoryImage.Image = Util.MakeArrow (A.Color_NachoBlue);
+            filesSharedFilesView.AddSubview (sharedFilesAccessoryImage);
+
+            AddTextLabelWithImage (40, 12.438f, 100, TEXT_LINE_HEIGHT, "Shared Files", UIImage.FromBundle ("icn-mtng-attachment"), 14.5f, filesSharedFilesView);
+
+            var sharedFilesTap = new UITapGestureRecognizer ();
+            sharedFilesTap.AddTarget (() => {
                 itemType = FilesViewController.ItemType.Document;
-                break;
-            case 2:
+                //PerformSegue ("EventToAttachment", this);
+            });
+            filesSharedFilesView.AddGestureRecognizer (sharedFilesTap);
+
+            // Shared Files Cell
+            UIView filesNotesView = new UIView (new RectangleF (0, LINE_OFFSET + CELL_HEIGHT * 2, SCREEN_WIDTH, CELL_HEIGHT));
+            filesNotesView.BackgroundColor = UIColor.White;
+
+            UIImageView noteAccessoryImage = new UIImageView (new RectangleF (SCREEN_WIDTH - 23, 14, 10, 16));
+            noteAccessoryImage.Image = Util.MakeArrow (A.Color_NachoBlue);
+            filesNotesView.AddSubview (noteAccessoryImage);
+
+            AddTextLabelWithImage (40, 12.438f, 100, TEXT_LINE_HEIGHT, "Notes", UIImage.FromBundle ("icn-mtng-attachment"), 14.5f, filesNotesView);
+
+            var noteTap = new UITapGestureRecognizer ();
+            noteTap.AddTarget (() => {
                 itemType = FilesViewController.ItemType.Note;
-                break;
-            }
+                //PerformSegue ("EventToAttachment", this);
+            });
+            filesNotesView.AddGestureRecognizer (noteTap);
 
-            PerformSegue (FilesHierarchyToFiles, holder); // holder can be filled with owner or null
+            //Content View
+            contentView.AddSubviews (new UIView[] {
+                filesAttachmentsView,
+                filesNotesView,
+                filesSharedFilesView
+            }); 
 
-            tableView.DeselectRow (indexPath, true);
+            AddLine (0, LINE_OFFSET, SCREEN_WIDTH, separatorColor, contentView);
+            AddLine (15, LINE_OFFSET + CELL_HEIGHT, SCREEN_WIDTH, separatorColor, contentView);
+            AddLine (15, LINE_OFFSET + CELL_HEIGHT * 2, SCREEN_WIDTH, separatorColor, contentView);
+            AddLine (0, LINE_OFFSET + CELL_HEIGHT * 3, SCREEN_WIDTH, separatorColor, contentView);
+
+            contentView.Frame = new RectangleF (0, 0, SCREEN_WIDTH, View.Frame.Height);
+            contentView.BackgroundColor = A.Color_NachoNowBackground;
+
+            //Scroll View
+            scrollView.BackgroundColor = A.Color_NachoNowBackground;
+            scrollView.ContentSize = new SizeF (SCREEN_WIDTH, contentView.Frame.Height);
+
         }
+
+        //        public override void RowSelected (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
+        //        {
+        //            SegueHolder holder = new SegueHolder (null);
+        //            if (owner != null) {
+        //                holder = new SegueHolder (owner);
+        //            }
+        //
+        //            switch (indexPath.Row) {
+        //            case 0:
+        //                itemType = FilesViewController.ItemType.Attachment;
+        //                break;
+        //            case 1:
+        //                itemType = FilesViewController.ItemType.Document;
+        //                break;
+        //            case 2:
+        //                itemType = FilesViewController.ItemType.Note;
+        //                break;
+        //            }
+        //
+        //            PerformSegue (FilesHierarchyToFiles, holder); // holder can be filled with owner or null
+        //
+        //            tableView.DeselectRow (indexPath, true);
+        //        }
 
         public override void PrepareForSegue (UIStoryboardSegue segue, NSObject sender)
         {
@@ -133,9 +209,25 @@ namespace NachoClient.iOS
             }
         }
 
-        public override float GetHeightForRow (UITableView tableView, NSIndexPath indexPath)
+
+        public void AddTextLabelWithImage (float xOffset, float yOffset, float width, float height, string text, UIImage image, float imageOffset, UIView parentView)
         {
-            return 50.0f;
+            var textLabel = new UILabel (new RectangleF (xOffset, yOffset, width, height));
+            textLabel.Text = text;
+            textLabel.Font = A.Font_AvenirNextRegular14;
+            textLabel.TextColor = solidTextColor;
+            parentView.AddSubview (textLabel);
+
+            UIImageView theImage = new UIImageView (new RectangleF ((xOffset - 25), imageOffset, 15, 15));
+            theImage.Image = image;
+            parentView.Add (theImage);
         }
-	}
+
+        public void AddLine (float offset, float yVal, float width, UIColor color, UIView parentView)
+        {
+            var lineUIView = new UIView (new RectangleF (offset, yVal, width, .5f));
+            lineUIView.BackgroundColor = color;
+            parentView.Add (lineUIView);
+        }
+    }
 }
