53fb7ebfc513c50ee7b3a280a908701b3e890d0b
Henry Kwok <henryk@nachocove.com>
2014-09-03 11:38:34 -0700

    Create a McFolder and uses .Link() to create map. Add negative cases to verify rejection.

--- Test.Android/McEmailMessageTest.cs
+++ Test.Android/McEmailMessageTest.cs
@@ -11,8 +11,20 @@ namespace Test.Common
     [TestFixture]
     public class McEmailMessageTest : NcTestBase
     {
+        McFolder Folder;
+
+        // Lazy. Going to fudge an account (instead of creating one)
         private const int defaultAccountId = 1;
-        private const int defaultFolderId = 2;
+
+        [SetUp]
+        public new void Setup ()
+        {
+            base.Setup ();
+            Folder = McFolder.Create (defaultAccountId, false, false, "0", "test", "Test Folder",
+                NachoCore.ActiveSync.Xml.FolderHierarchy.TypeCode.UserCreatedGeneric_1);
+            Folder.Insert ();
+            NcAssert.True (0 < Folder.Id);
+        }
 
         private McEmailAddress SetupAddress (string canonicalAddress, int received, int read, bool isVip)
         {
@@ -48,17 +60,6 @@ namespace Test.Common
             return message;
         }
 
-        private void SetupFolderMap (McEmailMessage message)
-        {
-            McMapFolderFolderEntry map = new McMapFolderFolderEntry (defaultAccountId);
-            map.FolderId = defaultFolderId;
-            map.FolderEntryId = message.Id;
-            map.ClassCode = McAbstrFolderEntry.ClassCodeEnum.Email;
-
-            map.Insert ();
-            NcAssert.True (0 < map.Id);
-        }
-
         public void SetupDependency (McEmailMessage message, McEmailAddress address)
         {
             McEmailMessageDependency dep = new McEmailMessageDependency ();
@@ -81,18 +82,21 @@ namespace Test.Common
         [Test]
         public void QueryActiveMessageItemsByScore ()
         {
-            // Create 6 email messages in the folder. 2 emails have scores over 0.5.
+            // Create 8 email messages in the folder. 2 emails have scores over 0.5.
             // 1 email is from VIP. 1 email is from VIP and has score over 0.5. 2
-            // emails have scores below 0.5.
-            McEmailAddress[] addresses = new McEmailAddress[6];
-            addresses[0] = SetupAddress ("aaron@company.net", 4, 2, false);
-            addresses[1] = SetupAddress ("bob@company.net", 5, 1, false);
-            addresses[2] = SetupAddress ("charles@compnay.net", 3, 3, false);
-            addresses[3] = SetupAddress ("david@company.net", 2, 0, false);
-            addresses[4] = SetupAddress ("ellen@company.net", 5, 3, true);
-            addresses[5] = SetupAddress ("fred@company.net", 3, 1, true);
+            // emails have scores below 0.5. 1 email is delete pending. 1 email
+            // has a start date in the future.
+            McEmailAddress[] addresses = new McEmailAddress[8];
+            addresses [0] = SetupAddress ("aaron@company.net", 4, 2, false);
+            addresses [1] = SetupAddress ("bob@company.net", 5, 1, false);
+            addresses [2] = SetupAddress ("charles@compnay.net", 3, 3, false);
+            addresses [3] = SetupAddress ("david@company.net", 2, 0, false);
+            addresses [4] = SetupAddress ("ellen@company.net", 5, 3, true);
+            addresses [5] = SetupAddress ("fred@company.net", 3, 1, true);
+            addresses [6] = SetupAddress ("gary@company.net", 6, 6, false);
+            addresses [7] = SetupAddress ("harry@company.net", 7, 7, false);
 
-            McEmailMessage[] messages = new McEmailMessage[6];
+            McEmailMessage[] messages = new McEmailMessage[8];
             messages [0] = SetupMessage (addresses [0], new DateTime (2014, 8, 15, 1, 23, 0));
             messages [1] = SetupMessage (addresses [1], new DateTime (2014, 8, 15, 2, 0, 0));
             // Intentional set this one received date earlier to test "SORT BY DateReceived"
@@ -100,17 +104,21 @@ namespace Test.Common
             messages [3] = SetupMessage (addresses [3], new DateTime (2014, 8, 15, 3, 0, 0));
             messages [4] = SetupMessage (addresses [4], new DateTime (2014, 8, 15, 4, 0, 0));
             messages [5] = SetupMessage (addresses [5], new DateTime (2014, 8, 15, 5, 0, 0));
+            messages [6] = SetupMessage (addresses [6], new DateTime (2014, 8, 15, 6, 0, 0));
+            messages [6].IsAwaitingDelete = true;
+            messages [7] = SetupMessage (addresses [7], new DateTime (2014, 8, 15, 7, 0, 0));
+            messages [7].FlagUtcStartDate = DateTime.MaxValue;
 
             for (int n = 0; n < 6; n++) {
-                SetupFolderMap (messages [n]);
+                Folder.Link (messages [n]);
                 SetupDependency (messages [n], addresses [n]);
             }
 
             List<NcEmailMessageIndex> messageList =
-                McEmailMessage.QueryActiveMessageItemsByScore (defaultAccountId, defaultFolderId, 0.6);
+                McEmailMessage.QueryActiveMessageItemsByScore (defaultAccountId, Folder.Id, 0.6);
             CheckMessages (messages, messageList, 5, 4, 2);
 
-            messageList = McEmailMessage.QueryActiveMessageItemsByScore (defaultAccountId, defaultFolderId, 0.4);
+            messageList = McEmailMessage.QueryActiveMessageItemsByScore (defaultAccountId, Folder.Id, 0.4);
             CheckMessages (messages, messageList, 5, 4, 0, 2);
         }
     }
