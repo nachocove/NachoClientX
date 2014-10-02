4672a87f7355c97b2d69e748388e748cbc637265
Steve Scalpone <sscalpone@gmail.com>
2014-10-01 20:41:52 -0700

    Nacho Now will scroll to the card offsets.

--- NachoClient.iOS/NachoUI.iOS/NachoNowViewController.cs
+++ NachoClient.iOS/NachoUI.iOS/NachoNowViewController.cs
@@ -73,8 +73,8 @@ namespace NachoClient.iOS
             carouselView.ContentOffset = new SizeF (0f, 0f);
             carouselView.BackgroundColor = UIColor.Clear;
             carouselView.IgnorePerpendicularSwipes = true;
-            carouselView.StopAtItemBoundary = false;
-            carouselView.ScrollToItemBoundary = false;
+            carouselView.StopAtItemBoundary = true;
+            carouselView.ScrollToItemBoundary = true;
             View.AddSubview (carouselView);
 
             carouselTapGestureRecognizer = new UITapGestureRecognizer ();
