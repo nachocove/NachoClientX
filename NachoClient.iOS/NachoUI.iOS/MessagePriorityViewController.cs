92303ec607e98443848015a4b8685305cfbec0f2
Zach Quiring <zachq@nachocove.com>
2014-10-06 16:06:37 -0700

    Started working on updating the DeferMessage / Date selector view. Adding new icons, matching design from Hugo. Slipped in a one-liner edit to the UIBlockMenu as well.

--- NachoClient.iOS/NachoUI.iOS/MessagePriorityViewController.cs
+++ NachoClient.iOS/NachoUI.iOS/MessagePriorityViewController.cs
@@ -18,10 +18,10 @@ namespace NachoClient.iOS
         protected INachoDateControllerParent owner;
         protected IntentSelectionViewController intentSelector;
 
-        const float BUTTON_SIZE = 60;
-        const float BUTTON_LABEL_HEIGHT = 20;
-        const float BUTTON_PADDING_HEIGHT = 25;
-        const float BUTTON_PADDING_WIDTH = 20;
+        const float BUTTON_SIZE = 64;
+        const float BUTTON_LABEL_HEIGHT = 40;
+        const float BUTTON_PADDING_HEIGHT = 15;
+        const float BUTTON_PADDING_WIDTH = 35;
 
         private List<UIButton> ActionButtons = new List<UIButton> ();
 
@@ -42,44 +42,61 @@ namespace NachoClient.iOS
 
         public void CreateView ()
         {
-            float frameHeight = 420; //view size isn't relative to screen size (it's fixed)
-            float frameWidth = View.Frame.Width - 40;   //20 px indent l/r sides
-            float windowX = (View.Frame.Width - frameWidth) / 2;
-            float windowY = (View.Frame.Height - frameHeight) / 2;
-
-            UIView priorityView = new UIView (new RectangleF (windowX, windowY, frameWidth, frameHeight));
-            priorityView.Layer.CornerRadius = 15.0f;
+            UIView priorityView = new UIView (View.Frame);
             priorityView.ClipsToBounds = true;
-            priorityView.BackgroundColor = UIColor.White;
+            priorityView.BackgroundColor = A.Color_NachoGreen;
 
-            UILabel viewTitle = new UILabel (new RectangleF (priorityView.Frame.Width / 2 - 75, 20, 150, 20));
-            viewTitle.Text = "Select a Date";
+            float yOffset = 40;
+
+            UIButton dismissButton = new UIButton (new RectangleF (30, yOffset, 25, 25));
+            dismissButton.SetImage(UIImage.FromBundle("modal-close"), UIControlState.Normal);
+            dismissButton.TouchUpInside += (object sender, EventArgs e) => {
+                DismissViewController (true, null);
+            };
+            priorityView.Add (dismissButton);
+
+            UILabel viewTitle = new UILabel (new RectangleF (priorityView.Frame.Width / 2 - 75, yOffset, 150, 25));
+            viewTitle.Text = "Defer Message";
             viewTitle.Font = A.Font_AvenirNextDemiBold17;
-            viewTitle.TextColor = A.Color_NachoBlack;
+            viewTitle.TextColor = UIColor.White;
             viewTitle.TextAlignment = UITextAlignment.Center;
             priorityView.Add (viewTitle);
 
-            var buttonInfoList = new List<ButtonInfo> (new ButtonInfo[] {
-                new ButtonInfo ("Later Today", "cup-48", () => DateSelected(MessageDeferralType.Later, DateTime.MinValue)),
-                new ButtonInfo ("Tonight", "navbar-icn-defer", () => DateSelected(MessageDeferralType.Tonight, DateTime.MinValue)),
-                new ButtonInfo ("Tomorrow", "navbar-icn-defer", () => DateSelected(MessageDeferralType.Tomorrow, DateTime.MinValue)),
-                new ButtonInfo (null, null, null),
-                new ButtonInfo ("Next Week", "navbar-icn-defer", () => DateSelected(MessageDeferralType.NextWeek, DateTime.MinValue)),
-                new ButtonInfo ("Next Month", "navbar-icn-defer", () => DateSelected(MessageDeferralType.NextMonth, DateTime.MinValue)),
-                new ButtonInfo ("Pick Date", "navbar-icn-defer", () =>  PerformSegue ("MessagePriorityToDatePicker", this)),
-                new ButtonInfo (null, null, null),
-                null,
-                null,
-                new ButtonInfo ("None", "navbar-icn-defer", () => DateSelected(MessageDeferralType.None, DateTime.MinValue)),
-            });
+            yOffset = viewTitle.Frame.Bottom + 20;
 
+            UIView sectionSeparator = new UIView (new RectangleF(0, yOffset, View.Frame.Width, .5f));
+            sectionSeparator.BackgroundColor = UIColor.LightGray.ColorWithAlpha(.6f);
+            priorityView.AddSubview (sectionSeparator);
+
+            yOffset = sectionSeparator.Frame.Bottom + 20;
+
+            UILabel messageSubject = new UILabel (new RectangleF (30, yOffset, View.Frame.Width - 60, 25));
+            messageSubject.Text = "Message Subject Goes Here";
+            messageSubject.Font = A.Font_AvenirNextRegular17;
+            messageSubject.TextColor = UIColor.White;
+            priorityView.Add (messageSubject);
+
+            yOffset = messageSubject.Frame.Bottom + 60;
+
+            var buttonInfoList = new List<ButtonInfo> (new ButtonInfo[] {
+                new ButtonInfo ("Later Today", "modal-later-today", () => DateSelected(MessageDeferralType.Later, DateTime.MinValue)),
+                new ButtonInfo ("Tonight", "modal-tonight", () => DateSelected(MessageDeferralType.Tonight, DateTime.MinValue)),
+                new ButtonInfo ("Tomorrow", "modal-tomorrow", () => DateSelected(MessageDeferralType.Tomorrow, DateTime.MinValue)),
+                new ButtonInfo (null, null, null),
+                new ButtonInfo ("This Week", "modal-this-week", () => DateSelected(MessageDeferralType.NextWeek, DateTime.MinValue)),
+                new ButtonInfo ("Next Week", "modal-next-week", () => DateSelected(MessageDeferralType.NextMonth, DateTime.MinValue)),
+                new ButtonInfo ("Pick Date", "modal-pick-date", () =>  PerformSegue ("MessagePriorityToDatePicker", this)),
+                new ButtonInfo (null, null, null),
+                new ButtonInfo ("Forever", "modal-forever", () => DateSelected(MessageDeferralType.None, DateTime.MinValue)),
+                new ButtonInfo ("None", "modal-none", () => DateSelected(MessageDeferralType.None, DateTime.MinValue)),
+                null,
+            });
 
             var center = priorityView.Center;
             center.X = (priorityView.Frame.Width / 2);
             center.Y = center.Y;
 
             var xOffset = center.X - BUTTON_SIZE - BUTTON_PADDING_WIDTH;
-            var yOffset = center.Y - (1.5F * BUTTON_PADDING_HEIGHT) - (2F * (BUTTON_SIZE + BUTTON_LABEL_HEIGHT)) + (0.5F * BUTTON_SIZE);
 
             foreach (var buttonInfo in buttonInfoList) {
                 if (null == buttonInfo) {
@@ -95,8 +112,8 @@ namespace NachoClient.iOS
                 var buttonRect = UIButton.FromType (UIButtonType.RoundedRect);
                 buttonRect.Layer.CornerRadius = BUTTON_SIZE / 2;
                 buttonRect.Layer.MasksToBounds = true;
-                buttonRect.Layer.BorderColor = A.Color_NachoGreen.CGColor;
-                buttonRect.Layer.BorderWidth = 1.0f;                  
+                buttonRect.Layer.BorderColor = UIColor.LightGray.CGColor;
+                buttonRect.Layer.BorderWidth = .5f;                 
                 buttonRect.Frame = new RectangleF (0, 0, BUTTON_SIZE, BUTTON_SIZE);
                 buttonRect.Center = new PointF (xOffset, yOffset);
                 buttonRect.SetImage (UIImage.FromBundle (buttonInfo.buttonIcon), UIControlState.Normal);
@@ -107,7 +124,7 @@ namespace NachoClient.iOS
                 priorityView.Add (buttonRect);
 
                 var label = new UILabel ();
-                label.TextColor = A.Color_NachoBlack;
+                label.TextColor = UIColor.White;
                 label.Text = buttonInfo.buttonLabel;
                 label.Font = A.Font_AvenirNextMedium14;
                 label.TextAlignment = UITextAlignment.Center;
@@ -117,21 +134,6 @@ namespace NachoClient.iOS
 
                 xOffset += BUTTON_SIZE + BUTTON_PADDING_WIDTH;
             }
-
-            var dismissLabel = new UILabel ();
-            dismissLabel.Text = "Dismiss";
-            dismissLabel.TextColor = A.Color_NachoBlack;
-            dismissLabel.Font = A.Font_AvenirNextRegular12;
-            dismissLabel.TextAlignment = UITextAlignment.Center;
-            dismissLabel.SizeToFit ();
-            dismissLabel.Frame = new RectangleF (priorityView.Frame.Width / 2 - 50, priorityView.Frame.Height - 30, 100, 20);
-            priorityView.AddSubview (dismissLabel);
-
-            var tap = new UITapGestureRecognizer ((UITapGestureRecognizer obj) => {
-                DismissViewController (true, null);
-            });
-            dismissLabel.AddGestureRecognizer (tap);
-            dismissLabel.UserInteractionEnabled = true;
 
             View.AddSubview (priorityView);
         }
