135d15eb96f713ec083ecaf7b34a1aa0ae6f8073
Zach Quiring <zachq@nachocove.com>
2014-10-16 14:36:02 -0700

    Updated the QueryActiveMessageItemsByScore unit test to include two new cases: Message has a HotScore below the threshold, but has a UserAction = 1, and Message has a HotScore above the threshold, but has a UserAction = -1. Also noticed a couple mistakes in the unit test and fixed those.

--- Test.Android/McEmailMessageTest.cs
+++ Test.Android/McEmailMessageTest.cs
@@ -87,7 +87,7 @@ namespace Test.Common
             // 1 email is from VIP. 1 email is from VIP and has score over 0.5. 2
             // emails have scores below 0.5. 1 email is delete pending. 1 email
             // has a start date in the future.
-            McEmailAddress[] addresses = new McEmailAddress[8];
+            McEmailAddress[] addresses = new McEmailAddress[10];
             addresses [0] = SetupAddress ("aaron@company.net", 4, 2, false);
             addresses [1] = SetupAddress ("bob@company.net", 5, 1, false);
             addresses [2] = SetupAddress ("charles@compnay.net", 3, 3, false);
@@ -96,8 +96,12 @@ namespace Test.Common
             addresses [5] = SetupAddress ("fred@company.net", 3, 1, true);
             addresses [6] = SetupAddress ("gary@company.net", 6, 6, false);
             addresses [7] = SetupAddress ("harry@company.net", 7, 7, false);
+            addresses [8] = SetupAddress ("ivan@company.net", 3, 1, false);
+            addresses [9] = SetupAddress ("jolene@company.net", 5, 5, false);
 
-            McEmailMessage[] messages = new McEmailMessage[8];
+
+
+            McEmailMessage[] messages = new McEmailMessage[10];
             messages [0] = SetupMessage (addresses [0], new DateTime (2014, 8, 15, 1, 23, 0));
             messages [1] = SetupMessage (addresses [1], new DateTime (2014, 8, 15, 2, 0, 0));
             // Intentional set this one received date earlier to test "SORT BY DateReceived"
@@ -107,20 +111,28 @@ namespace Test.Common
             messages [5] = SetupMessage (addresses [5], new DateTime (2014, 8, 15, 5, 0, 0));
             messages [6] = SetupMessage (addresses [6], new DateTime (2014, 8, 15, 6, 0, 0));
             messages [6].IsAwaitingDelete = true;
+            messages [6].Update ();
             messages [7] = SetupMessage (addresses [7], new DateTime (2014, 8, 15, 7, 0, 0));
             messages [7].FlagUtcStartDate = DateTime.MaxValue;
+            messages [7].Update ();
+            messages [8] = SetupMessage (addresses [8], new DateTime (2014, 8, 15, 8, 0, 0));
+            messages [8].UserAction = 1;
+            messages [8].Update ();
+            messages [9] = SetupMessage (addresses [9], new DateTime (2014, 8, 15, 9, 0, 0));
+            messages [9].UserAction = -1;
+            messages [9].Update ();
 
-            for (int n = 0; n < 6; n++) {
+            for (int n = 0; n < 10; n++) {
                 Folder.Link (messages [n]);
                 SetupDependency (messages [n], addresses [n]);
             }
 
             List<NcEmailMessageIndex> messageList =
                 McEmailMessage.QueryActiveMessageItemsByScore (defaultAccountId, Folder.Id, 0.6);
-            CheckMessages (messages, messageList, 5, 4, 2);
+            CheckMessages (messages, messageList, 8, 5, 4, 2);
 
             messageList = McEmailMessage.QueryActiveMessageItemsByScore (defaultAccountId, Folder.Id, 0.4);
-            CheckMessages (messages, messageList, 5, 4, 0, 2);
+            CheckMessages (messages, messageList, 8, 5, 4, 0, 2);
         }
 
         [Test]
