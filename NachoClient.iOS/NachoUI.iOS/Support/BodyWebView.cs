66212214d55a9159c7079d547ae2cfcfdc726e17
Henry Kwok <henryk@nachocove.com>
2014-10-22 01:00:40 -0700

    Move the sizing of the web view frame to reduce flickiering of the view.

--- NachoClient.iOS/NachoUI.iOS/Support/BodyWebView.cs
+++ NachoClient.iOS/NachoUI.iOS/Support/BodyWebView.cs
@@ -57,13 +57,15 @@ namespace NachoClient.iOS
 
             ViewFramer.Create (this)
                 .X (0)
-                .Y (0)
-                .Size (parentView.Frame.Size);
+                .Y (0);
 
             htmlBusy = new RecursionCounter (() => {
                 EvaluateJavascript (magic);
 
                 zoomRecognizer.Configure ();
+
+                ViewFramer.Create(this)
+                    .Size (parentView.Frame.Size);
 
                 // If the content is wider than the frame, try to scale it down
                 OnRenderComplete (1.0f);
