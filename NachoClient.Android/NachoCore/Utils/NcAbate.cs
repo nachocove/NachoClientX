d69ad6d38aecb0a7d0b3e8b76433467e95d3972f
Steve Scalpone <sscalpone@gmail.com>
2014-11-11 09:33:07 -0800

    Replace the abate status inds with direct sets of the two globals used to throttle the db and abate any background processing.  The status inds were measured to take from .5 to 1.0 seconds to be delivered.

--- NachoClient.Android/NachoCore/Utils/NcAbate.cs
+++ NachoClient.Android/NachoCore/Utils/NcAbate.cs
@@ -9,21 +9,26 @@ namespace NachoCore.Utils
         public static void HighPriority (string caller)
         {
             NachoCore.Utils.Log.Info (NachoCore.Utils.Log.LOG_UI, "HighPriority sent Info_BackgroundAbateStarted from {0}", caller);
-            NcApplication.Instance.InvokeStatusIndEvent (new StatusIndEventArgs () { 
-                Status = NachoCore.Utils.NcResult.Info (NcResult.SubKindEnum.Info_BackgroundAbateStarted),
-                Account = NachoCore.Model.ConstMcAccount.NotAccountSpecific,
-                Tokens = new String[] { DateTime.Now.ToString () },
-            });
+//            NcApplication.Instance.InvokeStatusIndEvent (new StatusIndEventArgs () { 
+//                Status = NachoCore.Utils.NcResult.Info (NcResult.SubKindEnum.Info_BackgroundAbateStarted),
+//                Account = NachoCore.Model.ConstMcAccount.NotAccountSpecific,
+//                Tokens = new String[] { DateTime.Now.ToString () },
+//            });
+            NcApplication.Instance.IsBackgroundAbateRequired = true;
+            NachoCore.Model.NcModel.Instance.RateLimiter.Enabled = true;
+
         }
 
         public static void RegularPriority (string caller)
         {
             NachoCore.Utils.Log.Info (NachoCore.Utils.Log.LOG_UI, "RegularPriority sent Info_BackgroundAbateStopped from {0}", caller);
-            NcApplication.Instance.InvokeStatusIndEvent (new StatusIndEventArgs () { 
-                Status = NachoCore.Utils.NcResult.Info (NcResult.SubKindEnum.Info_BackgroundAbateStopped),
-                Account = NachoCore.Model.ConstMcAccount.NotAccountSpecific,
-                Tokens = new String[] { DateTime.Now.ToString () },
-            });
+//            NcApplication.Instance.InvokeStatusIndEvent (new StatusIndEventArgs () { 
+//                Status = NachoCore.Utils.NcResult.Info (NcResult.SubKindEnum.Info_BackgroundAbateStopped),
+//                Account = NachoCore.Model.ConstMcAccount.NotAccountSpecific,
+//                Tokens = new String[] { DateTime.Now.ToString () },
+//            });
+            NcApplication.Instance.IsBackgroundAbateRequired = false;
+            NachoCore.Model.NcModel.Instance.RateLimiter.Enabled = false;
         }
 
         public static TimeSpan DeliveryTime (StatusIndEventArgs e)
