5ba703c115cda49f60a306c896c9f74e0fea0136
Steve Scalpone <sscalpone@gmail.com>
2014-12-11 20:59:16 -0800

    Add files with alpha & beta branding.

---
+++ NachoClient.iOS/Resources/alpha/copy.sh
@@ -0,0 +1,16 @@
+cp SettingsIcon.png ../Icon-Small.png
+cp SettingsIcon@2x.png ../Icon-Small@2x.png
+cp SpotlightIcon.png ../Icon-Small-40.png
+cp SpotlightIcon@2x.png ../Icon-Small-40@2x.png
+cp iPhoneIcon@2x.png ../Icon-60@2x.png
+cp iPadIcon.png ../Icon-76.png
+cp iPhoneIcon.png ../iPhoneIcon.png
+cp iPadIcon@2x.png ../Icon-76@2x.png
+
+# ../Icon-72.png:            PNG image data, 72 x 72, 8-bit/color RGBA, non-interlaced
+# ../Icon-72@2x.png:         PNG image data, 144 x 144, 8-bit/color RGBA, non-interlaced
+# ../Icon.png:               PNG image data, 57 x 57, 8-bit/color RGBA, non-interlaced
+# ../Icon@2x.png:            PNG image data, 114 x 114, 8-bit/color RGBA, non-interlaced
+# ../Nacho-Cove-Icon.png:    PNG image data, 24 x 24, 8-bit/color RGBA, non-interlaced
+# ../Nacho-Cove-Icon@2x.png: PNG image data, 48 x 48, 8-bit/color RGBA, non-interlaced
+
