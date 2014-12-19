665a8d4a04f0c46a1a0145a382f6f69b5db3a2bd
David Olsen <davido@nachocove.com>
2014-12-18 16:26:11 -0800

    Rewrite class AsDnsOperation to improve auto-d for GFE
    Rewrite class AsDnsOperation.  The class now calls res_query()
    directly rather than using DnDns to issue the DNS request.  DnDns is
    still used to parse the result of res_query().  The error code from
    res_query() is checked and the call is retried if the error code is
    TRY_AGAIN.
    
    Create a minimal C wrapper around res_query().  The wrapper gets the
    value from h_errno, which is not easily accessible from C#, and
    returns it via a reference parameter.  This wrapper is necessary so
    that AsDnsOperation can know when to retry the res_query() call.
    
    Fix #807

--- NachoClient.Android/NachoCore/BackEnd/ActiveSync/Operations/AsDnsOperation.cs
+++ NachoClient.Android/NachoCore/BackEnd/ActiveSync/Operations/AsDnsOperation.cs
@@ -1,103 +1,173 @@
-// # Copyright (C) 2013 Nacho Cove, Inc. All rights reserved.
+// # Copyright (C) 2013, 2014 Nacho Cove, Inc. All rights reserved.
 //
 using System;
-using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Threading;
-using DnDns.Query;
+using System.Threading.Tasks;
 using DnDns.Enums;
-using NachoCore.Utils;
+using DnDns.Query;
 using NachoCore.Model;
+using NachoCore.Utils;
 
 namespace NachoCore.ActiveSync
 {
     public class AsDnsOperation : IAsOperation
     {
-        private const string KDefaultTimeoutSeconds = "10";
+        private IAsDnsOperationOwner owner;
+        private TimeSpan timeout;
+        private NcStateMachine stateMachine;
 
-        public TimeSpan Timeout { set; get; }
+        private static object resQueryLock = new object ();
+        private object lockObject = new object ();
+        private bool complete = false;
+        private NcTimer timer;
 
-        private bool wasKilledByTimer;
-        private bool wasCancelled;
-        private IAsDnsOperationOwner m_owner;
-        private NcTimer TimeoutTimer;
-        private IDnsQueryRequest Request;
-        private object LockObj;
-        private NcStateMachine Sm;
+        // Allow the unit tests to mock up the call to res_query().
+        public delegate byte[] CallResQueryDelegate (AsDnsOperation op, string host, NsClass dnsClass, NsType dnsType, out int answerLength);
+        static public CallResQueryDelegate CallResQuery = ResQuery;
 
-        public static Type DnsQueryRequestType = typeof(MockableDnsQueryRequest);
+        private const int kDefaultTimeoutSeconds = 10;
+
+        // The res_query() error code for TRY_AGAIN.  This is the only error code
+        // that we care about.
+        private const int TRY_AGAIN_ERROR = 2;
 
         public AsDnsOperation (IAsDnsOperationOwner owner)
         {
-            LockObj = new object ();
-            var timeoutSeconds = McMutables.GetOrCreate (McAccount.GetDeviceAccount ().Id, "DNSOP", "TimeoutSeconds", KDefaultTimeoutSeconds);
-            Timeout = new TimeSpan (0, 0, timeoutSeconds.ToInt ());
-            m_owner = owner;
+            this.owner = owner;
+            int timeoutSeconds = kDefaultTimeoutSeconds;
+            string timeoutString = McMutables.GetOrCreate (
+                McAccount.GetDeviceAccount ().Id, "DNSOP", "TimeoutSeconds", kDefaultTimeoutSeconds.ToString ());
+            try {
+                timeoutSeconds = timeoutString.ToInt ();
+            } catch (Exception e) {
+                // The timeout value is not an integer.  Swallow the exception, leaving the
+                // timeout as the default.
+                Log.Error (Log.LOG_DNS, "The timeout value for DNS operations, {0}, is not an integer: {1}",
+                    timeoutString, e.ToString ());
+            }
+            timeout = new TimeSpan (0, 0, timeoutSeconds);
         }
 
-        public async void Execute (NcStateMachine sm)
+        public AsDnsOperation (IAsDnsOperationOwner owner, TimeSpan timeout)
         {
-            Sm = sm;
-            DnsQueryResponse response;
-            Request = (IDnsQueryRequest)Activator.CreateInstance (DnsQueryRequestType);
-            Request.Timeout = (int)Timeout.TotalMilliseconds;
-            TimeoutTimer = new NcTimer ("AsDnsOperation", TimerCallback, null, (int)Timeout.TotalMilliseconds,
-                System.Threading.Timeout.Infinite);
-            try {
-                try {
-                    response = await Request.ResolveAsync (m_owner.DnsHost (this),
-                        m_owner.DnsType (this),
-                        m_owner.DnsClass (this), ProtocolType.Udp).ConfigureAwait (false);
-                } catch (AggregateException aex) {
-                    Log.Error(Log.LOG_HTTP, "Received AggregateException in await ... ResolveAsync");
-                    throw aex.InnerException;
-                }
-                CleanupTimeoutTimer ();
-                if (!wasCancelled && !wasKilledByTimer) {
-                    var Event = m_owner.ProcessResponse (this, response);
-                    Sm.PostEvent (Event);
-                }
-            } catch (Exception ex) {
-                if (ex is ObjectDisposedException || ex is SocketException) {
-                    if (!wasKilledByTimer &&
-                        (ex is SocketException && !wasCancelled)) {
-                        Sm.PostEvent ((uint)SmEvt.E.TempFail, "DNSOPTEMP0");
-                    }
-                } else {
-                    throw;
-                }
-            }
+            this.owner = owner;
+            this.timeout = timeout;
+        }
+
+        public void Execute (NcStateMachine stateMachine)
+        {
+            this.stateMachine = stateMachine;
+
+            // Start the timer that will prevent the operation from running too long.
+            timer = new NcTimer ("AsDnsOperation", TimerCallback, null,
+                (int)timeout.TotalMilliseconds, Timeout.Infinite);
+
+            // Start the task on another thread. DoExecuteWithRetries takes care of
+            // reporting its results, so there is no need to wait for the task to
+            // complete or to check its value.
+            Task.Run ((Action)DoExecuteWithRetries);
         }
 
         public void Cancel ()
         {
-            lock (LockObj) {
-                wasCancelled = true;
-                CleanupTimeoutTimer ();
-                Close ();
+            lock (lockObject) {
+                if (!complete) {
+                    owner.CancelCleanup (this);
+                    StopTimer ();
+                }
+                complete = true;
             }
         }
 
-        private void Close ()
+        // res_query() stores its error code in h_errno, not errno.  The usual technique
+        // for getting the error code, SetLastError=true, won't work.  So we have created
+        // a wrapper function around res_query that gets the error code from h_errno and
+        // returns it through a ref parameter.
+        [DllImport("__Internal")]
+        private static extern int nacho_res_query (
+            string host, int queryClass, int queryType, byte[] answer, int anslen, ref int errorCode);
+
+        private static byte[] ResQuery (AsDnsOperation op, string host, NsClass dnsClass, NsType dnsType, out int answerLength)
         {
-            if (null != Request && null != Request.UdpClient) {
-                Request.UdpClient.Close ();
+            // See https://www.dns-oarc.net/oarc/services/replysizetest - 4k is enough to hold the response.
+            byte[] answer = new byte[4096];
+
+            // Since the res_query call will be retried only if the error code is TRY_AGAIN,
+            // set the maximum number of retries to a rediculously high number.  It is
+            // unlikely that this limit will ever be reached.
+            int retries = 200;
+            while (0 < --retries) {
+
+                // res_query() stores its error code in a global variable, h_errno.  Not a
+                // thread-specific variable like errno, but a truly global variable.  To
+                // prevent one call from overwriting the error code of another before it is
+                // read, use a lock to prevent simultaneous calls to res_query.  (This won't
+                // prevent some other thread that we don't control from calling res_query()
+                // or some other function that uses h_errno, but there is no feasible way to
+                // protect against that.)
+                int errorCode = 0;
+                int rc;
+                lock (resQueryLock) {
+                    rc = nacho_res_query (host, (int)dnsClass, (int)dnsType, answer, answer.Length, ref errorCode);
+                }
+
+                lock (op.lockObject) {
+                    if (op.complete || (0 > rc && TRY_AGAIN_ERROR != errorCode)) {
+                        // The operation timed out or was canceled, or res_query() failed.
+                        answerLength = 0;
+                        return null;
+                    }
+                    if (0 <= rc) {
+                        // res_query() succeeded.
+                        answerLength = rc;
+                        return answer;
+                    }
+                }
+            }
+            // Too many retries. Give up.
+            answerLength = 0;
+            return null;
+        }
+
+        private void DoExecuteWithRetries ()
+        {
+            int answerLength;
+            byte[] answer = CallResQuery (this, owner.DnsHost (this), owner.DnsClass (this), owner.DnsType (this), out answerLength);
+
+            lock (lockObject) {
+                if (complete) {
+                    // Operation was canceled or timed out.
+                    return;
+                }
+                complete = true;
+                StopTimer ();
+                if (null != answer) {
+                    DnsQueryResponse response = new DnsQueryResponse ();
+                    response.ParseResponse (answer, answerLength);
+                    stateMachine.PostEvent (owner.ProcessResponse (this, response));
+                } else {
+                    stateMachine.PostEvent ((uint)SmEvt.E.HardFail, "DNSHARDFAIL");
+                }
             }
         }
 
-        private void CleanupTimeoutTimer ()
+        private void TimerCallback (object state)
         {
-            if (null != TimeoutTimer) {
-                TimeoutTimer.Dispose ();
-                TimeoutTimer = null;
+            lock (lockObject) {
+                if (!complete) {
+                    stateMachine.PostEvent ((uint)SmEvt.E.TempFail, "DNSOPTO");
+                }
+                complete = true;
             }
         }
 
-        private void TimerCallback (object State)
+        private void StopTimer ()
         {
-            wasKilledByTimer = true;
-            CleanupTimeoutTimer ();
-            Close ();
-            Sm.PostEvent ((uint)SmEvt.E.TempFail, "DNSOPTO");
+            if (null != timer) {
+                timer.Dispose ();
+                timer = null;
+            }
         }
     }
 }
