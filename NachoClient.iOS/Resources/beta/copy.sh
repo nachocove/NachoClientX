88102d5a46c74c405538a157bdf9d7a40c34e3f8
colebritton <coleb@nachocove.com>
2014-12-19 15:54:21 -0800

    More icon clean up

--- NachoClient.iOS/Resources/beta/copy.sh
+++ NachoClient.iOS/Resources/beta/copy.sh
@@ -7,10 +7,6 @@ cp iPadIcon.png ../Icon-76.png
 cp iPhoneIcon.png ../iPhoneIcon.png
 cp iPadIcon@2x.png ../Icon-76@2x.png
 
-# ../Icon-72.png:            PNG image data, 72 x 72, 8-bit/color RGBA, non-interlaced
-# ../Icon-72@2x.png:         PNG image data, 144 x 144, 8-bit/color RGBA, non-interlaced
 # ../Icon.png:               PNG image data, 57 x 57, 8-bit/color RGBA, non-interlaced
 # ../Icon@2x.png:            PNG image data, 114 x 114, 8-bit/color RGBA, non-interlaced
-# ../Nacho-Cove-Icon.png:    PNG image data, 24 x 24, 8-bit/color RGBA, non-interlaced
-# ../Nacho-Cove-Icon@2x.png: PNG image data, 48 x 48, 8-bit/color RGBA, non-interlaced
 
