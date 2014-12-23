044b74ca4e2b5e804afb2d269a505a1f69f12a3d
Steve Scalpone <sscalpone@gmail.com>
2014-12-23 09:01:40 -0800

    Remove unused things from storyboard.

--- NachoClient.iOS/NachoUI.iOS/MessagePriorityViewController.designer.cs
+++ NachoClient.iOS/NachoUI.iOS/MessagePriorityViewController.designer.cs
@@ -12,95 +12,9 @@ namespace NachoClient.iOS
 	[Register ("MessagePriorityViewController")]
 	partial class MessagePriorityViewController
 	{
-		[Outlet]
-		MonoTouch.UIKit.UILabel currentDelayLabel { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton deadlineButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton deferDatePicker { get; set; }
-
-		[Outlet]
-		MonoTouch.UIKit.UIButton dismissButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton laterButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton meetingButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton nextMonthButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton nextWeekButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton taskButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton tomorrowButton { get; set; }
-
-		[Outlet]
-		NachoClient.iOS.LabeledIconButton tonightButton { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
-			if (currentDelayLabel != null) {
-				currentDelayLabel.Dispose ();
-				currentDelayLabel = null;
-			}
-
-			if (meetingButton != null) {
-				meetingButton.Dispose ();
-				meetingButton = null;
-			}
-
-			if (taskButton != null) {
-				taskButton.Dispose ();
-				taskButton = null;
-			}
-
-			if (deadlineButton != null) {
-				deadlineButton.Dispose ();
-				deadlineButton = null;
-			}
-
-			if (laterButton != null) {
-				laterButton.Dispose ();
-				laterButton = null;
-			}
-
-			if (tonightButton != null) {
-				tonightButton.Dispose ();
-				tonightButton = null;
-			}
-
-			if (tomorrowButton != null) {
-				tomorrowButton.Dispose ();
-				tomorrowButton = null;
-			}
-
-			if (nextWeekButton != null) {
-				nextWeekButton.Dispose ();
-				nextWeekButton = null;
-			}
-
-			if (nextMonthButton != null) {
-				nextMonthButton.Dispose ();
-				nextMonthButton = null;
-			}
-
-			if (deferDatePicker != null) {
-				deferDatePicker.Dispose ();
-				deferDatePicker = null;
-			}
-
-			if (dismissButton != null) {
-				dismissButton.Dispose ();
-				dismissButton = null;
-			}
 		}
 	}
 }
