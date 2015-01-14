74a88bd643860330e687c19474197af16daeeef9
Steve Scalpone <sscalpone@gmail.com>
2015-01-14 14:10:04 -0800

    Add a comment.

--- NachoClient.iOS/NachoUI.iOS/Support/PermissionManager.cs
+++ NachoClient.iOS/NachoUI.iOS/Support/PermissionManager.cs
@@ -11,6 +11,26 @@ using NachoCore.Model;
 
 namespace NachoClient.iOS
 {
+    // 
+    // Ask for permission code for notifications, calendar and contacts.
+    // TODO: The UIImagePickerController asks for permissions.  We need
+    // to our own code to ask for permissions to access the camera roll.
+    // 
+    // The ask is two-steps. The first step is for Nacho Mail to ask on
+    // behalf of itself instead of calling the iOS permission routines.
+    // The iOS permissions cannot be undone by the app so before we let
+    // the user say 'no' to the system, we ask first.  if the user says
+    // no to us, then we do not ask the system.
+    // 
+    // The second step is to ask the system for permission.  (We do not
+    // get a callback for notifications yet.) If the user says no, then
+    // we record that fact that the user said yes to Nacho Mail but not
+    // to iOS.
+    // 
+    // We can use these variables to prompt the user to give permission
+    // after they've said no. If the first step got a no, we can prompt
+    // the user to say yet. If the second step got a no, we can explain
+    // to the user to use the system settings to reset the permission.
 
     public class PermissionManager
     {
