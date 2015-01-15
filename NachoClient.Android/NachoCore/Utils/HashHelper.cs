1bb4f66cb24ac82a98793444551f785201bcd23c
Henry Kwok <henryk@nachocove.com>
2015-01-14 16:20:56 -0800

    Add hashed redaction. Support both a short (truncated) hash and a full SHA-256 hash of element contents.

---
+++ NachoClient.Android/NachoCore/Utils/HashHelper.cs
@@ -0,0 +1,25 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NachoCore.Utils
+{
+    public class HashHelper
+    {
+        public static string Sha256 (string s)
+        {   
+            byte[] bytes = Encoding.ASCII.GetBytes (s);
+            SHA256 sha256 = SHA256.Create ();
+            sha256.ComputeHash (bytes);
+
+            string hash = "";
+            for (int n = 0; n < sha256.Hash.Length; n++) {
+                hash += String.Format ("{0:x2}", sha256.Hash [n]);
+            }
+            return hash;
+        }
+    }
+}
+
