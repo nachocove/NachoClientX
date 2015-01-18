e255896230f14139a749f3de4f2ce90b4c6ec20a
Henry Kwok <henryk@nachocove.com>
2015-01-17 23:40:17 -0800

    Add migration infrastructure. Add a migration to add migration version to all tables. Add another migration to change to / cc addresses McEmailAddress.Id.

---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration.cs
@@ -0,0 +1,156 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using SQLite;
+
+using NachoCore.Utils;
+
+namespace NachoCore.Model
+{
+    /// <summary>
+    /// Base class for all migrations. Each migration is really a task that updates the database in some ways.
+    /// </summary>
+    public class NcMigration
+    {
+        // Convenient short hand for all migrations
+        public SQLiteConnection Db {
+            get {
+                return NcModel.Instance.Db;
+            }
+        }
+
+        // The total # of objects to be processed
+        public static int TotalObjects { get; protected set; }
+
+        // The # of objects processed (during this run)
+        public static int ProcessedObjects { get; protected set; }
+
+        // The highest migration version of the current build
+        public static int CurrentVersion { get; protected set; }
+
+        public NcMigration ()
+        {
+        }
+
+        public int Version ()
+        {
+            string className = this.GetType ().Name;
+            NcAssert.True (className.StartsWith ("NcMigration"));
+            string versionString = className.Substring (11);
+            return Convert.ToInt32 (versionString);
+        }
+
+        public static bool StartService ()
+        {
+            // Find all derived classes.
+            var subclasses = (from assembly in AppDomain.CurrentDomain.GetAssemblies ()
+                                       from type_ in assembly.GetTypes ()
+                                       where type_.IsSubclassOf (typeof(NcMigration))
+                                       select type_
+                             );
+
+            // Find the latest version
+            var latestMigration = McMigration.QueryLatestMigration ();
+            int lastMigration; // the latest complete migration
+            if (null == latestMigration) {
+                lastMigration = 0;
+            } else {
+                if (latestMigration.Finished) {
+                    lastMigration = latestMigration.Version;
+                } else {
+                    lastMigration = latestMigration.Version - 1;
+                }
+            }
+
+            // Filter out all migration versions that we have already finished.
+            List<NcMigration> migrations = new List<NcMigration> ();
+            foreach (var subclass in subclasses) {
+                NcMigration migration = (NcMigration)Activator.CreateInstance (subclass, false);
+                var version = migration.Version ();
+                if (version > CurrentVersion) {
+                    CurrentVersion = version;
+                }
+                if (version >= lastMigration) {
+                    migrations.Add (migration);
+                }
+            }
+
+            if (0 == migrations.Count) {
+                return false; // no outstanding migration
+            }
+
+            // Run them all starting with the lowest version
+            NcTask.Run (() => {
+                foreach (var migration in migrations) {
+                    var startTime = DateTime.Now;
+                    var version = migration.Version ();
+                    int rows;
+
+                    // Set up the counters
+                    ProcessedObjects = 0;
+                    TotalObjects = 0;
+
+                    var numObjects = migration.GetNumberOfObjects ();
+                    Log.Info (Log.LOG_DB, "Migration {0} will process {1} objects", migration.Version (), numObjects);
+                    TotalObjects = numObjects;
+
+                    // Create or update the record of this migration
+                    McMigration migrationRecord = McMigration.QueryByVersion (version);
+                    if (null == migrationRecord) {
+                        migrationRecord = new McMigration ();
+                        migrationRecord.Version = version;
+                        migrationRecord.StartTime = startTime;
+                        rows = migrationRecord.Insert ();
+                        NcAssert.True (1 == rows);
+                    }
+                    migrationRecord.NumberOfTimesRan += 1;
+
+                    // Run the migration
+                    Log.Info (Log.LOG_DB, "Running migration {0}...", version);
+                    NcTask.Cts.Token.ThrowIfCancellationRequested ();
+                    try {
+                        migration.Run (NcTask.Cts.Token);
+                        migrationRecord.DurationMsec += (int)(DateTime.Now - startTime).TotalMilliseconds;
+                        migrationRecord.Finished = true;
+                        rows = migrationRecord.Update ();
+                        NcAssert.True (1 == rows);
+                    } catch (OperationCanceledException) {
+                        migrationRecord.DurationMsec += (int)(DateTime.Now - startTime).TotalMilliseconds;
+                        rows = migrationRecord.Update ();
+                        NcAssert.True (1 == rows);
+                        throw;
+                    }
+                }
+            }, "Migration");
+            return true;
+        }
+
+        /// <summary>
+        /// Return the number of db objects to be processed. The number is only for informative
+        /// purpose. It does not have to be absolutely accurate.
+        /// </summary>
+        /// <returns>The number of objects.</returns>
+        public virtual int GetNumberOfObjects ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        /// <summary>
+        /// The actual implementation of a migration. Between processing of consecutive objects,
+        /// it must check for cancellation using the provided token. The only facility that are
+        /// assumed available is DB. Telemetry events can be recorded but they will not uploaded
+        /// until later.
+        /// </summary>
+        /// <param name="token">Token.</param>
+        public virtual void Run (CancellationToken token)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
+
---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration2.cs
@@ -0,0 +1,64 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using System.Threading;
+using MimeKit;
+
+using NachoCore.Utils;
+
+namespace NachoCore.Model
+{
+    public class NcMigration2 : NcMigration
+    {
+        public NcMigration2 ()
+        {
+        }
+
+        public override int GetNumberOfObjects ()
+        {
+            // We only count the # of invalid ToEmailAddressId. But in reality, we will migrate
+            // both To and Cc.
+            return Db.Table<McEmailMessage> ().Where (x => x.MigrationVersion < 1).Count ();
+        }
+
+        public void ProcessAddressList (McEmailMessage emailMessage, string addressString, EmailAddressType addressType)
+        {
+            var addressList = NcEmailAddress.ParseAddressListString (addressString);
+            var thisVersion = Version ();
+            foreach (MailboxAddress address in addressList) {
+                McEmailAddress emailAddress;
+                var got = McEmailAddress.Get (emailMessage.AccountId, address.Address, out emailAddress);
+                NcAssert.True (got);
+
+                var map = new McMapEmailMessageAddress ();
+                map.EmailMessageId = emailMessage.Id;
+                map.EmailAddressId = emailAddress.Id;
+                map.AddressType = addressType;
+                // don't use the current version as there may be migration between this and current version
+                map.MigrationVersion = thisVersion;
+                map.Insert ();
+            }
+        }
+
+        public override void Run (CancellationToken token)
+        {
+            var thisVersion = Version ();
+            foreach (var emailMessage in Db.Table<McEmailMessage> ().Where (x => x.MigrationVersion < thisVersion)) {
+                token.ThrowIfCancellationRequested ();
+
+                NcModel.Instance.RunInTransaction (() => {
+                    if (!String.IsNullOrEmpty (emailMessage.To)) {
+                        ProcessAddressList (emailMessage, emailMessage.To, EmailAddressType.TO);
+                    }
+                    if (!String.IsNullOrEmpty (emailMessage.Cc)) {
+                        ProcessAddressList (emailMessage, emailMessage.Cc, EmailAddressType.CC);
+                    }
+                    emailMessage.MigrationVersion = thisVersion;
+                    emailMessage.Update ();
+                    NcMigration.ProcessedObjects += 1;
+                });
+            }
+        }
+    }
+}
+
