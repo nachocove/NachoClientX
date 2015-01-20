c08410607b8e34d4cd6b481bd3f551a0d8bf1174
Steve Scalpone <sscalpone@gmail.com>
2015-01-20 00:16:19 -0800

    Fix #1249.  Rewrite the query to include all contacts.

--- Test.iOS/McContactTest.cs
+++ Test.iOS/McContactTest.cs
@@ -2,11 +2,15 @@
 //
 using System;
 using NUnit.Framework;
-using NachoCore;
-using NachoCore.Utils;
 using NachoCore.Model;
+using NachoCore.ActiveSync;
+using NachoCore.Utils;
 using System.Collections.Generic;
-
+using System.Linq;
+using TypeCode = NachoCore.ActiveSync.Xml.FolderHierarchy.TypeCode;
+using ClassCode = NachoCore.Model.McAbstrFolderEntry.ClassCodeEnum;
+using NachoAssertionFailure = NachoCore.Utils.NcAssert.NachoAssertionFailure;
+using Test.iOS;
 namespace Test.Common
 {
     public class McContactTest : NcTestBase
@@ -24,6 +28,116 @@ namespace Test.Common
             d.Update ();
             var e = McContact.QueryById<McContact> (d.Id);
             Assert.AreEqual (1, e.EmailAddresses.Count);
+        }
+
+        public void CreateFolder ()
+        {
+
+
+        }
+
+        [Test]
+        public void Query_01 ()
+        {
+            // Same name, parent id; different typecodes
+            int accountId = 1;
+            string parentId = "0";
+            string name = "Name";
+
+            TypeCode typeCode2 = TypeCode.DefaultContacts_9;
+
+            FolderOps.CreateFolder (accountId, typeCode: typeCode2, parentId: parentId, name: name);
+
+            McFolder expected2 = McFolder.GetUserFolders (accountId, typeCode2, parentId.ToInt (), name).First();
+
+            var c = new McContact ();
+            c.AccountId = 1;
+            c.FirstName = "Bob";
+            c.LastName = "Smith";
+            c.AddEmailAddressAttribute (c.AccountId, "bob", "home", "bob@foo.com");
+            c.AddPhoneNumberAttribute (c.AccountId, "mobile", null, "123");
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.LastName = "Adleman";
+            c.AddEmailAddressAttribute (c.AccountId, "aaron", "home", "aaron@foo.com");
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.FirstName = "Charlie";
+            c.LastName = "Clark";
+            c.AddEmailAddressAttribute (c.AccountId, "Charlie", "home", "charlie@foo.com");
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.FirstName = "David";
+            c.LastName = "Dark";
+            c.AddPhoneNumberAttribute (c.AccountId, "mobile", null, "123");
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.AddEmailAddressAttribute (c.AccountId, "Eddie", "home", "eddie@foo.com");
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.AddPhoneNumberAttribute (c.AccountId, "mobile", null, "1234567890");
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.FirstName = "Gary";
+            c.LastName = "Glitter";
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.FirstName = "Ingrid";
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.LastName = "Holmes";
+            c.Insert ();
+            expected2.Link (c);
+
+            c = new McContact ();
+            c.AccountId = 1;
+            c.Insert ();
+            expected2.Link (c);
+
+            var l = McContact.AllContactsSortedByName ();
+            var m = McContact.AllContactsSortedByName (1);
+            Assert.AreEqual (l.Count, m.Count);
+            Assert.AreEqual (10, l.Count);
+
+            for (int i = 0; i < l.Count; i++) {
+                Assert.AreEqual (l [i].Id, m [i].Id);
+            }
+
+            Assert.AreEqual ("#", l [0].FirstLetter);
+            Assert.AreEqual ("#", l [1].FirstLetter);
+            Assert.AreEqual ("Adleman", l [2].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("Bob Smith", l [3].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("Charlie Clark", l [4].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("David Dark", l [5].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("eddie@foo.com", l [6].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("Gary Glitter", l [7].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("Holmes", l [8].GetContact ().GetDisplayNameOrEmailAddress ());
+            Assert.AreEqual ("Ingrid", l [9].GetContact ().GetDisplayNameOrEmailAddress ());
+
         }
     }
 }
