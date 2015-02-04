353b9728fc1897ac5d902a790a1c0d09241d406f
Henry Kwok <henryk@nachocove.com>
2015-02-04 00:48:35 -0800

    Add unit tests for new delete methods for McMapEmailAddressEntry

--- Test.Android/McMapEmailAddressEntryTest.cs
+++ Test.Android/McMapEmailAddressEntryTest.cs
@@ -21,6 +21,27 @@ namespace Test.Common
             }
         }
 
+        private void CheckIdentical (McMapEmailAddressEntry a, McMapEmailAddressEntry b)
+        {
+            Assert.AreEqual (a.Id, b.Id);
+            Assert.AreEqual (a.AddressType, b.AddressType);
+            Assert.AreEqual (a.EmailAddressId, b.EmailAddressId);
+            Assert.AreEqual (a.ObjectId, b.ObjectId);
+        }
+
+        private void CheckNotExist (int mapId)
+        {
+            var map = McMapEmailAddressEntry.QueryById<McMapEmailAddressEntry> (mapId);
+            Assert.AreEqual (null, map);
+        }
+
+        private void CheckExist (McMapEmailAddressEntry expected)
+        {
+            var map = McMapEmailAddressEntry.QueryById<McMapEmailAddressEntry> (expected.Id);
+            Assert.AreNotEqual (null, map);
+            CheckIdentical (expected, map);
+        }
+
         [Test]
         public void EmailMessageIdsToEmailAddressIds ()
         {
@@ -159,6 +180,100 @@ namespace Test.Common
             // Message ids with From address 4 - None
             idList = McMapEmailAddressEntry.QueryMessageIdsByFromAddress (AccountId, 4);
             Assert.AreEqual (0, idList.Count);
+        }
+
+        [Test]
+        public void DeleteEntries ()
+        {
+            List<McMapEmailAddressEntry> entries = new List<McMapEmailAddressEntry> () {
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.To,
+                    ObjectId = 1,
+                    EmailAddressId = 2,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.From,
+                    ObjectId = 1,
+                    EmailAddressId = 1,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.Cc,
+                    ObjectId = 1,
+                    EmailAddressId = 4,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.Sender,
+                    ObjectId = 1,
+                    EmailAddressId = 5,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.To,
+                    ObjectId = 2,
+                    EmailAddressId = 3,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.Optional,
+                    ObjectId = 3,
+                    EmailAddressId = 1,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.Required,
+                    ObjectId = 4,
+                    EmailAddressId = 6,
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.Resource,
+                    ObjectId = 5,
+                    EmailAddressId = 7
+                },
+                new McMapEmailAddressEntry () {
+                    AccountId = AccountId,
+                    AddressType = NcEmailAddress.Kind.Optional,
+                    ObjectId = 6,
+                    EmailAddressId = 8
+                }
+            };
+            InsertList (entries);
+
+            // Verify that every object now has a valid (> 0) id
+            foreach (var e in entries) {
+                Assert.True (0 < e.Id);
+            }
+
+            // Delete all email message 1 map entries. Verify that 1st 4 entries are gone
+            McMapEmailAddressEntry.DeleteMessageMapEntries (AccountId, entries [0].ObjectId);
+            for (int n = 0; n < 4; n++) {
+                CheckNotExist (entries [n].Id);
+            }
+
+            // Verify that email message 2 map is still around
+            CheckExist (entries [4]);
+
+            // Delete just From address map for email 2. Verify the entry is still there
+            McMapEmailAddressEntry.DeleteMapEntries (AccountId, entries [4].Id, NcEmailAddress.Kind.From);
+            CheckExist (entries [4]);
+
+            // Delete the To address map. The entry should be gone
+            McMapEmailAddressEntry.DeleteMapEntries (AccountId, entries [4].ObjectId, NcEmailAddress.Kind.To);
+            CheckNotExist (entries [4].Id);
+
+            // Delete all maps for attendees 3, 4, and 5
+            for (int m = 5; m < 8; m++) {
+                CheckExist (entries [m]);
+                McMapEmailAddressEntry.DeleteAttendeeMapEntries (AccountId, entries [m].ObjectId);
+                CheckNotExist (entries [m].Id);
+                for (int n = (m + 1); n < entries.Count; n++) {
+                    CheckExist (entries [n]);
+                }
+            }
         }
     }
 }
