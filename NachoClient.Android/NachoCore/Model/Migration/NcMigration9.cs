e3d09eb90cff5dc56a56ba79ff23b5b22065b83c
Henry Kwok <henryk@nachocove.com>
2015-03-07 21:16:45 -0800

    Fix nachocove/qa#99.
    Contact edit view creates a contact copy but does not copy the source type. So, it creates a contact with source type of Unknown. Copy the source as well. Create a migration to set all Unknown source to ActiveSync.

---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration9.cs
@@ -0,0 +1,24 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Model
+{
+    public class NcMigration9 : NcMigration
+    {
+        public override int GetNumberOfObjects ()
+        {
+            return Db.Table<McContact> ().Where (x => McAbstrItem.ItemSource.Unknown == x.Source).Count ();
+        }
+
+        public override void Run (System.Threading.CancellationToken token)
+        {
+            foreach (var contact in Db.Table<McContact> ().Where (x => McAbstrItem.ItemSource.Unknown == x.Source)) {
+                token.ThrowIfCancellationRequested ();
+                contact.Source = McAbstrItem.ItemSource.ActiveSync;
+                contact.Update ();
+                UpdateProgress (1);
+            }
+        }
+    }
+}
