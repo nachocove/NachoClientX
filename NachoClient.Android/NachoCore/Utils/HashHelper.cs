1b2df1c7be68b95e1a6d40ff3656be5c56959eb7
azimnacho <azimo@nachocove.com>
2015-03-18 15:28:50 -0700

    changed email reggex again to conform to
    https://msdn.microsoft.com/en-us/library/01escwtf%28v=vs.110%29.aspx
    not sure this is the right approach given that the reggae matches more
    of the url than it should if it is not bounded by ‘User=‘ and ‘&’

--- NachoClient.Android/NachoCore/Utils/HashHelper.cs
+++ NachoClient.Android/NachoCore/Utils/HashHelper.cs
@@ -24,11 +24,13 @@ namespace NachoCore.Utils
 
         public static string HashEmailAddressesInString (string value)
         {
-            string emailPattern = "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]+\\b";
-            string hashed = Regex.Replace(value, emailPattern, delegate(Match match)
+            string hashed = Regex.Replace(value,
+                @"User=(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))&",
+                delegate(Match match)
                 {
                     // not doing a short hash here since email address can be long
-                    return HashHelper.Sha256 (match.ToString());
+                    return ("User=" + HashHelper.Sha256 (match.ToString().Substring(5,match.ToString().Length-6)) + "&");
                 }, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
             return hashed;
         }
--- Test.iOS/HashHelperTest.cs
+++ Test.iOS/HashHelperTest.cs
@@ -8,7 +8,13 @@ namespace Test.iOS
     [TestFixture]
     public class HashHelperTest 
     {
-        public static string[] emailAddresses = new string[] {"johnq@nachocove.com", "Peter@smithsonian.museum", "philip@us.gov", "ramesh@timesofindia.in", "tester@d2.officeburrito.com"};
+        public static string[] emailAddresses = new string[]  { 
+            "david.jones@proseware.com", "d.j@server1.proseware.com",
+            "jones@ms1.proseware.com",
+            "j@proseware.com9", "js#internal@proseware.com",
+            "j_9@[129.126.118.1]",
+            "js@proseware.com9", "j.s@server1.proseware.com",
+            "\"j\\\"s\\\"\"@proseware.com", "jēn@späm.de"};
         public static string urlPrefixFrag = "https://mail.d2.officeburrito.com/Microsoft-Server-ActiveSync?Cmd=ItemOperations&User=";
         public static string urlSuffixFrag = "&DeviceId=Nchob8f6b1150c41&DeviceType=iPhone";
 
