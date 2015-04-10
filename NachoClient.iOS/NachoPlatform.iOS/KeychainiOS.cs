3424c74c6d1be5de99b47ac787bc97768b616684
azimnacho <azimo@nachocove.com>
2015-04-10 13:34:38 -0700

    added comment to remove the log code before releasing to app store.

--- NachoClient.iOS/NachoPlatform.iOS/KeychainiOS.cs
+++ NachoClient.iOS/NachoPlatform.iOS/KeychainiOS.cs
@@ -64,6 +64,7 @@ namespace NachoPlatform
                 // return match.ValueData.ToString ();
             } else {
                 Log.Error (Log.LOG_SYS, "GetPassword: SecKeyChain.QueryAsRecord returned status {0} for handle {1}", res.ToString (), handle);
+                // TODO : remove this before Appstore
                 if (match != null) {
                     Log.Error (Log.LOG_SYS, "GetPassword: SecKeyChain.QueryAsRecord returned value {0} for handle {1}", match.ValueData.ToString (), handle);
                 }
