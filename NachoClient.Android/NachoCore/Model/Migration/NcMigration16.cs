745cdb5e77ea6b71ef33f72d2718cfef4d9945a7
Steve Scalpone <sscalpone@gmail.com>
2015-04-10 13:48:22 -0700

    Add per-account setting screen to configure notification delivery.

---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration16.cs
@@ -0,0 +1,22 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Model
+{
+    public class NcMigration16 : NcMigration
+    {
+        public override int GetNumberOfObjects ()
+        {
+            return 1;
+        }
+
+        public override void Run (System.Threading.CancellationToken token)
+        {
+            NcModel.Instance.Db.Execute (
+                "UPDATE McAccount SET NotificationConfiguration = ?", (int)McAccount.DefaultNotificationConfiguration);
+            UpdateProgress (1);
+        }
+    }
+}
+
