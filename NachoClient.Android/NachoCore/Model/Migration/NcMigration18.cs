1c6c30fc2580499978ba89d74c4da33e38160d1c
Henry Kwok <henryk@nachocove.com>
2015-04-16 14:25:25 -0700

    Fix nachocove/qa#271. Rebase McBrainEvent from McAbstrObjectPerAcc. Add migration to fill in account id.

---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration18.cs
@@ -0,0 +1,30 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using NachoCore.Brain;
+
+namespace NachoCore.Model
+{
+    public class NcMigration18 : NcMigration
+    {
+        public override int GetNumberOfObjects ()
+        {
+            return Db.Table<McBrainEvent> ().Count ();
+        }
+
+        public override void Run (System.Threading.CancellationToken token)
+        {
+            foreach (var dbEvent in Db.Table<McBrainEvent>()) {
+                var brainEvent = dbEvent.BrainEvent () as NcBrainMessageEvent;
+                if (null == brainEvent) {
+                    dbEvent.AccountId = NcBrainEvent.KNotSpecificAccountId;
+                } else {
+                    dbEvent.AccountId = (int)brainEvent.AccountId;
+                }
+                dbEvent.Update ();
+                UpdateProgress (1);
+            }
+        }
+    }
+}
+
