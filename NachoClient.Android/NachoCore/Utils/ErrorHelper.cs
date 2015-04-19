33941facf6afa45c93b530c5a7f9f31475f649af
Steve Scalpone <sscalpone@gmail.com>
2015-04-19 14:31:16 -0700

    Placeholder to EmailHelper.cs

---
+++ NachoClient.Android/NachoCore/Utils/ErrorHelper.cs
@@ -0,0 +1,14 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Utils
+{
+    public class ErrorHelper
+    {
+        public ErrorHelper ()
+        {
+        }
+    }
+}
+
