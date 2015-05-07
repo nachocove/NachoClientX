1be9582063af0c154837517d964b709750ac38d1
Jan Vilhuber <jan@vilhuber.com>
2015-05-07 14:28:26 -0600

    initial imap files

---
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/ImProtoControl/IImStrategy.cs
@@ -0,0 +1,11 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Imap
+{
+    public interface IImStrategy
+    {
+    }
+}
+
---
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/ImProtoControl/ImStrategy.cs
@@ -0,0 +1,27 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore.Imap
+{
+    public class ImStrategy : IImStrategy
+    {
+        // TODO This is pasted from AsStrategy. Abstract this?
+        public enum LadderChoiceEnum
+        {
+            Production,
+            Test,
+        };
+        private IBEContext BEContext;
+
+        public ImStrategy (IBEContext beContext, LadderChoiceEnum ladder)
+        {
+        }
+
+        public ImStrategy (IBEContext beContext) : this (beContext, LadderChoiceEnum.Production)
+        {
+        }
+
+    }
+}
+
