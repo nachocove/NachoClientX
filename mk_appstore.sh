1c89bb99b5af9157b62ccdf08674a2224d192f5c
Henry Kwok <henryk@nachocove.com>
2015-05-11 01:36:23 -0700

    Switch to use repos.py in mk scirpt. Remove helper shell scripts that are now obsoleted by repos.py.

--- mk_appstore.sh
+++ mk_appstore.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 
-# USAGE: mk_appstore.sh [VERSION] [BUILD]
+# USAGE: mk_appstore.sh [BRANCH] [VERSION] [BUILD]
 #
-# For example, mk_appstore.sh 0.9 123 or mk_appstore.sh 1.0.appstore 321
+# For example, mk_appstore.sh master 0.9 123 or mk_appstore.sh throttle_v1.0 1.0.appstore 321
 #
-# VERSION and BUILD are both strings. They preferably have no space. But if they
+# BRANCH, VERSION, and BUILD are all strings. They preferably have no space. But if they
 # do contain spaces, please use ".
 
-if [ $# -ne 2 ]; then
-   echo "USAGE: mk_appstore.sh [VERSION] [BUILD]"
+if [ $# -ne 3 ]; then
+   echo "USAGE: mk_appstore.sh [BRANCH] [VERSION] [BUILD]"
+   echo "\nFor example,"
+   echo "mk_appstore.sh throttle_v1.0 1.0.beta 321 - build 1.0(321) off throttle_v1.0\n"
    exit 1
 fi
+branch=$1
+version=$2
+build=$3
+tag="v$version_$build"
 
-# Fetch all git repos
+die () {
+  echo "ERROR: $1"
+  exit 1
+}
+
+# Fetch all git repos and check out the tag
 source repos.sh
 ./fetch.py $repos
-
-# Tag all repos
-tag="v$1_$2"
-sh checkout_tag.sh "$tag"
+./scripts/repos.py checkout-tag --tag "$tag" || die "fail to switch to tag $tag"
 
 # Build everything else
 timestamp=`date "+%Y%m%d_%H%M%S"`
@@ -31,7 +39,7 @@ then
 fi
 
 # Build NachoClient
-VERSION="$1" BUILD="$2" RELEASE="appstore" /Applications/Xamarin\ Studio.app/Contents/MacOS/XamarinStudio ./NachoClient.sln
+VERSION="$version" BUILD="$build" RELEASE="appstore" /Applications/Xamarin\ Studio.app/Contents/MacOS/XamarinStudio ./NachoClient.sln
 if [ $? -eq 0 ]
 then
     echo "appstore build $tag is made."
