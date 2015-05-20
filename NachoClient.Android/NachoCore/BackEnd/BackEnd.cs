ed7db07ae470523cd8715a8fa0f099166f7f5474
Jeff Enderwick <jeff.enderwick@gmail.com>
2015-05-20 15:52:08 -0700

    small changes, logging, strategy base class

--- NachoClient.Android/NachoCore/BackEnd/BackEnd.cs
+++ NachoClient.Android/NachoCore/BackEnd/BackEnd.cs
@@ -77,12 +77,12 @@ namespace NachoCore
             NcAssert.True (0 != accountId, "0 != accountId");
             ConcurrentQueue<NcProtoControl> services;
             if (!Services.TryGetValue (accountId, out services)) {
-                Log.Error (Log.LOG_SYS, "ServiceFromAccountId called with bad accountId {0} @ {1}", accountId, new StackTrace ());
+                Log.Error (Log.LOG_BACKEND, "ServiceFromAccountId called with bad accountId {0} @ {1}", accountId, new StackTrace ());
                 return NcResult.Error (NcResult.SubKindEnum.Error_AccountDoesNotExist);
             }
             var protoControl = services.FirstOrDefault (x => capability == (x.Capabilities & capability));
             if (null == protoControl) {
-                Log.Error (Log.LOG_SYS, "ServiceFromAccountId: can't find controller with desired capability {0}", capability);
+                Log.Error (Log.LOG_BACKEND, "ServiceFromAccountId: can't find controller with desired capability {0}", capability);
                 return NcResult.Error (NcResult.SubKindEnum.Error_NoCapableService);
             }
             return func (protoControl);
@@ -101,12 +101,12 @@ namespace NachoCore
                     }
                 }
                 if (result.isOK ()) {
-                    Log.Info (Log.LOG_LIFECYCLE, "{0}({1})", name, accountId);
+                    Log.Info (Log.LOG_BACKEND, "{0}({1})", name, accountId);
                 } else {
-                    Log.Warn (Log.LOG_LIFECYCLE, "BackEnd.ApplyAcrossServices {0}({1}):{2}.", name, accountId, result.Message);
+                    Log.Warn (Log.LOG_BACKEND, "BackEnd.ApplyAcrossServices {0}({1}):{2}.", name, accountId, result.Message);
                 }
             } else {
-                Log.Warn (Log.LOG_LIFECYCLE, "BackEnd.ApplyAcrossServices {0}({1}) could not find services.", name, accountId);
+                Log.Warn (Log.LOG_BACKEND, "BackEnd.ApplyAcrossServices {0}({1}) could not find services.", name, accountId);
             }
             return result;
         }
@@ -139,7 +139,7 @@ namespace NachoCore
 
         public void Start ()
         {
-            Log.Info (Log.LOG_LIFECYCLE, "BackEnd.Start() called");
+            Log.Info (Log.LOG_BACKEND, "BackEnd.Start() called");
             // The callee does Task.Run.
             ApplyAcrossAccounts ("Start", (accountId) => {
                 Start (accountId);
@@ -151,7 +151,7 @@ namespace NachoCore
         // return without waiting.
         public void Stop ()
         {
-            Log.Info (Log.LOG_LIFECYCLE, "BackEnd.Stop() called");
+            Log.Info (Log.LOG_BACKEND, "BackEnd.Stop() called");
             ApplyAcrossAccounts ("Stop", (accountId) => {
                 Stop (accountId);
             });
@@ -196,10 +196,10 @@ namespace NachoCore
                 NcAssert.True (false);
                 break;
             }
-            Log.Info (Log.LOG_LIFECYCLE, "CreateServices {0}", accountId);
+            Log.Info (Log.LOG_BACKEND, "CreateServices {0}", accountId);
             if (!Services.TryAdd (accountId, services)) {
                 // Concurrency. Another thread has jumped in and done the add.
-                Log.Info (Log.LOG_LIFECYCLE, "Another thread has already called CreateServices for Account.Id {0}", accountId);
+                Log.Info (Log.LOG_BACKEND, "Another thread has already called CreateServices for Account.Id {0}", accountId);
             }
         }
 
@@ -214,7 +214,7 @@ namespace NachoCore
 
         public void Start (int accountId)
         {
-            Log.Info (Log.LOG_LIFECYCLE, "BackEnd.Start({0}) called", accountId);
+            Log.Info (Log.LOG_BACKEND, "BackEnd.Start({0}) called", accountId);
             NcCommStatus.Instance.Refresh ();
             if (!AccountHasServices (accountId)) {
                 CreateServices (accountId);
@@ -225,7 +225,7 @@ namespace NachoCore
                     return NcResult.OK ();
                 });
             }, "Start");
-            Log.Info (Log.LOG_LIFECYCLE, "BackEnd.Start({0}) exited", accountId);
+            Log.Info (Log.LOG_BACKEND, "BackEnd.Start({0}) exited", accountId);
         }
 
         public void CertAskResp (int accountId, McAccount.AccountCapabilityEnum capabilities, bool isOkay)
