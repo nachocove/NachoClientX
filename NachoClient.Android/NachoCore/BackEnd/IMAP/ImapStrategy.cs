9c5aeb76017884e90f8556dfbf0e69b2400bb9d3
Jan Vilhuber <jan@vilhuber.com>
2015-06-11 09:51:20 -0600

    add comment for function

--- NachoClient.Android/NachoCore/BackEnd/IMAP/ImapStrategy.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/ImapStrategy.cs
@@ -53,6 +53,47 @@ namespace NachoCore.IMAP
             return KBaseNoIdlePollTime;
         }
 
+        /// <summary>
+        /// GenSyncKit generates a data structure (SyncKit) that contains parameters and values
+        /// needed for the BE to do a sync with the server.
+        /// </summary>
+        /// <param name="accountId">The account Id.</param>
+        /// <param name="protocolState">The protocol state.</param>
+        /// <param name="folder">The folder to sync.</param>
+        /// <param name="UserRequested">Whether this is a user-requested action.</param>
+        /// <remarks>
+        /// This function reads folder.ImapUidHighestUidSynced and folder.ImapUidLowestUidSynced
+        /// (and other values), but does NOT SET THEM. When the sync is executed (via ImapSymcCommand),
+        /// it will set folder.ImapUidHighestUidSynced and folder.ImapUidLowestUidSynced. Next time
+        /// GenSyncKit is called, these values are used to create the next SyncKit for ImapSyncCommand
+        /// to consume.
+        /// 
+        /// The current 'strategy' (I use the term lightly) is as follows:
+        ///   - If the folder has never been sync'd (or hasn't been sync'd in a while), we tell
+        ///     ImapSyncCommand to merely open the folder and record the current state (mainly UIDNEXT).
+        ///     folder.ImapUidHighestUidSynced and folder.ImapUidLowestUidSynced will remain unchanged at
+        ///     their default values.
+        ///   - Once we know UIDNEXT, starting with the newest X messages, where X is expected to be small.
+        ///     This is to get us going fast and have something to display in the UI. Currently we use 10.
+        ///     ImapSyncCommand sets folder.ImapUidHighestUidSynced to UIDNEXT-1, since that's the highest
+        ///     UID we've sync'd. ImapSyncCommand also sets folder.ImapUidLowestUidSynced to the lowest
+        ///     we've sync'd to now (presumably UIDNEXT-11, but that depends on how many messages there are)
+        ///   - After that, we start syncing from folder.ImapUidLowestUidSynced downwards, in chunks of 
+        ///     'span' (which depends on NcCommStatus). Unless new mail comes in, ImapUidHighestUidSynced
+        ///     is unlikely to change.
+        ///   - When new mail comes in, UIDNEXT will be higher than ImapUidHighestUidSynced, so we again try
+        ///     to sync the newest messages.
+        ///   - After new mail has been processed, we can continue syncing 'downwards', knowing where we
+        ///     left off via ImapUidLowestUidSynced.
+        /// 
+        /// There is a bug with this approach, however, in that the two variables are not enough to keep
+        /// track of all possible cases. In the case where the number of new emails that have arrived is
+        /// > than the span selected, we would sync the newest messages, but since we don't want to move 
+        /// ImapUidLowestUidSynced (because then we'd lose track of where to continue syncing older messages),
+        /// we have no way of keeping track of the downward progress of the 'new mail' sync. So instead,
+        /// for now, we sync from ImapUidHighestUidSynced upwards, rather then from UIDNEXT downwards.
+        /// This is not optimal and will be changed in the future.
+        /// </remarks>
         public SyncKit GenSyncKit (int accountId, McProtocolState protocolState, McFolder folder, bool UserRequested = false)
         {
             if (folder.ImapNoSelect) {
