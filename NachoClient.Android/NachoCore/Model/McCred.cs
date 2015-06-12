e5517f940fe10c8e55b9f3a234749568e9f9a386
Jeff Enderwick <jeff.enderwick@gmail.com>
2015-06-12 15:42:55 -0700

    cred-req driven OAUTH2 refresh has been seen to work

--- NachoClient.Android/NachoCore/Model/McCred.cs
+++ NachoClient.Android/NachoCore/Model/McCred.cs
@@ -1,5 +1,11 @@
 using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
 using SQLite;
+using ModernHttpClient;
+using Newtonsoft.Json;
 using NachoCore.Utils;
 using NachoPlatform;
 
@@ -152,6 +158,79 @@ namespace NachoCore.Model
                 Update ();
             } 
             return base.Delete ();
+        }
+
+        public bool AttemptRefresh (Action<McCred> onSuccess, Action<McCred> onFailure)
+        {
+            switch (CredType) {
+            case CredTypeEnum.Password:
+                return false;
+            case CredTypeEnum.OAuth2:
+                var account = McAccount.QueryById<McAccount> (AccountId);
+                if (account.AccountService == McAccount.AccountServiceEnum.GoogleDefault) {
+                    RefreshOAuth2Google (onSuccess, onFailure);
+                    return true;
+                }
+                Log.Error (Log.LOG_BACKEND, "Unable to do OAUTH2 refresh for {0}", account.AccountService.ToString ());
+                return false;
+            default:
+                NcAssert.CaseError (CredType.ToString ());
+                return false;
+            }
+        }
+
+        public static Type HttpClientType = typeof(MockableHttpClient);
+
+        private class OAuth2RefreshRespose
+        {
+            public string access_token { get; set; }
+            public string expires_in { get; set; }
+            public string token_type { get; set; }
+        }
+
+        private async Task<bool> TryRefresh ()
+        {
+            var handler = new NativeMessageHandler ();
+            var client = (IHttpClient)Activator.CreateInstance (HttpClientType, handler, true);
+            var query = "client_secret=" + "3NKmUHorCm8_IS4lkOWuN_7i" +
+                        "&grant_type=" + "refresh_token" +
+                        "&refresh_token=" + GetRefreshToken () +
+                        "&client_id=" + "135541750674-l44k46h09u2obl3upnchl9lt3nicfd52.apps.googleusercontent.com";
+            var requestUri = new Uri ("https://www.googleapis.com/oauth2/v3/token" + "?" + query);
+            var httpRequest = new HttpRequestMessage (HttpMethod.Post, requestUri);
+            try {
+                var httpResponse = await client.SendAsync (httpRequest, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
+                if (httpResponse.StatusCode == System.Net.HttpStatusCode.OK) {
+                    var jsonResponse = await httpResponse.Content.ReadAsStringAsync ().ConfigureAwait (false);
+                    var response = JsonConvert.DeserializeObject<OAuth2RefreshRespose> (jsonResponse);
+                    if ("Bearer" != response.token_type) {
+                        Log.Error (Log.LOG_SYS, "Unknown OAUTH2 token_type {0}", response.token_type);
+                    }
+                    if (null == response.access_token || null == response.expires_in) {
+                        Log.Error (Log.LOG_SYS, "Missing OAUTH2 access_token {0} or expires_in {1}", response.access_token, response.expires_in);
+                        return false;
+                    }
+                    UpdateOauth2 (response.access_token, GetRefreshToken (), 
+                        DateTime.UtcNow.AddSeconds (int.Parse (response.expires_in)));
+                    return true;
+                } else {
+                    Log.Error (Log.LOG_SYS, "OAUTH2 HTTP Status {0}", httpResponse.StatusCode.ToString ());
+                    return false;
+                }
+            } catch (Exception ex) {
+                Log.Error (Log.LOG_SYS, "OAUTH2 Exception {0}", ex.ToString ());
+                return false;
+            }
+        }
+
+        private async void RefreshOAuth2Google (Action<McCred> onSuccess, Action<McCred> onFailure)
+        {    
+            bool result = await TryRefresh ();
+            if (result) {
+                onSuccess (this);
+            } else {
+                onFailure (this);
+            }
         }
     }
 }
