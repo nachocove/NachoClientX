1f2d8159d818efa1b905e974b0c5b048bb7d3518
Jeff Enderwick <jeff.enderwick@gmail.com>
2015-06-12 09:25:32 -0700

    added McCred migration

---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration30.cs
@@ -0,0 +1,23 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using System.Threading;
+
+namespace NachoCore.Model
+{
+    public class NcMigration30 : NcMigration
+    {
+        public override int GetNumberOfObjects ()
+        {
+            return Db.Table<McCred> ().Count ();
+        }
+
+        public override void Run (CancellationToken token)
+        {
+            foreach (var cred in Db.Table<McCred> ()) {
+                cred.CredType = McCred.CredTypeEnum.Password;
+                cred.Update ();
+            }
+        }
+    }
+}
