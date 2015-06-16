e4373c061d49208b7362e4e10079c5891e4042dc
Jeff Enderwick <jeff.enderwick@gmail.com>
2015-06-15 21:21:59 -0700

    empty class

---
+++ NachoClient.Android/NachoCore/Adapters/NcMdmConfig.cs
@@ -0,0 +1,29 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+
+namespace NachoCore
+{
+    public class NcMdmConfig
+    {
+        private static volatile NcMdmConfig instance;
+        private static object syncRoot = new Object ();
+
+        public static NcMdmConfig Instance {
+            get {
+                if (instance == null) {
+                    lock (syncRoot) {
+                        if (instance == null) {
+                            instance = new NcMdmConfig ();
+                        }
+                    }
+                }
+                return instance; 
+            }
+        }
+
+        private NcMdmConfig ()
+        {
+        }
+    }
+}
