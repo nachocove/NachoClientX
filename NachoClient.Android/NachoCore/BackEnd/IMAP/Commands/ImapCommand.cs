279f44c04f76b7943b49ad7cef50a31bf4fad121
Jan Vilhuber <jan@vilhuber.com>
2015-06-17 17:30:12 -0600

    add a better log call (but leave it commented out.

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapCommand.cs
@@ -103,6 +103,7 @@ namespace NachoCore.IMAP
 
             byte[] requestData;
             byte[] responseData;
+            //Log.Info (Log.LOG_IMAP, "IMAP exchange\n{0}", Encoding.UTF8.GetString (Client.ProtocolLogger.GetCombinedBuffer ()));
             Client.ProtocolLogger.Stop (out requestData, out responseData);
             byte[] ClassNameBytes = Encoding.UTF8.GetBytes (ClassName + "\n");
 
