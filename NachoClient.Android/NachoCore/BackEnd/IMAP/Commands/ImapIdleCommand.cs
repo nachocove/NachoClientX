fc1c667a88582d9a683b821db5f6bf9f36f3e1ba
Jan Vilhuber <jan@vilhuber.com>
2015-06-22 13:44:11 -0600

    add some handlers to idle

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapIdleCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapIdleCommand.cs
@@ -28,17 +28,48 @@ namespace NachoCore.IMAP
             IMailFolder mailKitFolder;
             bool mailArrived = false;
             var done = CancellationTokenSource.CreateLinkedTokenSource (new [] { Cts.Token });
-            EventHandler<MessagesArrivedEventArgs> messageHandler = (sender, maea) => {
+
+            // TODO Add handlers for folder deletion, folder rename, etc.
+
+            EventHandler<MessagesArrivedEventArgs> CountChangedMessageHandler = (sender, e) => {
                 mailArrived = true;
                 done.Cancel ();
             };
+            EventHandler<MessageEventArgs> ExpungedMessageHandler = (sender, e) => {
+                // Sadly, the Expunged message only passes an ID, not a UID. In the absence of keeping the ID in our DB, we just
+                // have to do a sync.
+                Log.Info (Log.LOG_IMAP, "Message expunged");
+                done.Cancel ();
+            };
+            EventHandler<MessageFlagsChangedEventArgs> MessageFlagsChangedHandler = (sender, e) => {
+                var mkFolder = (ImapFolder) sender;
+
+                McFolder folder = McFolder.QueryByServerId<McFolder> (BEContext.Account.Id, mkFolder.FullName);
+                if (!e.UniqueId.HasValue) {
+                    Log.Warn (Log.LOG_IMAP, "{0}: flags for message Index {1} have changed to: {2}. No UID passed.", folder.ImapFolderNameRedacted (), e.Index, e.Flags);
+                } else {
+                    Log.Info (Log.LOG_IMAP, "{0}: flags for message {1} have changed to: {2}.", folder.ImapFolderNameRedacted (), e.UniqueId, e.Flags);
+                    McEmailMessage emailMessage = McEmailMessage.QueryByServerId<McEmailMessage> (BEContext.Account.Id, ImapProtoControl.MessageServerId (folder, e.UniqueId.Value));
+                    if (null != emailMessage) {
+                        if (emailMessage.IsRead != e.Flags.HasFlag (MessageFlags.Seen)) {
+                            emailMessage = emailMessage.UpdateWithOCApply<McEmailMessage> ((record) => {
+                                var target = (McEmailMessage)record;
+                                target.IsRead = e.Flags.HasFlag (MessageFlags.Seen);
+                                return true;
+                            });
+                            BEContext.ProtoControl.StatusInd (NcResult.Info (NcResult.SubKindEnum.Info_EmailMessageSetChanged));
+                        }
+                    }
+                }
+            };
+
             lock (Client.SyncRoot) {
                 mailKitFolder = Client.GetFolder (IdleFolder.ServerId, Cts.Token);
-                NcAssert.NotNull (mailKitFolder);
-            }
-            try {
-                mailKitFolder.MessagesArrived += messageHandler;
-                lock (Client.SyncRoot) {
+                try {
+                    mailKitFolder.MessagesArrived += CountChangedMessageHandler;
+                    mailKitFolder.MessageFlagsChanged += MessageFlagsChangedHandler;
+                    mailKitFolder.MessageExpunged += ExpungedMessageHandler;
+
                     if (FolderAccess.None == mailKitFolder.Open (FolderAccess.ReadOnly, Cts.Token)) {
                         return Event.Create ((uint)SmEvt.E.HardFail, "IMAPSYNCNOOPEN");
                     }
@@ -53,24 +84,26 @@ namespace NachoCore.IMAP
                         StatusItems.UidValidity |
                         StatusItems.HighestModSeq;
                     mailKitFolder.Status (statusItems, Cts.Token);
-                }
-                UpdateImapSetting (mailKitFolder, IdleFolder);
+                    UpdateImapSetting (mailKitFolder, IdleFolder);
 
-                var protocolState = BEContext.ProtocolState;
-                protocolState = protocolState.UpdateWithOCApply<McProtocolState> ((record) => {
-                    var target = (McProtocolState)record;
-                    target.LastPing = DateTime.UtcNow;
-                    return true;
-                });
-                if (mailArrived) {
-                    Log.Info (Log.LOG_IMAP, "New mail arrived during idle");
+                    var protocolState = BEContext.ProtocolState;
+                    protocolState = protocolState.UpdateWithOCApply<McProtocolState> ((record) => {
+                        var target = (McProtocolState)record;
+                        target.LastPing = DateTime.UtcNow;
+                        return true;
+                    });
+                    if (mailArrived) {
+                        Log.Info (Log.LOG_IMAP, "New mail arrived during idle");
+                    }
+                    return Event.Create ((uint)SmEvt.E.Success, "IMAPIDLEDONE");
+                } catch {
+                    throw;
+                } finally {
+                    mailKitFolder.MessagesArrived -= CountChangedMessageHandler;
+                    mailKitFolder.MessageFlagsChanged -= MessageFlagsChangedHandler;
+                    mailKitFolder.MessageExpunged -= ExpungedMessageHandler;
+                    done.Dispose ();
                 }
-                return Event.Create ((uint)SmEvt.E.Success, "IMAPIDLEDONE");
-            } catch {
-                throw;
-            } finally {
-                mailKitFolder.MessagesArrived -= messageHandler;
-                done.Dispose ();
             }
         }
     }
