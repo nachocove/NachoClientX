51e84f053e2d94fe95be72f0a27b55c375b011c8
Jan Vilhuber <jan@vilhuber.com>
2015-06-22 18:08:29 -0600

    fix iCloud login

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapAuthenticateCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapAuthenticateCommand.cs
@@ -5,6 +5,7 @@ using NachoCore.Utils;
 using MailKit.Net.Imap;
 using NachoCore;
 using NachoCore.Model;
+using MailKit.Security;
 
 namespace NachoCore.IMAP
 {
@@ -35,7 +36,16 @@ namespace NachoCore.IMAP
                     Client.Authenticate (BEContext.Cred.Username, BEContext.Cred.GetAccessToken (), Cts.Token);
                 } else {
                     Client.AuthenticationMechanisms.Remove ("XOAUTH2");
-                    Client.Authenticate (BEContext.Cred.Username, BEContext.Cred.GetPassword (), Cts.Token);
+                    var username = BEContext.Cred.Username;
+                    if (McAccount.AccountServiceEnum.iCloud == BEContext.Account.AccountService) {
+                        username = username.Split ('@') [0];
+                    }
+                    try {
+                        Client.Authenticate (username, BEContext.Cred.GetPassword (), Cts.Token);
+                    } catch (ImapProtocolException ex) {
+                        // some servers (icloud.com) seem to close the connection on a bad password/username.
+                        throw new AuthenticationException ();
+                    }
                 }
                 Log.Info (Log.LOG_IMAP, "IMAP Server capabilities: {0}", Client.Capabilities.ToString ());
             }
