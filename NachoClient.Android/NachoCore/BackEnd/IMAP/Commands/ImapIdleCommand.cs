6cd4200bee729a335ef47cab7b4e3d7399e3b8ff
Jan Vilhuber <jan@vilhuber.com>
2015-07-14 18:33:02 -0600

    add comment

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapIdleCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapIdleCommand.cs
@@ -52,7 +52,7 @@ namespace NachoCore.IMAP
             if (mailDeleted) {
                 Log.Info (Log.LOG_IMAP, "{0}: Mail Deleted during idle", IdleFolder.ImapFolderNameRedacted ());
             }
-            mailKitFolder.Close (false, Cts.Token);
+            mailKitFolder.Close (false, Cts.Token); // close and then reopen to pull in the latest values
             mailKitFolder = GetOpenMailkitFolder (IdleFolder);
             UpdateImapSetting (mailKitFolder, ref IdleFolder);
             if (mailArrived || mailDeleted || needResync) {
