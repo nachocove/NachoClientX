63859a786c27bb572f41153449a3b728ea460ab8
Jan Vilhuber <jan@vilhuber.com>
2015-07-21 12:26:10 -0600

    make sure to resolve as failed if move fails
    resolves nachocove/qa#666

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailMoveCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailMoveCommand.cs
@@ -43,17 +43,23 @@ namespace NachoCore.IMAP
             McFolder dst = McFolder.QueryByServerId<McFolder> (BEContext.Account.Id, PendingSingle.DestParentId);
             NcAssert.NotNull (dst);
 
-            MoveEmail (emailMessage, src, dst, Cts.Token);
-
-            // FIXME Need to do fixup stuff in pending. Are there API's for that?
-            PendingResolveApply ((pending) => {
-                pending.ResolveAsSuccess (BEContext.ProtoControl, NcResult.Info (NcResult.SubKindEnum.Info_EmailMessageMoveSucceeded));
-            });
-            return Event.Create ((uint)SmEvt.E.Success, "IMAPMOVSUC");
+            var result = MoveEmail (emailMessage, src, dst, Cts.Token);
+            if (result.isOK ()) {
+                // FIXME Need to do fixup stuff in pending. Are there API's for that?
+                PendingResolveApply ((pending) => {
+                    pending.ResolveAsSuccess (BEContext.ProtoControl, NcResult.Info (NcResult.SubKindEnum.Info_EmailMessageMoveSucceeded));
+                });
+                Event evt = result.GetValue<Event> ();
+                return evt;
+            } else {
+                ResolveAllFailed (NcResult.WhyEnum.Unsupported);
+                return Event.Create((uint)SmEvt.E.HardFail, "IMAPMOVHARD");
+            }
         }
 
-        public void MoveEmail(McEmailMessage emailMessage, McFolder src, McFolder dst, CancellationToken Token)
+        public NcResult MoveEmail(McEmailMessage emailMessage, McFolder src, McFolder dst, CancellationToken Token)
         {
+            NcResult result;
             UniqueId? newUid;
             var folderGuid = ImapProtoControl.ImapMessageFolderGuid (emailMessage.ServerId);
             var emailUid = ImapProtoControl.ImapMessageUid (emailMessage.ServerId);
@@ -64,16 +70,23 @@ namespace NachoCore.IMAP
             NcAssert.NotNull (dstFolder);
 
             srcFolder.Open (FolderAccess.ReadWrite, Token);
-            newUid = srcFolder.MoveTo (emailUid, dstFolder, Token);
-            if (null != newUid && newUid.HasValue && 0 != newUid.Value.Id) {
-                emailMessage.UpdateWithOCApply<McEmailMessage> ((record) => {
-                    var target = (McEmailMessage)record;
-                    target.ServerId = ImapProtoControl.MessageServerId (dst, (UniqueId)newUid);
-                    return true;
-                });
-            } else {
-                // FIXME How do we determine the new ID? This can happen with servers that don't support UIDPLUS.
+            try {
+                newUid = srcFolder.MoveTo (emailUid, dstFolder, Token);
+                if (null != newUid && newUid.HasValue && 0 != newUid.Value.Id) {
+                    emailMessage.UpdateWithOCApply<McEmailMessage> ((record) => {
+                        var target = (McEmailMessage)record;
+                        target.ServerId = ImapProtoControl.MessageServerId (dst, (UniqueId)newUid);
+                        return true;
+                    });
+                } else {
+                    // FIXME How do we determine the new ID? This can happen with servers that don't support UIDPLUS.
+                }
+                result = NcResult.OK ();
+                result.Value = Event.Create ((uint)SmEvt.E.Success, "IMAPMOVSUC");
+            } catch (ImapCommandException ex) {
+                result = NcResult.Error (string.Format ("ImapCommandException {0}", ex.Message));
             }
+            return result;
         }
     }
 }
