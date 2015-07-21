d0aefe43be4644d0749a52e63d9507049ac5e265
David Olsen <davido@nachocove.com>
2015-07-21 11:16:22 -0700

    Avoid compiler warning; fix mismatched log message arguments

--- NachoClient.Android/NachoCore/Utils/NcTask.cs
+++ NachoClient.Android/NachoCore/Utils/NcTask.cs
@@ -141,7 +141,7 @@ namespace NachoCore.Utils
                     if (!task.IsCompleted && warnLivedTasks) {
                         Log.Error (Log.LOG_SYS, "Task {0} survives across shutdown", pair.Value);
                     }
-                    Log.Info (Log.LOG_SYS, "Task {0}: IsCompleted={0}, IsCanceled={1}, IsFaulted={2}",
+                    Log.Info (Log.LOG_SYS, "Task {0}: IsCompleted={1}, IsCanceled={2}, IsFaulted={3}",
                         taskName, task.IsCompleted, task.IsCanceled, task.IsFaulted);
                 } catch {
                     // tasks may be going away as we iterate.
