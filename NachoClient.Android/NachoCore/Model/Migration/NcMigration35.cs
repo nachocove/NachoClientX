ea47050fa697ce0a3b8854872b634c9f8f497deb
Jan Vilhuber <jan@vilhuber.com>
2015-07-24 14:25:53 -0600

    fix migration

--- NachoClient.Android/NachoCore/Model/Migration/NcMigration35.cs
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration35.cs
@@ -8,11 +8,13 @@ namespace NachoClient.AndroidClient
 {
     public class NcMigration35 : NcMigration
     {
+        static string querySQLTemplate = "SELECT {0} FROM McEmailMessage WHERE AccountId = ? AND ImapUid <> 0 AND ServerId <> '' AND ServerId IS NOT NULL";
+
         public override int GetNumberOfObjects ()
         {
             int count = 0;
             foreach (var account in NcModel.Instance.Db.Table<McAccount> ().Where (x => x.AccountType == McAccount.AccountTypeEnum.IMAP_SMTP)) {
-                count += NcModel.Instance.Db.Table<McEmailMessage> ().Where (e => e.AccountId == account.Id && e.ImapUid == 0).Count ();
+                count += NcModel.Instance.Db.ExecuteScalar<int> (string.Format (querySQLTemplate, "COUNT(*)"), account.Id);
             }
             return count;
         }
@@ -20,11 +22,10 @@ namespace NachoClient.AndroidClient
         public override void Run (System.Threading.CancellationToken token)
         {
             foreach (var account in NcModel.Instance.Db.Table<McAccount> ().Where (x => x.AccountType == McAccount.AccountTypeEnum.IMAP_SMTP)) {
-                var emails = NcModel.Instance.Db.Table<McEmailMessage> ().Where (e => e.AccountId == account.Id && e.ImapUid == 0);
+                var emails = NcModel.Instance.Db.Query<McEmailMessage> (string.Format (querySQLTemplate, "*"), account.Id);
                 foreach (McEmailMessage email in emails) {
                     token.ThrowIfCancellationRequested ();
-                    Db.Execute ("UPDATE McEmailMessage SET ImapUid = ? WHERE Id = ?",
-                        ImapMessageUid (email.ServerId), email.Id);
+                    Db.Execute ("UPDATE McEmailMessage SET ImapUid = ? WHERE Id = ?", ImapMessageUid (email.ServerId), email.Id);
                 }
             }
         }
