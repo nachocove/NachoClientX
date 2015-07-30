0e50a803f129026fb3231f21ed9329e66bc87274
Jan Vilhuber <jan@vilhuber.com>
2015-07-30 12:22:22 -0600

    remove commented out line.

--- NachoClient.Android/NachoCore/Utils/NcMailKitProtocolLogger.cs
+++ NachoClient.Android/NachoCore/Utils/NcMailKitProtocolLogger.cs
@@ -182,7 +182,6 @@ namespace NachoCore.Utils
             byte[] logData = buffer.Skip (offset).Take (count).ToArray ();
             byte[] timestamp = Encoding.ASCII.GetBytes (String.Format ("{0:yyyy-MM-ddTHH:mm:ss.fffZ}: ", DateTime.UtcNow));
             byte[] prefix = isRequest ? Encoding.ASCII.GetBytes ("C: ") : Encoding.ASCII.GetBytes ("S: ");
-            //Log.Info (Log.LOG_IMAP, "{0}{1}", Encoding.ASCII.GetString (prefix), Encoding.UTF8.GetString (logData));
 
             if (!_Enabled || null == RedactProtocolLogFunc) {
                 return;
