4c6fc81c5958af18e46bcc47d0f0dd50281b7d5e
Henry Kwok <henryk@nachocove.com>
2015-08-03 15:23:28 -0700

    Compare the original and refreshed version of GAL contact. Update only if they are different.

--- NachoClient.Android/NachoCore/BackEnd/ActiveSync/Commands/AsSearchCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/ActiveSync/Commands/AsSearchCommand.cs
@@ -227,10 +227,13 @@ namespace NachoCore.ActiveSync
             if (1 != existingItems.Count) {
                 Log.Error (Log.LOG_AS, "{0} GAL-cache entries for email address {1}", existingItems.Count, emailAddress);
             }
+            var original = McContact.QueryByEmailAddressInFolder (accountId, galFolderId, emailAddress).First ();
             var existing = existingItems.First ();
             existing.RefreshFromGalXml (xmlProperties);
             existing.GalCacheToken = token;
-            existing.Update ();
+            if (!McContact.CompareOnEditableFields (existing, original)) {
+                existing.Update ();
+            }
             return true;
         }
 
