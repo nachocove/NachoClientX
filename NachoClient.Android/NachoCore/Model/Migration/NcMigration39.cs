be8d4f55e54fcf90541587003503550ae80cd6bd
Steve Scalpone <sscalpone@gmail.com>
2015-08-10 14:17:21 -0700

    Remove any lingering partial google placeholder accounts.  Should not be an issue for the existing beta because the google accounts have never been deployed, but it would clean up alpha issues with out needs a re-install.

---
+++ NachoClient.Android/NachoCore/Model/Migration/NcMigration39.cs
@@ -0,0 +1,27 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using NachoCore.ActiveSync;
+
+namespace NachoCore.Model
+{
+    public class NcMigration39 : NcMigration
+    {
+        public override int GetNumberOfObjects ()
+        {
+            return Db.Table<McAccount> ().Count ();
+        }
+
+        // Alpha users only; no beta has left with google in-progress account.
+        // Remove left over accounts that have Google Callback in their name.
+        public override void Run (System.Threading.CancellationToken token)
+        {
+            foreach (var account in Db.Table<McAccount> ()) {
+                if (McAccount.ConfigurationInProgressEnum.GoogleCallback == account.ConfigurationInProgress) {
+                    account.Delete ();
+                }
+            }
+        }
+    }
+}
+
