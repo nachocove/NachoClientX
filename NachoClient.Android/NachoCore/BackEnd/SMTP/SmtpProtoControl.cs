c9f7ae507ebc447a8700eb094b0bca8341940335
Jan Vilhuber <jan@vilhuber.com>
2015-08-27 13:01:05 -0600

    some more re-start of discovery protection

--- NachoClient.Android/NachoCore/BackEnd/SMTP/SmtpProtoControl.cs
+++ NachoClient.Android/NachoCore/BackEnd/SMTP/SmtpProtoControl.cs
@@ -458,6 +458,10 @@ namespace NachoCore.SMTP
             //  UI:Info:1:: avl: handleStatusEnums 2 sender=Running reader=Running
             // But this is an illegal state in SubMitWait:
             //  STATE:Error:1:: SM(Account:3): S=SubmitWait & E=Running/avl: EventFromEnum running => INVALID EVENT
+            if (null != Cmd && Cmd is SmtpDiscoveryCommand) {
+                // a SmtpDiscoveryCommand is already running.
+                return;
+            }
             BackEndStatePreset = BackEndStateEnum.Running;
             var cmd = new SmtpDiscoveryCommand (this, SmtpClient);
             cmd.Execute (Sm);
