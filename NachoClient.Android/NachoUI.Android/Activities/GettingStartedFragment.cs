6da6f8ad7450aea285652271d8a4136ea597fd1b
Steve Scalpone <sscalpone@gmail.com>
2015-10-06 09:20:31 -0700

    Android clean up launch, get started, choose service, get credentials.

--- NachoClient.Android/NachoUI.Android/Activities/GettingStartedFragment.cs
+++ NachoClient.Android/NachoUI.Android/Activities/GettingStartedFragment.cs
@@ -16,12 +16,12 @@ using NachoCore.Utils;
 
 namespace NachoClient.AndroidClient
 {
-    public class StartupFragment : Fragment
+    public class GettingStartedFragment : Fragment
     {
-        // Just shows "Let's get started"
-        public static StartupFragment newInstance ()
+        // Just shows "Welcome to NachoMail"
+        public static GettingStartedFragment newInstance ()
         {
-            var fragment = new StartupFragment ();
+            var fragment = new GettingStartedFragment ();
             return fragment;
         }
 
@@ -32,10 +32,18 @@ namespace NachoClient.AndroidClient
 
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            var view = inflater.Inflate (Resource.Layout.StartupFragment, container, false);
+            var view = inflater.Inflate (Resource.Layout.GettingStartedFragment, container, false);
+
+            var submitButton = view.FindViewById<Button> (Resource.Id.submit);
+            submitButton.Click += SubmitButton_Click;
             return view;
         }
 
+        void SubmitButton_Click (object sender, EventArgs e)
+        {
+            var parent = (LaunchActivity)Activity;
+            parent.GettingStartedFinished ();
+        }
     }
 }
 
