f966a1ea17687a475bffb6e4a60812b0dd5f4178
Jan Vilhuber <jan@vilhuber.com>
2015-10-12 20:40:05 +0200

    rosa encrypt and decrypt the key

--- NachoClient.Android/NachoPlatform.Android/KeychainAndroid.cs
+++ NachoClient.Android/NachoPlatform.Android/KeychainAndroid.cs
@@ -5,6 +5,10 @@ using NachoCore.Utils;
 using Android.Content;
 using Android.Preferences;
 using NachoClient.AndroidClient;
+using Java.Security;
+using Android.Security;
+using Javax.Crypto;
+using Javax.Crypto.Spec;
 
 namespace NachoPlatform
 {
@@ -25,8 +29,18 @@ namespace NachoPlatform
             }
         }
 
-        private const string KDefaultAccount = "device";
-        private const string KIdentifierForVendor = "IdentifierForVendor";
+        private Keychain ()
+        {
+            try {
+                GetKeyPair ();
+                GetPrefsKey ();
+
+            } catch (Exception ex) {
+                var str = ex.ToString ();
+                Log.Error (Log.LOG_SYS, "Keypair generation error: {0}", str);
+            }
+        }
+
         private const string KAccessToken = "AccessToken";
         private const string KRefreshToken = "RefreshToken";
         private const string KUserId = "UserId";
@@ -143,7 +157,7 @@ namespace NachoPlatform
         #endregion
 
         #region ISharedPreferences
-        ISharedPreferences _Prefs = null;
+        ISharedPreferences _Prefs;
         ISharedPreferences Prefs
         {
             get
@@ -158,13 +172,26 @@ namespace NachoPlatform
         public string GetKeyString (string key)
         {
             var r = Prefs.GetString(key, null);
-            return r;
+            if (null != r) {
+                return DecryptString (r);
+            } else {
+                throw new Exception ("No string found");
+            }
         }
 
         public bool SetKeyString (string key, string value)
         {
+            if (null == value) {
+                throw new Exception ("Null string passed");
+            }
+            var enc = EncryptString (value);
+
+            // DEBUG
+            var dec = DecryptString (enc);
+            NcAssert.True (value == dec);
+
             var editor = Prefs.Edit ();
-            editor.PutString(key, value);
+            editor.PutString(key, enc);
             editor.Commit();
             return true;
         }
@@ -175,6 +202,112 @@ namespace NachoPlatform
             editor.Remove (key);
             editor.Commit ();
             return true;
+        }
+        #endregion
+
+        #region PrefsKey
+        private ISecretKey PrefsKey;
+        private Mac PrefsMac;
+        const string KPrefsKeyKey = "PreferencesKey";
+        private void GetPrefsKey ()
+        {
+            if (null == PrefsKey) {
+                DeleteKey (KPrefsKeyKey); // DEBUG
+                var r = Prefs.GetString (KPrefsKeyKey, null);
+                if (null == r) {
+                    byte[] raw = new byte[32]; // 32*8 == 256
+                    new SecureRandom ().NextBytes (raw);
+                    var key = new SecretKeySpec (raw, "AES");
+                    var editor = Prefs.Edit ();
+                    editor.PutString (KPrefsKeyKey, RSAEncryptKey (key));
+                    editor.Commit ();
+                    r = Prefs.GetString (KPrefsKeyKey, null);
+                    NcAssert.True (null != r);
+                }
+                PrefsKey = (ISecretKey)RSADecryptKey (r);
+                PrefsMac = Mac.GetInstance("HmacSHA256");
+                PrefsMac.Init(PrefsKey); // TODO Same key? If not, we'll need to create a new one and also encrypt and store it.
+            }
+        }
+
+        private string DecryptString (string encryptedTextB64)
+        {
+            Log.Error (Log.LOG_SYS, "DEBUG DecryptString DEBUG");
+            return encryptedTextB64;
+        }
+
+        private string EncryptString (string data)
+        {
+            Log.Error (Log.LOG_SYS, "DEBUG EncryptString DEBUG");
+            return data;
+        }
+        #endregion
+
+        #region Keystore
+        const int KeyPairSize = 2048;
+        const string KDefaultKeyPair = "NachoMailDefaultKeyPair";
+        const string KDefaultKeyStore = "AndroidKeyStore";
+        string KeyStoreProvider;
+        KeyStore _ks;
+        KeyStore ks {
+            get {
+                if (_ks == null) {
+                    KeyStoreProvider = KDefaultKeyStore;
+                    _ks = KeyStore.GetInstance (KeyStoreProvider);
+                    _ks.Load (null);
+                }
+                return _ks;
+            }
+        }
+
+        IPrivateKey privateKey;
+        IPublicKey publicKey;
+        private void GetKeyPair ()
+        {
+            if (!ks.ContainsAlias (KDefaultKeyPair)) {
+                GenerateKeyPair ();
+            }
+            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry)ks.GetEntry (KDefaultKeyPair, null);
+            publicKey = privateKeyEntry.Certificate.PublicKey;
+            privateKey = privateKeyEntry.PrivateKey;
+        }
+
+        private void GenerateKeyPair ()
+        {
+            Java.Util.Calendar start = Java.Util.Calendar.GetInstance (Java.Util.TimeZone.Default);
+            Java.Util.Calendar end = Java.Util.Calendar.GetInstance (Java.Util.TimeZone.Default);
+            end.Add (Java.Util.CalendarField.Year, 20);
+            KeyPairGenerator generator = KeyPairGenerator.GetInstance("RSA", KeyStoreProvider);
+            KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(MainApplication.Instance.ApplicationContext)
+                .SetKeyType ("RSA")
+                .SetKeySize (KeyPairSize)
+                .SetAlias (KDefaultKeyPair)
+                .SetSubject(new Javax.Security.Auth.X500.X500Principal("CN=NachoMail"))
+                .SetSerialNumber (Java.Math.BigInteger.One)
+                .SetStartDate(start.Time)
+                .SetEndDate(end.Time)
+                .SetEncryptionRequired () // TODO We probably need to see if we can still use the key in the background if the device is locked.
+                .Build ();
+            generator.Initialize(spec);
+            generator.GenerateKeyPair();
+            NcAssert.True (ks.ContainsAlias (KDefaultKeyPair)); // make sure it got saved to the keystore
+        }
+
+        private string RSAEncryptKey (IKey key)
+        {
+            byte[] encryptedData;
+            var rsaCipher = Cipher.GetInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
+            rsaCipher.Init (CipherMode.WrapMode, publicKey);
+            encryptedData = rsaCipher.Wrap (key);
+            return Convert.ToBase64String (encryptedData);
+        }
+
+        private IKey RSADecryptKey (string encryptedTextB64)
+        {
+            var bytesToDecrypt = Convert.FromBase64String(encryptedTextB64);
+            var rsaCipher = Cipher.GetInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
+            rsaCipher.Init (CipherMode.UnwrapMode, privateKey);
+            return rsaCipher.Unwrap (bytesToDecrypt, "AES", KeyType.SecretKey);
         }
         #endregion
     }
