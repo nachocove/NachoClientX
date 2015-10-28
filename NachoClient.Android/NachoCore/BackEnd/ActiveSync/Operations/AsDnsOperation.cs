c6ec34ad7b2b748239baa7632ae22ff183a9fffe
Jan Vilhuber <jan@vilhuber.com>
2015-10-28 14:34:01 -0600

    move platform-dependent dos code into IPlatform

--- NachoClient.Android/NachoCore/BackEnd/ActiveSync/Operations/AsDnsOperation.cs
+++ NachoClient.Android/NachoCore/BackEnd/ActiveSync/Operations/AsDnsOperation.cs
@@ -8,17 +8,33 @@ using DnDns.Enums;
 using DnDns.Query;
 using NachoCore.Model;
 using NachoCore.Utils;
+using NachoPlatform;
 
 namespace NachoCore.ActiveSync
 {
-    public class AsDnsOperation : IAsOperation
+    public class AsDnsOperation : IAsOperation, IDnsLockObject
     {
         private IAsDnsOperationOwner owner;
         private TimeSpan timeout;
         private NcStateMachine stateMachine;
 
-        private object lockObject = new object ();
-        private bool complete = false;
+        private object _lockObject = new object ();
+        public object lockObject { 
+            get {
+                return _lockObject;
+            }
+        }
+
+        private bool _complete = false;
+        public bool complete {
+            get {
+                return _complete;
+            }
+            protected set {
+                _complete = value;
+            }
+        }
+
         private NcTimer timer;
 
         // Allow the unit tests to mock up the call to res_query().
@@ -27,10 +43,6 @@ namespace NachoCore.ActiveSync
         static public CallResQueryDelegate CallResQuery = ResQuery;
 
         private const int kDefaultTimeoutSeconds = 10;
-
-        // The res_query() error code for TRY_AGAIN.  This is the only error code
-        // that we care about.
-        private const int TRY_AGAIN_ERROR = 2;
 
         public AsDnsOperation (IAsDnsOperationOwner owner)
         {
@@ -70,77 +82,12 @@ namespace NachoCore.ActiveSync
             }
         }
 
-        // TODO The declaration for nacho_res_query needs to be moved to platform-specific code.
-#if __IOS__
-        // res_query() stores its error code in h_errno, not errno.  The usual technique
-        // for getting the error code, SetLastError=true, won't work.  So we have created
-        // a wrapper function around res_query that gets the error code from h_errno and
-        // returns it through a ref parameter.
-        private static object resQueryLock = new object ();
-        [DllImport("__Internal")]
-        private static extern int nacho_res_query (
-            string host, int queryClass, int queryType, byte[] answer, int anslen, ref int errorCode);
-
-        private static DnsQueryResponse ResQuery (AsDnsOperation op, string host, NsClass dnsClass, NsType dnsType)
+        private static DnsQueryResponse ResQuery (IDnsLockObject op, string host, NsClass dnsClass, NsType dnsType)
         {
-            // See https://www.dns-oarc.net/oarc/services/replysizetest - 4k is enough to hold the response.
-            byte[] answer = new byte[4096];
-
-            // Since the res_query call will be retried only if the error code is TRY_AGAIN,
-            // set the maximum number of retries to a rediculously high number.  It is
-            // unlikely that this limit will ever be reached.
-            int retries = 200;
-            while (0 < --retries) {
-
-                // res_query() stores its error code in a global variable, h_errno.  Not a
-                // thread-specific variable like errno, but a truly global variable.  To
-                // prevent one call from overwriting the error code of another before it is
-                // read, use a lock to prevent simultaneous calls to res_query.  (This won't
-                // prevent some other thread that we don't control from calling res_query()
-                // or some other function that uses h_errno, but there is no feasible way to
-                // protect against that.)
-                int errorCode = 0;
-                int rc;
-                lock (resQueryLock) {
-                    rc = nacho_res_query (host, (int)dnsClass, (int)dnsType, answer, answer.Length, ref errorCode);
-                }
-
-                lock (op.lockObject) {
-                    if (op.complete || (0 > rc && TRY_AGAIN_ERROR != errorCode)) {
-                        // The operation timed out or was canceled, or res_query() failed.
-                        return null;
-                    }
-                    if (0 <= rc) {
-                        // res_query() succeeded.
-                        try {
-                            DnsQueryResponse response = new DnsQueryResponse ();
-                            response.ParseResponse (answer, rc);
-                            return response;
-                        } catch (Exception e) {
-                            Log.Error (Log.LOG_DNS, "DNS response parsing failed with an exception, likely because the response is malformed: {0}", e.ToString ());
-                            return null;
-                        }
-                    }
-                }
-            }
-            // Too many retries. Give up.
-            return null;
+            var platformDns = new PlatformDns ();
+            return platformDns.ResQuery (op, host, dnsClass, dnsType);
         }
 
-#elif __ANDROID__
-        // We'd prefer to move the low-level DNS query capability to IPlatform.
-        private static DnsQueryResponse ResQuery (AsDnsOperation op, string host, NsClass dnsClass, NsType dnsType)
-        {
-            var DnsQuery = new DnsQueryRequest ();
-            DnsQueryResponse response = DnsQuery.Resolve (host, dnsType, dnsClass, System.Net.Sockets.ProtocolType.Udp);
-            if (null == response || response.Answers.Length == 0) {
-                return null;
-            }
-            return response;
-        }
-#else
-#error
-#endif
         private void DoExecuteWithRetries ()
         {
             DnsQueryResponse response = CallResQuery (this, owner.DnsHost (this), owner.DnsClass (this), owner.DnsType (this));
