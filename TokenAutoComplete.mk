ad5438e3d4494e220c0012abc99fc2392b2089f6
Jan Vilhuber <jan@vilhuber.com>
2015-10-30 17:27:59 -0600

    Auto-discover XAM_ANDROID_HOME

--- TokenAutoComplete.mk
+++ TokenAutoComplete.mk
@@ -1,4 +1,6 @@
+XAM_ANDROID_HOME = $(shell ls -d1 ${HOME}/Library/Developer/Xamarin/android-sdk*)
 all:
+	if [ -z "$(XAM_ANDROID_HOME)" ] ; then echo "No XAM_ANDROID_HOME found."; exit 1; fi
 	ANDROID_HOME=${XAM_ANDROID_HOME} bash gradlew jar
 
 clean:
