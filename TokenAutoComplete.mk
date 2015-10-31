b1ee8be0b5bc6a3c2ce680caaf5d34d73150496e
Jan Vilhuber <jan@vilhuber.com>
2015-10-30 18:11:42 -0600

    Add checks and error message for the android support repository

--- TokenAutoComplete.mk
+++ TokenAutoComplete.mk
@@ -1,6 +1,7 @@
 XAM_ANDROID_HOME = $(shell ls -d1 ${HOME}/Library/Developer/Xamarin/android-sdk*)
 all:
-	if [ -z "$(XAM_ANDROID_HOME)" ] ; then echo "No XAM_ANDROID_HOME found."; exit 1; fi
+	@if [ -z "$(XAM_ANDROID_HOME)" ] ; then echo "No XAM_ANDROID_HOME found."; exit 1; fi
+	@if [ ! -d $(XAM_ANDROID_HOME)/extras/android/m2repository ] ; then echo "Please make sure to install the 'Android Support Repository' in the 'Extras' \n    section of the Android SDK Manager (Xamarin Studio -> Tools -> \n    Open Android SDK Manager...)"; exit 1; fi
 	ANDROID_HOME=${XAM_ANDROID_HOME} bash gradlew jar
 
 clean:
