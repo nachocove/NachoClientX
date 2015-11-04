9e3e0f3723137cf3aec5b8882413650e3f73ee30
Owen Shaw <owen@owenshaw.com>
2015-11-04 14:20:35 -0800

    start filling in attachment picker

--- NachoClient.Android/NachoUI.Android/Activities/AttachmentPickerFragment.cs
+++ NachoClient.Android/NachoUI.Android/Activities/AttachmentPickerFragment.cs
@@ -22,6 +22,9 @@ namespace NachoClient.AndroidClient
     public class AttachmentPickerFragment : DialogFragment
     {
 
+        GridView OptionsGridView;
+        List<AttachmentOption> Options;
+
         public AttachmentPickerFragmentDelegate Delegate;
 
         public override Dialog OnCreateDialog (Bundle savedInstanceState)
@@ -29,6 +32,14 @@ namespace NachoClient.AndroidClient
             var builder = new AlertDialog.Builder (Activity);
             var inflater = Activity.LayoutInflater;
             var view = inflater.Inflate (Resource.Layout.AttachmentPickerFragment, null);
+            OptionsGridView = view.FindViewById<GridView> (Resource.Id.attachment_options);
+            Options = new List<AttachmentOption> (new AttachmentOption[] {
+                new AttachmentOption ("Add Photo", Resource.Drawable.calendar_add_photo, AddPhoto),
+                new AttachmentOption ("Take Photo", Resource.Drawable.calendar_take_photo, TakePhoto),
+                new AttachmentOption ("Add File", Resource.Drawable.calendar_add_files, AddFile)
+            });
+            OptionsGridView.Adapter = new AttachmentOptionsAdapter (this, Options);
+            OptionsGridView.ItemClick += OptionClicked;
             builder.SetTitle ("Attach");
             builder.SetView (view);
             return builder.Create ();
@@ -38,6 +49,83 @@ namespace NachoClient.AndroidClient
         {
             base.OnAttach (activity);
         }
+
+        void AddPhoto ()
+        {
+        }
+
+        void TakePhoto ()
+        {
+        }
+
+        void AddFile ()
+        {
+        }
+
+        void OptionClicked (object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var option = Options [e.Position];
+            option.Action ();
+        }
+    }
+
+    public class AttachmentOption
+    {
+        public string Label;
+        public int Drawable;
+        public Action Action;
+
+        public AttachmentOption (string label, int drawable, Action action)
+        {
+            Label = label;
+            Drawable = drawable;
+            Action = action;
+        }
+    }
+
+    public class AttachmentOptionsAdapter : BaseAdapter<AttachmentOption>
+    {
+
+        Fragment Parent;
+        List<AttachmentOption> Options;
+
+        public AttachmentOptionsAdapter (Fragment parent, List<AttachmentOption> options)
+        {
+            Parent = parent;
+            Options = options;
+        }
+
+        public override int Count {
+            get {
+                return Options.Count;
+            }
+        }
+
+        public override AttachmentOption this[int index] {
+            get {
+                return Options [index];
+            }
+        }
+
+        public override long GetItemId (int position)
+        {
+            return position;
+        }
+
+        public override View GetView (int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+            if (view == null) {
+                view = Parent.Activity.LayoutInflater.Inflate (Resource.Layout.AttachmentOptionItemView, null);
+            }
+            var option = Options [position];
+            var icon = view.FindViewById<ImageView> (Resource.Id.option_icon);
+            var label = view.FindViewById<TextView> (Resource.Id.option_label);
+            icon.SetImageResource (option.Drawable);
+            label.Text = option.Label;
+            return view;
+        }
+
     }
 }
 
