2a01a84c9ecd945c4c04818163918c16a20cf621
Jan Vilhuber <jan@vilhuber.com>
2015-11-13 14:00:33 -0700

    catch a socket error and log it instead of crashing
    resolves nacho cove/qa#1374

--- NachoClient.Android/NachoPlatform.Android/DnsAndroid.cs
+++ NachoClient.Android/NachoPlatform.Android/DnsAndroid.cs
@@ -26,7 +26,12 @@ namespace NachoPlatform
             }
 
             var DnsQuery = new DnsQueryRequest ();
-            DnsQueryResponse response = DnsQuery.Resolve (host, dnsType, dnsClass, System.Net.Sockets.ProtocolType.Udp);
+            DnsQueryResponse response = null;
+            try {
+                response = DnsQuery.Resolve (host, dnsType, dnsClass, System.Net.Sockets.ProtocolType.Udp);
+            } catch (System.Net.Sockets.SocketException ex) {
+                Log.Info (Log.LOG_DNS, "DnsQuery failed: {0}", ex.Message);
+            }
             lock (op.lockObject) {
                 if (op.complete) {
                     // The operation timed out or was canceled
