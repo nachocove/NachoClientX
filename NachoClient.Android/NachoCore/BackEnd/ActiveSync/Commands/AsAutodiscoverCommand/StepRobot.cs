9eb861f00bf32cac960148e6147045bfc526f0ec
Jan Vilhuber <jan@vilhuber.com>
2015-11-17 15:07:18 -0800

    also unset the handler in the error case

--- NachoClient.Android/NachoCore/BackEnd/ActiveSync/Commands/AsAutodiscoverCommand/StepRobot.cs
+++ NachoClient.Android/NachoCore/BackEnd/ActiveSync/Commands/AsAutodiscoverCommand/StepRobot.cs
@@ -706,6 +706,7 @@ namespace NachoCore.ActiveSync
 
             void GetServerCertError (Exception exception)
             {
+                ServerCertificatePeek.Instance.ValidationEvent -= ServerCertificateEventHandler;
                 Log.Info (Log.LOG_AS, "SR:GetAsync Exception: {0}", exception.ToString ());
                 GetServerCertSuccess (null, Cts.Token);
             }
