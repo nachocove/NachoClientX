167886df9cb946b7c2704a8286f28e84b3a11128
Owen Shaw <owen@owenshaw.com>
2015-12-07 15:34:43 -0800

    replace body render methods with single method to render a bundle

--- NachoClient.iOS/NachoUI.iOS/Support/BodyWebView.cs
+++ NachoClient.iOS/NachoUI.iOS/Support/BodyWebView.cs
@@ -7,6 +7,7 @@ using UIKit;
 using NachoCore.Utils;
 using NachoCore;
 using System.IO;
+using System.Collections.Generic;
 
 namespace NachoClient.iOS
 {
@@ -19,16 +20,14 @@ namespace NachoClient.iOS
     {
         public delegate void LinkSelectedCallback (NSUrl url);
 
-        protected NSUrl baseUrl;
         protected nfloat preferredWidth;
         private Action sizeChangedCallback;
         private bool loadingComplete;
         private LinkSelectedCallback onLinkSelected;
 
-        public BodyWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, NSUrl baseUrl, LinkSelectedCallback onLinkSelected)
+        public BodyWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, LinkSelectedCallback onLinkSelected)
             : base (new CGRect(0, Y, preferredWidth, initialHeight))
         {
-            this.baseUrl = baseUrl;
             this.preferredWidth = preferredWidth;
             this.sizeChangedCallback = sizeChangedCallback;
             this.DataDetectorTypes = UIDataDetectorType.Link | UIDataDetectorType.PhoneNumber;
@@ -151,36 +150,13 @@ namespace NachoClient.iOS
     /// </summary>
     public class BodyHtmlWebView : BodyWebView
     {
-        private string html;
 
-        private const string magic = @"
-            var style = document.createElement(""style"");
-            style.setAttribute(""type"", ""text/css"");
-            style.innerHTML = ""@import '__nacho__css__'"";
-            document.head.appendChild(style);
-            var viewPortTag=document.createElement('meta');
-            viewPortTag.id=""viewport"";
-            viewPortTag.name = ""viewport"";
-            viewPortTag.content = ""width={0}"";
-            document.getElementsByTagName('head')[0].appendChild(viewPortTag);
-        ";
-        private const string disableJavaScript = "<meta http-equiv=\"Content-Security-Policy\" content=\"script-src 'none'\">";
+        NcEmailMessageBundle Bundle;
 
-        public BodyHtmlWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, string html, NSUrl baseUrl, BodyWebView.LinkSelectedCallback onLinkSelected)
-            : base (Y, preferredWidth, initialHeight, sizeChangedCallback, baseUrl, onLinkSelected)
+        public BodyHtmlWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, NcEmailMessageBundle bundle, BodyWebView.LinkSelectedCallback onLinkSelected)
+            : base (Y, preferredWidth, initialHeight, sizeChangedCallback, onLinkSelected)
         {
-            SetHtml (html);
-        }
-
-        public BodyHtmlWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, NSUrl baseUrl, BodyWebView.LinkSelectedCallback onLinkSelected)
-            : base (Y, preferredWidth, initialHeight, sizeChangedCallback, baseUrl, onLinkSelected)
-        {
-        }
-
-        public void SetHtml (string html)
-        {
-            this.html = html;
-
+            Bundle = bundle;
             if (NcApplication.ExecutionContextEnum.Foreground == NcApplication.Instance.ExecutionContext) {
                 LoadContent ();
             }
@@ -188,61 +164,21 @@ namespace NachoClient.iOS
 
         protected override void LoadContent ()
         {
-            LoadHtmlString (disableJavaScript + html, baseUrl);
+            if (Bundle.FullHtmlUrl == null) {
+                var baseUrl = new NSUrl (Bundle.BaseUrl.ToString ());
+                LoadHtmlString (Bundle.FullHtml, baseUrl);
+            } else {
+                var url = new NSUrl (Bundle.FullHtmlUrl.ToString ());
+                var request = new NSUrlRequest (url);
+                LoadRequest (request);
+            }
         }
 
         protected override void PostLoadAdjustment ()
         {
-            EvaluateJavascript (string.Format (magic, preferredWidth));
-        }
-    }
-
-    /// <summary>
-    /// Display RTF in a UIWebView.
-    /// </summary>
-    public class BodyRtfWebView : BodyWebView
-    {
-        private string rtf;
-
-        public BodyRtfWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, string rtf, NSUrl baseUrl, BodyWebView.LinkSelectedCallback onLinkSelected)
-            : base (Y, preferredWidth, initialHeight, sizeChangedCallback, baseUrl, onLinkSelected)
-        {
-            this.rtf = rtf;
-
-            if (NcApplication.ExecutionContextEnum.Foreground == NcApplication.Instance.ExecutionContext) {
-                LoadContent ();
-            }
-        }
-
-        protected override void LoadContent ()
-        {
-            LoadData (NSData.FromString (rtf, NSStringEncoding.UTF8), "text/rtf", "utf-8", baseUrl);
-        }
-
-        protected override void PostLoadAdjustment ()
-        {
-            // No adjustment is necessary.
-        }
-    }
-
-    /// <summary>
-    /// Display plain text within a UIWebView, using our own custom font instead of the default fixed-space font.
-    /// </summary>
-    public class BodyPlainWebView : BodyHtmlWebView
-    {
-        public BodyPlainWebView (nfloat Y, nfloat preferredWidth, nfloat initialHeight, Action sizeChangedCallback, string text, NSUrl baseUrl, BodyWebView.LinkSelectedCallback onLinkSelected)
-            : base (Y, preferredWidth, initialHeight, sizeChangedCallback, baseUrl, onLinkSelected)
-        {
-            var serializer = new HtmlTextDeserializer ();
-            var doc = serializer.Deserialize (text);
-            string html = "";
-            using (var writer = new StringWriter ()) {
-                doc.Save (writer);
-                html = writer.ToString ();
-            }
-
-            SetHtml (html);
         }
 
     }
+
+
 }
