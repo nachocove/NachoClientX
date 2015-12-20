b946302ab8906cdd8e274532bdb8e76adb8044df
Jan Vilhuber <jan@vilhuber.com>
2015-12-20 12:44:21 -0700

    move push to platform

---
+++ NachoClient.Android/NachoPlatform.Android/PushAssistAndroid.cs
@@ -0,0 +1,39 @@
+﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
+//
+using NachoCore;
+
+namespace NachoPlatform
+{
+    public class PushAssist : IPushAssist
+    {
+        public PushAssist (IPushAssistOwner owner)
+        {
+        }
+
+        #region IPushAssist implementation
+
+        public void Dispose ()
+        {
+        }
+
+        public void Execute ()
+        {
+        }
+
+        public void Park ()
+        {
+        }
+
+        public bool IsStartOrParked ()
+        {
+            return false;
+        }
+
+        public void Stop ()
+        {
+        }
+
+        #endregion
+    }
+}
+
