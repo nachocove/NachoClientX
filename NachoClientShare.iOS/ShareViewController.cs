fd31d197c651e6c2f261b86c7413ce0f3178a801
Owen Shaw <owen@owenshaw.com>
2015-12-29 11:43:39 -0800

    copy shared files to McDocument

--- NachoClientShare.iOS/ShareViewController.cs
+++ NachoClientShare.iOS/ShareViewController.cs
@@ -2,6 +2,8 @@
 //
 using System;
 using System.Drawing;
+using System.IO;
+using NachoClient.Build;
 
 using Foundation;
 using UIKit;
@@ -10,6 +12,10 @@ namespace NachoClientShare.iOS
 {
     public partial class ShareViewController : UIViewController
     {
+        string StashName;
+        NSUrl StashUrl;
+        int RemainingAttachments = 0;
+
         public ShareViewController (IntPtr handle) : base (handle)
         {
         }
@@ -32,8 +38,79 @@ namespace NachoClientShare.iOS
         public override void ViewWillAppear (bool animated)
         {
             base.ViewWillAppear (animated);
-            OpenApp ();
+            StashFiles ();
+        }
+
+        void Complete (bool openApp = true)
+        {
+            if (openApp) {
+                OpenApp ();
+            }
             ExtensionContext.CompleteRequest (null, null);
+        }
+
+        void StashFiles ()
+        {
+            var uuid = new NSUuid ();
+            StashName = uuid.AsString ();
+            var containerUrl = NSFileManager.DefaultManager.GetContainerUrl (BuildInfo.AppGroup);
+            if (containerUrl != null) {
+                StashUrl = containerUrl.Append (StashName, true);
+                Directory.CreateDirectory (StashUrl.Path);
+                RemainingAttachments = 0;
+                foreach (var item in ExtensionContext.InputItems) {
+                    foreach (var attachment in item.Attachments) {
+                        ++RemainingAttachments;
+                    }
+                }
+                if (RemainingAttachments > 0) {
+                    foreach (var item in ExtensionContext.InputItems) {
+                        foreach (var attachment in item.Attachments) {
+                            attachment.LoadItem ("public.data", null, HandleAttachmentLoad);
+                        }
+                    }
+                } else {
+                    Console.WriteLine ("NACHO ERROR: Nothing to share");
+                    Complete (false);
+                }
+            } else {
+                Console.WriteLine ("NACHO ERROR: No container URL");
+                Complete (false);
+            }
+        }
+
+        void HandleAttachmentLoad (NSObject provider, NSError error)
+        {
+            --RemainingAttachments;
+            if (error == null) {
+                var url = provider as NSUrl;
+                if (url != null) {
+                    var filename = url.LastPathComponent;
+                    var filenameRoot = Path.GetFileNameWithoutExtension (filename);
+                    var ext = Path.GetExtension (filename);
+                    var destinationPath = StashUrl.Append (filename, false).Path;
+                    var counter = 1;
+                    while (File.Exists (destinationPath)) {
+                        filename = String.Format ("{0}-{1}{2}", filenameRoot, counter, ext);
+                        destinationPath = StashUrl.Append (filename, false).Path;
+                        ++counter;
+                    }
+                    if (url.IsFileUrl) {
+                        File.Copy (url.Path, destinationPath);
+                    } else {
+                        Console.WriteLine ("NACHO ERROR: provider is not a file NSUrl");
+                    }
+                } else {
+                    Console.WriteLine ("NACHO ERROR: provider is not an NSUrl");
+                }
+            } else {
+                Console.WriteLine ("NACHO ERROR: Error loading attachment: {0} {1}", error.Code, error.Description);
+            }
+            if (RemainingAttachments == 0) {
+                this.BeginInvokeOnMainThread (() => {
+                    Complete ();
+                });
+            }
         }
 
         void OpenApp ()
@@ -44,7 +121,7 @@ namespace NachoClientShare.iOS
                 responder = responder.NextResponder;
             }
             if (responder != null) {
-                var url = new NSUrl ("com.nachocove.nachomail://");
+                var url = new NSUrl (String.Format ("com.nachocove.nachomail:///share/{0}", StashName));
                 responder.PerformSelector (openURL, url);
             }
         }
