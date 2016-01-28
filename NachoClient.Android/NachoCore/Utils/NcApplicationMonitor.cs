8b6a4788366be80585e77131d7f6942684ddb1c4
Steve Scalpone <sscalpone@gmail.com>
2016-01-28 09:33:42 -0800

    Don't dump the open fds until more critical.

--- NachoClient.Android/NachoCore/Utils/NcApplicationMonitor.cs
+++ NachoClient.Android/NachoCore/Utils/NcApplicationMonitor.cs
@@ -137,7 +137,7 @@ namespace NachoCore
             }
             Log.Info (Log.LOG_SYS, "NcApplicationMonitor: Files: Max {0}, Currently open {1}",
                 PlatformProcess.GetCurrentNumberOfFileDescriptors (), PlatformProcess.GetCurrentNumberOfInUseFileDescriptors ());
-            if (100 < PlatformProcess.GetCurrentNumberOfInUseFileDescriptors ()) {
+            if (200 < PlatformProcess.GetCurrentNumberOfInUseFileDescriptors ()) {
                 Log.DumpFileDescriptors ();
             }
             NcModel.Instance.DumpLastAccess ();
