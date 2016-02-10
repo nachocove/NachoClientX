f103f5ff77bfa1b8c877ce51ae869ad68ec03f15
Jan Vilhuber <jan@vilhuber.com>
2016-02-10 13:35:00 -0700

    use the correct indication

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailDeleteCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailDeleteCommand.cs
@@ -85,7 +85,7 @@ namespace NachoCore.IMAP
                 if (Client.Capabilities.HasFlag (ImapCapabilities.UidPlus)) {
                     mailKitFolder.Expunge (uids, Cts.Token);
                 }
-                StatusInd (NcResult.Info (NcResult.SubKindEnum.Info_EmailMessageSetFlagSucceeded));
+                BEContext.ProtoControl.StatusInd (NcResult.Info (NcResult.SubKindEnum.Info_EmailMessageSetChanged));
             } catch (MessageNotFoundException) {
                 // ignore. We are deleting it anyway.
             }
