0238255c475e2096b5d67ca72f758bef818169b5
Jan Vilhuber <jan@vilhuber.com>
2016-03-07 12:00:41 -0700

    adapt the existing code to the the changed mailkit move

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailMoveCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailMoveCommand.cs
@@ -118,23 +118,23 @@ namespace NachoCore.IMAP
                 // Note: There's still a tiny window where something might have deleted
                 // one of these messages, too. We can't prevent it. MailKit doesn't pass back 
                 // the necessary information we need from the COPYUID response to accomplish this.
-                var newUids = srcFolder.MoveTo (existingUids, dstFolder, Token);
-                if (existingUids.Count != newUids.Count) {
-                    Log.Warn (Log.LOG_IMAP, "Messages seem to have disappeared during move! Wanted to move: {0}, found existing UIDS {1}, and new UIDS {2}", uids, existingUids, newUids);
+                var uidMapping = srcFolder.MoveTo (existingUids, dstFolder, Token);
+                if (existingUids.Count != uidMapping.Count) {
+                    Log.Warn (Log.LOG_IMAP, "Messages seem to have disappeared during move! Wanted to move: {0}, found existing UIDS {1}, and new UIDS {2}", uids, existingUids, uidMapping);
                 }
 
                 NcModel.Instance.RunInTransaction (() => {
-                    for (var i = 0; i < existingUids.Count; i++) {
+                    foreach (var pair in uidMapping) {
                         McEmailMessage email;
-                        if (emailUidMapping.TryGetValue (existingUids [i].Id, out email)) {
+                        if (emailUidMapping.TryGetValue (pair.Key.Id, out email)) {
                             email.UpdateWithOCApply<McEmailMessage> ((record) => {
                                 var target = (McEmailMessage)record;
-                                target.ServerId = ImapProtoControl.MessageServerId (dst, newUids [i]);
-                                target.ImapUid = newUids [i].Id;
+                                target.ServerId = ImapProtoControl.MessageServerId (dst, pair.Value);
+                                target.ImapUid = pair.Value.Id;
                                 return true;
                             });
                         } else {
-                            Log.Error (Log.LOG_IMAP, "Could not match UID {0} to email", existingUids [i]);
+                            Log.Error (Log.LOG_IMAP, "Could not match UID {0} to email", pair.Key);
                         }
                     }
                 });
