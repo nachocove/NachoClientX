e8686dddf6b08c729d3427ea99e5b6d4ff02bb0f
Jan Vilhuber <jan@vilhuber.com>
2016-03-14 17:01:51 -0600

    add relevant code and files and settings for android MDM/ACE/ACC

--- NachoClient.Android/NachoPlatform.Android/MdmConfigAndroid.cs
+++ NachoClient.Android/NachoPlatform.Android/MdmConfigAndroid.cs
@@ -1,6 +1,9 @@
 ﻿//  Copyright (C) 2015 Nacho Cove, Inc. All rights reserved.
 //
 using System;
+using Android.Content;
+using NachoClient.AndroidClient;
+using NachoCore;
 using NachoCore.Utils;
 
 namespace NachoPlatform
@@ -28,8 +31,43 @@ namespace NachoPlatform
 
         public void ExtractValues ()
         {
-            // FIXME.
-            NcAssert.True (false);
+            if (Android.OS.Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.Lollipop) {
+                return;
+            }
+
+            var myRestrictionsMgr = (RestrictionsManager)MainApplication.Context.GetSystemService (Context.RestrictionsService);
+            ExtractValuesFromRestrictions (myRestrictionsMgr.ApplicationRestrictions);
+        }
+
+        public void ExtractValuesFromRestrictions (Android.OS.Bundle appRestrictions)
+        {
+            try {
+                NcMdmConfig.Instance.SetValues ((mdmConfig) => {
+                    mdmConfig.Host = appRestrictions.GetString ("AppServiceHost");
+                    mdmConfig.Port = uint.Parse (appRestrictions.GetString ("AppServicePort"));
+                    mdmConfig.Username = appRestrictions.GetString ("UserName");
+                    mdmConfig.Domain = appRestrictions.GetString ("UserDomain");
+                    mdmConfig.EmailAddr = appRestrictions.GetString ("UserEmail");
+                    mdmConfig.BrandingName = appRestrictions.GetString ("BrandingName");
+                    mdmConfig.BrandingLogoUrl = appRestrictions.GetString ("BrandingLogo");
+                });
+            } catch (ArgumentException ex) {
+                Log.Error (Log.LOG_SYS, "Could not get app config: {0}", ex);
+            }
+        }
+
+        [BroadcastReceiver (Enabled = true)]
+        [Android.App.IntentFilter (new[] { Intent.ActionApplicationRestrictionsChanged })]
+        class RestrictionsChangedBroadcastReceiver : BroadcastReceiver
+        {
+            public override void OnReceive (Context context, Intent intent)
+            {
+                MainApplication.OneTimeStartup ("RestrictionsChangedBroadcastReceiverNotificationActivity");
+                if (intent.Action == Intent.ActionApplicationRestrictionsChanged) {
+                    var myRestrictionsMgr = (RestrictionsManager)context.GetSystemService (Context.RestrictionsService);
+                    MdmConfig.Instance.ExtractValuesFromRestrictions (myRestrictionsMgr.ApplicationRestrictions);
+                }
+            }
         }
     }
 }
