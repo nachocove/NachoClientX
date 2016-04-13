be66e2f8f50fb6758fda141f15e5af192066b50f
Owen Shaw <owen@owenshaw.com>
2016-04-13 16:44:25 -0700

    new general settings view
    - better grouping for future additions
    - better selector for unread
    - uses new table view base

---
+++ NachoClient.iOS/NachoUI.iOS/Support/ImageAccessoryView.cs
@@ -0,0 +1,29 @@
+﻿//  Copyright (C) 2016 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using UIKit;
+using CoreGraphics;
+
+namespace NachoClient.iOS
+{
+    public class ImageAccessoryView : UIView
+    {
+        UIImageView ImageView;
+
+        public ImageAccessoryView (string imageName, float width = 30.0f) : base (new CGRect(0.0f, 0.0f, (nfloat)width, 0.0f))
+        {
+            BackgroundColor = UIColor.White;
+            using (var image = UIImage.FromBundle (imageName)){
+                ImageView = new UIImageView (image);
+            }
+            AddSubview (ImageView);
+        }
+
+        public override void LayoutSubviews ()
+        {
+            base.LayoutSubviews ();
+            ImageView.Center = new CGPoint (ImageView.Frame.Width / 2.0f, Bounds.Height / 2.0f);
+        }
+    }
+}
+
---
+++ NachoClient.iOS/NachoUI.iOS/Support/UIFont+Utils.cs
@@ -0,0 +1,16 @@
+﻿//  Copyright (C) 2016 Nacho Cove, Inc. All rights reserved.
+//
+using System;
+using UIKit;
+
+namespace NachoClient.iOS
+{
+    public static class UIFont_Utils
+    {
+        public static nfloat RoundedLineHeight (this UIFont font, nfloat scale)
+        {
+            return (nfloat)Math.Ceiling (font.LineHeight * scale) / scale;
+        }
+    }
+}
+
