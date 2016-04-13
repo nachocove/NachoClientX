2b866fa2287da0f3ed126b0d7b75672becae8815
Owen Shaw <owen@owenshaw.com>
2016-04-12 20:26:06 -0700

    rework nacho activity indicator
    - better behavior when stopping (doesn't jump)
    - compatibile with other animations on the same layer while running (like size changes)
    
    These changes are helpful as preparation for a new table view refresh indicator

--- NachoClient.iOS/NachoUI.iOS/Support/NcActivityIndicatorView.cs
+++ NachoClient.iOS/NachoUI.iOS/Support/NcActivityIndicatorView.cs
@@ -12,17 +12,35 @@ namespace NachoClient.iOS
     public class NcActivityIndicatorView : UIView
     {
 
-        private CALayer stripImageLayer;
-        private CALayer maskLayer;
-        private nfloat indicatorSize;
-        private bool isAnimating;
-        private CABasicAnimation stripAnimation = null;
+        private CALayer StripImageLayer;
+        private CAReplicatorLayer StripImageReplicatorLayer;
+        private CALayer MaskLayer;
+        private nfloat IndicatorSize;
+        private bool _IsAnimating;
+        private CABasicAnimation StripAnimation = null;
+        private CGSize RawIndicatorSize;
+        private nfloat CompleteCycleHeight;
+        private nfloat StripHeight;
+        private nfloat? OriginalOffset;
+        private nfloat _Speed = 1.0f;
+        public nfloat Speed {
+            get {
+                return _Speed;
+            }
+            set {
+                _Speed = value;
+                if (IsAnimating) {
+                    StopAnimating ();
+                    StartAnimating ();
+                }
+            }
+        }
 
         private const string STRIP_ANIMATION_KEY = "strip";
 
         public bool IsAnimating {
             get {
-                return isAnimating;
+                return _IsAnimating;
             }
         }
 
@@ -39,77 +57,141 @@ namespace NachoClient.iOS
 
         private void SetupIndicator ()
         {
+            _IsAnimating = false;
             UIImage stripImage = UIImage.FromBundle ("NachoActivityIndicatorStrip");
+            RawIndicatorSize = stripImage.Size;
+            CompleteCycleHeight = RawIndicatorSize.Height;
             Layer.MasksToBounds = true;
-            stripImageLayer = new CALayer ();
-            stripImageLayer.ContentsScale = stripImage.CurrentScale;
-            stripImageLayer.Contents = stripImage.CGImage;
-            stripImageLayer.Frame = new CGRect (0, 0, stripImage.Size.Width, stripImage.Size.Height);
-            stripImageLayer.AnchorPoint = new CGPoint (0.5, 1.0);
-            stripImageLayer.Opaque = true;
-            maskLayer = new CALayer ();
-            maskLayer.Frame = Frame;
-            maskLayer.MasksToBounds = true;
-            stripImageLayer.Position = new CGPoint (0, Frame.Size.Height);
-            maskLayer.AddSublayer (stripImageLayer);
-            Layer.AddSublayer (maskLayer);
+            StripImageLayer = new CALayer ();
+            StripImageLayer.Frame = new CGRect (0.0f, 0.0f, RawIndicatorSize.Width, RawIndicatorSize.Height);
+            StripImageLayer.Contents = stripImage.CGImage;
+            StripImageLayer.ContentsScale = stripImage.CurrentScale;
+            StripImageLayer.Opaque = true;
+            StripImageReplicatorLayer = new CAReplicatorLayer ();
+            StripImageReplicatorLayer.InstanceCount = 3;
+            StripHeight = StripImageLayer.Frame.Size.Height * StripImageReplicatorLayer.InstanceCount;
+            StripImageReplicatorLayer.Frame = new CGRect (0.0f, 0.0f, RawIndicatorSize.Width, StripHeight);
+            StripImageReplicatorLayer.InstanceTransform = CATransform3D.MakeTranslation (0.0f, StripImageLayer.Frame.Size.Height, 0.0f);
+            StripImageReplicatorLayer.AddSublayer (StripImageLayer);
+            StripImageReplicatorLayer.Opaque = true;
+            MaskLayer = new CALayer ();
+            MaskLayer.Bounds = new CGRect (0.0f, StripHeight - RawIndicatorSize.Width, RawIndicatorSize.Width, RawIndicatorSize.Width);
+            MaskLayer.Position = new CGPoint (Layer.Bounds.Width / 2.0f, Layer.Bounds.Height / 2.0f);
+            MaskLayer.MasksToBounds = true;
+            MaskLayer.CornerRadius = MaskLayer.Frame.Width / 2.0f;
+            MaskLayer.AddSublayer (StripImageReplicatorLayer);
+            Layer.AddSublayer (MaskLayer);
             ResizeIndicator ();
         }
+
+        public void StartAnimating ()
+        {
+            if (!_IsAnimating) {
+                if (StripAnimation == null) {
+                    SetupAnimation ();
+                }
+                StripAnimation.Duration = 2.0 / _Speed;
+                StripAnimation.From = new NSNumber (MaskLayer.Bounds.Y);
+                StripAnimation.To = new NSNumber (MaskLayer.Bounds.Y - CompleteCycleHeight);
+                _IsAnimating = true;
+                MaskLayer.AddAnimation (StripAnimation, STRIP_ANIMATION_KEY);
+            }
+        }
+
+        public void StopAnimating ()
+        {
+            if (_IsAnimating) {
+                _IsAnimating = false;
+                var y = MaskLayer.PresentationLayer.Bounds.Y;
+                MaskLayer.RemoveAnimation (STRIP_ANIMATION_KEY);
+                CATransaction.Begin ();
+                CATransaction.DisableActions = true;
+                if (y < StripHeight - CompleteCycleHeight - MaskLayer.Bounds.Height) {
+                    y += CompleteCycleHeight;
+                }
+                MaskLayer.Bounds = new CGRect (0.0f, y, MaskLayer.Bounds.Width, MaskLayer.Bounds.Height);
+                CATransaction.Commit ();
+            }
+        }
+
+        public void SetOffset (nfloat offset)
+        {
+            if (!_IsAnimating) {
+                if (!OriginalOffset.HasValue) {
+                    OriginalOffset = MaskLayer.Bounds.Y;
+                }
+                nfloat scale = IndicatorSize / RawIndicatorSize.Width;
+                offset = offset / scale;
+                CATransaction.Begin ();
+                CATransaction.DisableActions = true;
+                nfloat y = OriginalOffset.Value - offset % CompleteCycleHeight;
+                MaskLayer.Bounds = new CGRect (0.0f, y, MaskLayer.Bounds.Width, MaskLayer.Bounds.Height);
+                CATransaction.Commit ();
+            }
+        }
+
+        public void ClearOffset ()
+        {
+            if (OriginalOffset.HasValue) {
+                var offset = OriginalOffset.Value;
+                OriginalOffset = null;
+                if (!IsAnimating) {
+                    CATransaction.Begin ();
+                    CATransaction.DisableActions = true;
+                    MaskLayer.Bounds = new CGRect (0.0f, offset, MaskLayer.Bounds.Width, MaskLayer.Bounds.Height);
+                    CATransaction.Commit ();
+                }
+            }
+        }
+
+        public void ResetOffset ()
+        {
+            CATransaction.Begin ();
+            CATransaction.DisableActions = true;
+            MaskLayer.Bounds = new CGRect (0.0f, StripHeight - RawIndicatorSize.Width, MaskLayer.Bounds.Width, MaskLayer.Bounds.Height);
+            CATransaction.Commit ();
+        }
+
+        #region Layout
 
         public override void LayoutSubviews ()
         {
             ResizeIndicator ();
         }
 
-        public void ResizeIndicator ()
+        private void ResizeIndicator ()
         {
             var keys = Layer.AnimationKeys;
+            bool isPartOfExternalAnimation = false;
             CATransaction.Begin ();
-            if (keys != null && keys.Length > 0) {
-                var animation = Layer.AnimationForKey (keys [0]);
-                CATransaction.AnimationDuration = animation.Duration;
-                CATransaction.AnimationTimingFunction = animation.TimingFunction;
-            } else {
+            if (keys != null) {
+                foreach (var key in keys) {
+                    if (key != STRIP_ANIMATION_KEY) {
+                        isPartOfExternalAnimation = true;
+                        var animation = Layer.AnimationForKey (key);
+                        CATransaction.AnimationDuration = animation.Duration;
+                        CATransaction.AnimationTimingFunction = animation.TimingFunction;
+                        break;
+                    }
+                }
+            }
+            if (!isPartOfExternalAnimation) {
                 CATransaction.DisableActions = true;
             }
-            // Known issue: this resize code probably doesn't work while the animation is running.
-            // Ideally, any fix would continue animation smoothly as the size changes.
-            indicatorSize = (nfloat)Math.Min ((double)Frame.Width, (double)Frame.Height);
-            maskLayer.Frame = new CGRect ((Frame.Width - indicatorSize) / 2.0, (Frame.Height - indicatorSize) / 2.0, indicatorSize, indicatorSize);
-            maskLayer.CornerRadius = indicatorSize / 2.0f;
-            stripImageLayer.Position = new CGPoint (maskLayer.Bounds.Width / 2.0, Frame.Height);
-            nfloat scale = indicatorSize / stripImageLayer.Bounds.Width;
-            stripImageLayer.Transform = CATransform3D.MakeScale (scale, scale, 1.0f);
+            IndicatorSize = (nfloat)Math.Min ((double)Frame.Width, (double)Frame.Height);
+            nfloat scale = IndicatorSize / RawIndicatorSize.Width;
+            MaskLayer.AffineTransform = CGAffineTransform.MakeScale (scale, scale);
+            MaskLayer.Position = new CGPoint (Layer.Bounds.Width / 2.0f, Layer.Bounds.Height / 2.0f);
             CATransaction.Commit ();
         }
 
         private void SetupAnimation ()
         {
-            stripAnimation = CABasicAnimation.FromKeyPath ("position");
-            stripAnimation.Duration = 2.0;
-            stripAnimation.From = NSValue.FromCGPoint (stripImageLayer.Position);
-            stripAnimation.To = NSValue.FromCGPoint (new CGPoint (stripImageLayer.Position.X, stripImageLayer.Position.Y + stripImageLayer.Frame.Height - maskLayer.Frame.Height));
-            stripAnimation.RepeatCount = Single.PositiveInfinity;
+            StripAnimation = CABasicAnimation.FromKeyPath ("bounds.origin.y");
+            StripAnimation.RepeatCount = Single.PositiveInfinity;
         }
 
-        public void StartAnimating ()
-        {
-            if (!isAnimating) {
-                if (stripAnimation == null) {
-                    SetupAnimation ();
-                }
-                isAnimating = true;
-                stripImageLayer.AddAnimation (stripAnimation, STRIP_ANIMATION_KEY);
-            }
-        }
-
-        public void StopAnimating ()
-        {
-            if (isAnimating) {
-                isAnimating = false;
-                stripImageLayer.RemoveAnimation (STRIP_ANIMATION_KEY);
-            }
-        }
+        #endregion
 
     }
 }
