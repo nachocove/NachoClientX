4a2d3c8218e0bfe2c6d69cd54b8d3e670d03e912
Jan Vilhuber <jan@vilhuber.com>
2016-05-09 11:19:39 -0600

    use AddFlags not SetFlags.
    Set flags assumes the full flag-set has been passed, whereas AddFlags,
    as the name implies, adds a flag (sets one (or more) bits, instead of
    overwriting the set).
    Also remove a redundant DB operation.

--- NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailMarkAnsweredCommand.cs
+++ NachoClient.Android/NachoCore/BackEnd/IMAP/Commands/ImapEmailMarkAnsweredCommand.cs
@@ -25,7 +25,7 @@ namespace NachoCore.IMAP
             McEmailMessage email = McEmailMessage.QueryByServerId<McEmailMessage> (AccountId, PendingSingle.ServerId);
             try {
                 if (PendingSingle.EmailSetFlag_FlagType == McPending.MarkAnsweredFlag) {
-                    mailKitFolder.SetFlags (new UniqueId (email.ImapUid), MessageFlags.Answered, true, Cts.Token);
+                    mailKitFolder.AddFlags (new UniqueId (email.ImapUid), MessageFlags.Answered, true, Cts.Token);
                 } else {
                     mailKitFolder.RemoveFlags (new UniqueId (email.ImapUid), MessageFlags.Answered, true, Cts.Token);
                 }
