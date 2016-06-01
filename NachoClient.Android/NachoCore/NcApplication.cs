0d7b890f395dd5870b3e5cdd3da78f12a648facd
Jan Vilhuber <jan@vilhuber.com>
2016-06-01 11:17:25 -0600

    catch XAMMIT until bug is fixed
    Or until xamarin suggests a way to fix it in our code.
    
    resolves nachocove/qa#2111

--- NachoClient.Android/NachoCore/NcApplication.cs
+++ NachoClient.Android/NachoCore/NcApplication.cs
@@ -13,6 +13,7 @@ using NachoCore.Utils;
 using NachoClient.Build;
 using NachoPlatform;
 using System.Security.Cryptography.X509Certificates;
+using System.Net.Sockets;
 
 namespace NachoCore
 {
@@ -303,6 +304,12 @@ namespace NachoCore
             if (ex is OperationCanceledException && message.Contains ("Telemetry.Process")) {
                 Log.Error (Log.LOG_SYS, "XAMMIT AggregateException: OperationCanceledException with Telemetry.Process");
                 // XAMMIT. Cancel exception should be caught by system when c-token is the Task's c-token.
+                return true;
+            }
+            if (ex is SocketException && ex.Message.Contains ("The requested address is not valid in this context")) {
+                // XAMMIT. Best guess is that something triggers the network connection to close and mono tries to clean up,
+                // which throws this error. Bug has been filed with xamarin: https://bugzilla.xamarin.com/show_bug.cgi?id=41436
+                Log.Error (Log.LOG_SYS, "XAMMIT AggregateException: SocketException {0}", ex.Message);
                 return true;
             }
             if (message.Contains ("Amazon.Runtime")) {
