a036679a3b62c9557702f53c591d77b318469bdc
Owen Shaw <owen@owenshaw.com>
2016-12-18 12:14:09 -0800

    get send working in conversation view
    saving of drafts is not enabled

--- NachoClient.iOS/NachoUI.iOS/ConversationViewController.cs
+++ NachoClient.iOS/NachoUI.iOS/ConversationViewController.cs
@@ -13,7 +13,7 @@ using System.Linq;
 
 namespace NachoClient.iOS
 {
-    public class ConversationViewController : NcUIViewControllerNoLeaks, ChatMessageComposeDelegate, ChatViewDelegate, ChatViewDataSource, MessageDownloadDelegate, ThemeAdopter
+    public class ConversationViewController : NcUIViewControllerNoLeaks, ChatMessageComposeDelegate, ChatViewDelegate, ChatViewDataSource, MessageDownloadDelegate, ThemeAdopter, INachoFileChooserParent, MessageComposerDelegate
     {
         
         public NachoEmailMessages Messages;
@@ -23,6 +23,8 @@ namespace NachoClient.iOS
         Dictionary<int, List<McAttachment>> AttachmentsByMessageId;
         Dictionary<int, McChatParticipant> ParticipantsByEmailId;
         Dictionary<int, MessageDownloader> Downloaders;
+        List<McAttachment> Attachments;
+        List<string> SendTokens;
 
         public ConversationViewController ()
         {
@@ -32,6 +34,8 @@ namespace NachoClient.iOS
             AttachmentsByMessageId = new Dictionary<int, List<McAttachment>> ();
             ParticipantsByEmailId = new Dictionary<int, McChatParticipant> ();
             Downloaders = new Dictionary<int, MessageDownloader> ();
+            Attachments = new List<McAttachment>();
+            SendTokens = new List<string> ();
         }
 
         #region Theme
@@ -55,10 +59,17 @@ namespace NachoClient.iOS
             base.ViewWillAppear (animated);
             AdoptTheme (Theme.Active);
             SubviewChangedHeight ();
+            startListeningForNotifications ();
             if (IsMovingToParentViewController) {
                 ChatView.ReloadData ();
                 ChatView.ScrollToBottom ();
             }
+        }
+
+        public override void ViewDidDisappear (bool animated)
+        {
+            base.ViewDidDisappear (animated);
+            stopListeningForNotifications ();
         }
 
         protected override void CreateViewHierarchy ()
@@ -131,11 +142,148 @@ namespace NachoClient.iOS
             }
         }
 
+        #region User Actions
+
+        MessageComposer Composer;
+
+        public void Send ()
+        {
+            ComposeView.SetEnabled (false);
+            var text = ComposeView.GetMessage ();
+            //var previousMessages = new List<McEmailMessage> ();
+            var mostRecentMessage = Messages.GetCachedMessage (0);
+
+            Composer = new MessageComposer (Account);
+            Composer.Delegate = this;
+            if (mostRecentMessage != null) {
+                Composer.RelatedThread = new McEmailMessageThread ();
+                Composer.RelatedThread.FirstMessageId = mostRecentMessage.Id;
+                Composer.RelatedThread.MessageCount = 1;
+                Composer.Kind = EmailHelper.Action.ReplyAll;
+            } else {
+                Composer.Kind = EmailHelper.Action.Send;
+            }
+            Composer.InitialAttachments = Attachments;
+            Composer.InitialText = text;
+
+            Composer.StartPreparingMessage ();
+
+            //ChatMessageComposer.SendChatMessage (Chat, text, previousMessages, (McEmailMessage message, NcResult result) => {
+            //    if (IsViewLoaded) {
+            //        ComposeView.SetEnabled (true);
+            //        if (result.isOK ()) {
+            //            Chat.AddMessage (message);
+            //            ComposeView.Clear ();
+            //            ChatView.ScrollToBottom ();
+            //            PendingSendMap.Add (result.Value as string, message.Id);
+            //        } else {
+            //            NcAlertView.ShowMessage (this, "Could not send messasge", "Sorry, there was a problem sending the message.  Please try again.");
+            //        }
+            //    }
+            //});
+        }
+
+        void ReportSendError ()
+        {
+            NcAlertView.ShowMessage (this, "Could not send messasge", "Sorry, there was a problem sending the message.  Please try again.");
+            CleanupComposer ();
+            ComposeView.SetEnabled (true);
+        }
+
+        public void MessageComposerDidCompletePreparation (MessageComposer composer)
+        {
+            composer.Save (composer.Bundle.FullHtml, invalidateBundle: false);
+            var result = composer.Send ();
+            if (result.isOK ()) {
+                SendTokens.Add (result.Value as string);
+            } else {
+                ReportSendError ();
+            }
+        }
+
+        public void MessageComposerDidFailToLoadMessage (MessageComposer composer)
+        {
+            ReportSendError ();
+        }
+
+        void CleanupComposer ()
+        {
+            Composer.Delegate = null;
+            Composer = null;
+        }
+
+        public void Attach ()
+        {
+            var helper = new AddAttachmentViewController.MenuHelper (this, Account, View);
+            PresentViewController (helper.MenuViewController, true, null);
+        }
+
+        // User picking a file as an attachment
+        public void SelectFile (INachoFileChooser vc, McAbstrObject obj)
+        {
+            var attachment = obj as McAttachment;
+            if (attachment == null) {
+                var file = obj as McDocument;
+                if (file != null) {
+                    attachment = McAttachment.InsertSaveStart (Account.Id);
+                    attachment.SetDisplayName (file.DisplayName);
+                    attachment.UpdateFileCopy (file.GetFilePath ());
+                } else {
+                    var note = obj as McNote;
+                    if (note != null) {
+                        attachment = McAttachment.InsertSaveStart (Account.Id);
+                        attachment.SetDisplayName (note.DisplayName + ".txt");
+                        attachment.UpdateData (note.noteContent);
+                    }
+                }
+            }
+
+            if (attachment != null) {
+                Attachments.Add (attachment);
+                ComposeView.AddAttachment (attachment);
+                this.DismissViewController (true, null);
+            } else {
+                NcAssert.CaseError ();
+            }
+
+        }
+
+        // User adding an attachment from media browser
+        public void Append (McAttachment attachment)
+        {
+            attachment.Update ();
+            Attachments.Add (attachment);
+            ComposeView.AddAttachment (attachment);
+        }
+
+        public void AttachmentUpdated (McAttachment attachment)
+        {
+            ComposeView.UpdateAttachment (attachment);
+        }
+
+        // Not really a direct user action, but caused by the user selecting a date for the intent
+        public void DismissPhotoPicker ()
+        {
+            DismissViewController (true, null);
+        }
+
+        public void PresentFileChooserViewController (UIViewController vc)
+        {
+            PresentViewController (vc, true, null);
+        }
+
+        public void RemoveAttachment (McAttachment attachment)
+        {
+            Attachments.Remove (attachment);
+        }
+
+        #endregion
+
         #region Compose Delegate
 
         public void ChatComposeDidSend (ChatMessageComposeView composeView)
         {
-            //Send ();
+            Send ();
         }
 
         public void ChatComposeWantsAttachment (ChatMessageComposeView composeView)
@@ -203,6 +351,21 @@ namespace NachoClient.iOS
 
         public void UpdateMessageViewBlockProperties (ChatView chatView, int index, ChatMessageView messageView)
         {
+            var messageIndex = Messages.Count () - 1 - index;
+            var message = Messages.GetCachedMessage(messageIndex);
+            var previous = index > 0 ? Messages.GetCachedMessage(messageIndex + 1) : null;
+            var next = messageIndex > 0 ? Messages.GetCachedMessage(messageIndex - 1) : null;
+            var oneHour = TimeSpan.FromHours (1);
+            var atTimeBlockStart = previous == null || (message.DateReceived - previous.DateReceived > oneHour);
+            var atTimeBlockEnd = next == null || (next.DateReceived - message.DateReceived > oneHour);
+            var atParticipantBlockStart = previous == null || previous.FromEmailAddressId != message.FromEmailAddressId;
+            var atParticipantBlockEnd = next == null || next.FromEmailAddressId != message.FromEmailAddressId;
+            var showName = atTimeBlockStart || atParticipantBlockStart;
+            var showPortrait = atTimeBlockEnd || atParticipantBlockEnd;
+            var showTimestamp = atTimeBlockStart;
+            messageView.SetShowsName (showName);
+            messageView.SetShowsPortrait (showPortrait);
+            messageView.SetShowsTimestamp (showTimestamp);
         }
 
         public void ChatMessageViewDidSelectError (ChatView chatView, int index)
@@ -285,5 +448,136 @@ namespace NachoClient.iOS
             }
             Downloaders.Clear ();
         }
+
+        #region Notifications
+
+        bool IsListeningForNotifications = false;
+
+        void startListeningForNotifications ()
+        {
+            if (!IsListeningForNotifications) {
+                IsListeningForNotifications = true;
+                NcApplication.Instance.StatusIndEvent += StatusIndCallback;
+            }
+        }
+
+        void stopListeningForNotifications ()
+        {
+            if (IsListeningForNotifications) {
+                NcApplication.Instance.StatusIndEvent -= StatusIndCallback;
+                IsListeningForNotifications = false;
+            }
+        }
+
+        void StatusIndCallback (object sender, EventArgs e)
+        {
+            var s = (StatusIndEventArgs)e;
+
+            if (s.Account == null || (Messages != null && Messages.IsCompatibleWithAccount (s.Account))) {
+                Log.Info (Log.LOG_UI, "ConversationViewController status indicator callback: {0}", s.Status.SubKind.ToString ());
+                switch (s.Status.SubKind) {
+                case NcResult.SubKindEnum.Info_EmailMessageSetChanged:
+                    SetNeedsReload ();
+                    break;
+                case NcResult.SubKindEnum.Error_EmailMessageSendFailed:
+                    if (SendTokens.Contains (s.Tokens [0])) {
+                        ReportSendError ();
+                        SendTokens.Remove (s.Tokens [0]);
+                    }
+                    break;
+                case NcResult.SubKindEnum.Error_EmailMessageReplyFailed:
+                    if (SendTokens.Contains(s.Tokens[0])){
+                        ReportSendError ();
+                        SendTokens.Remove (s.Tokens [0]);
+                    }
+                    break;
+                case NcResult.SubKindEnum.Info_EmailMessageSendSucceeded:
+                    if (SendTokens.Contains (s.Tokens [0])) {
+                        SendTokens.Remove (s.Tokens [0]);
+                        ComposeView.Clear ();
+                        ComposeView.SetEnabled (true);
+                    }
+                    break;
+                }
+            }
+        }
+
+        #endregion
+        #region Reloading Messages
+
+        bool NeedsReload;
+        bool IsReloading;
+
+        object MessagesLock = new object ();
+
+        protected void SetNeedsReload ()
+        {
+            NeedsReload = true;
+            if (!IsReloading) {
+                Reload ();
+            }
+        }
+
+        protected void Reload ()
+        {
+            if (!IsReloading) {
+                IsReloading = true;
+                NeedsReload = false;
+                if (Messages.HasBackgroundRefresh ()) {
+                    Log.Info (Log.LOG_UI, "ConversationViewController.Reload: using NachoEmailMessages background refresh");
+                    Messages.BackgroundRefresh (HandleReloadResults);
+                } else {
+                    Log.Info (Log.LOG_UI, "ConversationViewController.Reload: simulating a background refresh because this NachoEmailMessages doesn't have one");
+                    NcTask.Run (() => {
+                        List<int> adds;
+                        List<int> deletes;
+                        NachoEmailMessages messages;
+                        lock (MessagesLock) {
+                            messages = Messages;
+                        }
+                        bool changed = messages.BeginRefresh (out adds, out deletes);
+                        BeginInvokeOnMainThread (() => {
+                            bool handledResults = false;
+                            lock (MessagesLock) {
+                                if (messages == Messages) {
+                                    Messages.CommitRefresh ();
+                                    HandleReloadResults (changed, adds, deletes);
+                                    handledResults = true;
+                                }
+                            }
+                            if (!handledResults) {
+                                IsReloading = false;
+                                if (NeedsReload) {
+                                    Reload ();
+                                }
+                            }
+                        });
+                    }, "ConversationViewReloadTask");
+                }
+            }
+        }
+
+        void HandleReloadResults (bool changed, List<int> adds, List<int> deletes)
+        {
+            Log.Info (Log.LOG_UI, "ConversationViewController.HandleReloadResults: changed = {0}, {1} adds, {2} deletes",
+                changed, adds == null ? 0 : adds.Count, deletes == null ? 0 : deletes.Count);
+            if (changed) {
+                Messages.ClearCache ();
+                if (deletes.Count == 0 && adds.Count == 1 && adds [0] == 0) {
+                    Log.Info (Log.LOG_UI, "ConversationViewController adding message to end");
+                    ChatView.InsertMessageViewAtEnd ();
+                } else {
+                    Log.Info (Log.LOG_UI, "ConversationViewController reloading (complex changes)");
+                    ChatView.ReloadData ();
+                }
+            }
+            IsReloading = false;
+            if (NeedsReload) {
+                Reload ();
+            }
+        }
+
+        #endregion
+
     }
 }
