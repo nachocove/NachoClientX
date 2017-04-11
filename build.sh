9be0ae4ead4fd7cc516d95d9c20691f35b903f9b
Owen Shaw <owen@owenshaw.com>
2017-04-11 10:02:59 -0700

    remove NachoClient.userprefs so it's not tracked by git
    This has long bugged me because NachoClient.userprefs is tracked by git,
    but is also in the .gitignore file, so git becomes confused sometimes
    when there are edits, which is almost always

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 ./scripts/fetch.py || (echo "Could not complete fetch.py"; exit 1) || exit 1
-git update-index --assume-unchanged NachoClient.userprefs || (echo "Could not complete git update-index operation"; exit 1) || exit 1
 make -f build.mk || (echo "Build failed."; exit 1) || exit 1
