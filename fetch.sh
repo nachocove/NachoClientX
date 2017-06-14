2435c03303ee3342c0ee11f32be2bf16cea0fca9
Owen Shaw <owen@owenshaw.com>
2017-06-14 10:52:01 -0700

    update default repo list
    remove items that are now submodules in MailKit

--- fetch.sh
+++
@@ -1,8 +0,0 @@
-# Fetch all sources from git
-#
-# USAGE: sh fetch.sh
-#
-# NOTE: If you add a new repo that is part of NachoClientX, you 
-# MUST add that repo to scripts/repos_cfg.py.
-
-./scripts/fetch.py
--- scripts/repos_cfg.py
+++ scripts/repos_cfg.py
@@ -1,38 +1,27 @@
 repos = (
     'Reachability',
     'registered-domain-libs',
-    'SWRevealViewController',
-    'SWRevealViewControllerBinding',
     'ios-openssl',
     'NachoPlatformBinding',
     'NachoUIMonitorBinding',
-    'bc-csharp',
-    'MimeKit',
     'MailKit',
     'DnDns',
     'DDay-iCal-Xamarin',
     'CSharp-Name-Parser',
-    'Telemetry',
     'aws-sdk-xamarin',
     'lucene.net-3.0.3',
     'MobileHtmlAgilityPack',
-    'Google.iOS',
-    'Portable.Text.Encoding',
     'JetBlack.Caching',
     'TokenAutoComplete',
     'TokenAutoCompleteBinding',
     'rtfparserkit',
     'rtfparserkitBinding',
     'OkHttp-Xamarin',
-    # This is always the last one
+    # # This is always the last one
     'NachoClientX'
 )
 
 branch_exceptions = {
-    'bc-csharp': {
-        # bc-csharp is fixed to this branch no matter what
-        'fixed-branch': 'visual-studio-2010'
-    },
 }
 
 # You can run this script:
