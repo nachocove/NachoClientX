94f33d09e17dbe806499708ea8f5c8de8cebd86a
Owen Shaw <owen@owenshaw.com>
2017-06-16 10:39:02 -0700

    remove old unused scripts

--- scripts/remove_itunesmetadata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ~/Library/Developer/Xcode/Archives
-find . -name "iTunesMetadata.plist" -exec rm {} + 
--- scripts/update_build_number.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -f .build_number.new ]; then
-  echo "Updating build number..."
-  mv .build_number.new .build_number
-fi
-
