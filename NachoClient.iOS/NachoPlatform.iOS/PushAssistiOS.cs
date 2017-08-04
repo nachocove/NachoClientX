b6def1c85254a7dec136dde1ae7b76642cf0de0b
Owen Shaw <owen@owenshaw.com>
2017-08-03 21:24:17 -0700

    restore ios push assist
    just reverting the changes Jan made to disable it

--- NachoClient.iOS/NachoPlatform.iOS/PushAssistiOS.cs
+++ NachoClient.iOS/NachoPlatform.iOS/PushAssistiOS.cs
@@ -4,46 +4,11 @@ using NachoCore;
 
 namespace NachoPlatform
 {
-    public class PushAssist : IPushAssist
+    public class PushAssist : PushAssistCommon
     {
-        public PushAssist (IPushAssistOwner owner)
+        public PushAssist (IPushAssistOwner owner) : base (owner)
         {
         }
-
-        public static bool SetDeviceToken (string token)
-        {
-            return true;
-        }
-
-        public static bool ProcessRemoteNotification (PingerNotification pinger, NotificationFetchFunc fetch)
-        {
-            return true;
-        }
-
-        #region IPushAssist implementation
-
-        public void Dispose ()
-        {
-        }
-
-        public void Execute ()
-        {
-        }
-
-        public void Park ()
-        {
-        }
-
-        public bool IsStartOrParked ()
-        {
-            return false;
-        }
-
-        public void Stop ()
-        {
-        }
-
-        #endregion
     }
 }
 
